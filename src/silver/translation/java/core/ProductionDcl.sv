41d45ba55d16719b58c76d2e098b9973b91d6d0b
ted <ted@localhost>
2009-07-28 20:52:44 +0000

    Switched to TreeMaps as these are slightly more memory efficient.

--- src/silver/translation/java/core/ProductionDcl.sv
+++ src/silver/translation/java/core/ProductionDcl.sv
@@ -35,10 +35,10 @@ makeIndexDcls(0, sigNames) ++ "\n" ++
 "\tpublic static final Class childTypes[] = {" ++ makeChildTypesList(ns.inputElements, top.env) ++ "};\n\n" ++
 
 "\tpublic static common.Lazy forward;\n" ++
-"\tpublic static final java.util.Map<String, common.Lazy> forwardAttributes = new java.util.HashMap<String, common.Lazy>();\n\n" ++
+"\tpublic static final java.util.Map<String, common.Lazy> forwardAttributes = new java.util.TreeMap<String, common.Lazy>();\n\n" ++
 
-"\tpublic static final java.util.Map<String, common.Lazy> localAttributes = new java.util.HashMap<String, common.Lazy>();\n" ++
-"\tpublic static final java.util.Map<String, common.Lazy> synthesizedAttributes = new java.util.HashMap<String, common.Lazy>();\n" ++
+"\tpublic static final java.util.Map<String, common.Lazy> localAttributes = new java.util.TreeMap<String, common.Lazy>();\n" ++
+"\tpublic static final java.util.Map<String, common.Lazy> synthesizedAttributes = new java.util.TreeMap<String, common.Lazy>();\n" ++
 "\tpublic static final java.util.Map<Object, java.util.Map<String, common.Lazy>> inheritedAttributes = new java.util.HashMap<Object, java.util.Map<String, common.Lazy>>();\n\n" ++	
 
 
@@ -106,7 +106,7 @@ String ::= className::String s::[String]{
   return if null(s) 
 	 then "" 
 	 else "\t" ++ className ++ ".inheritedAttributes.put(i_" ++ head(s) ++ ", " ++ 
-							   "new java.util.HashMap<String, common.Lazy>());\n"  ++ makeStaticDcls(className, tail(s));
+							   "new java.util.TreeMap<String, common.Lazy>());\n"  ++ makeStaticDcls(className, tail(s));
 }
 
 function makeConstructor
