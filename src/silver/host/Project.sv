cc9c48698085f1ed137152b7ef58700cbe645d6c
Ted Kaminski <tedinski@cs.umn.edu>
2009-08-26 20:50:35 +0000

    make use of exports

---
+++ src/silver/host/Project.sv
@@ -0,0 +1,19 @@
+grammar silver:host;
+
+-- concrete syntax from these grammars
+exports silver:definition:core;
+exports silver:definition:concrete_syntax;
+exports silver:definition:type:io;
+exports silver:definition:type:higherorder;
+exports silver:definition:type:productiontype;
+exports silver:definition:type:decorated;
+exports silver:definition:type:anytype;
+
+-- symbols
+exports silver:analysis:binding:driver;
+exports silver:analysis:typechecking:driver;
+exports silver:analysis:typechecking:core;
+exports silver:analysis:typechecking:concrete_syntax;
+exports silver:analysis:typechecking:type:io;
+exports silver:analysis:typechecking:type:anytype;
+
