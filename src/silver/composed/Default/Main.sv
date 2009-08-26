f95f0f2d7e81744ec2407e886fd21c55ca88693c
ted <ted@localhost>
2009-08-26 20:50:35 +0000

    make use of exports

--- src/silver/composed/Default/Main.sv
+++ src/silver/composed/Default/Main.sv
@@ -1,57 +1,24 @@
 grammar silver:composed:Default;
 
---import core hiding parse;
-
+import silver:host;
+import silver:host:env;
+import silver:translation:java;
 import silver:driver;
-
-import silver:definition:concrete_syntax;
-import silver:definition:core;
-
 import silver:util:command;
 
-import silver:definition:env;
-import silver:definition:env:parser;
-
---BINDING CHECKING
-import silver:analysis:binding:driver;
-
---TYPE CHECKING
-import silver:analysis:typechecking:driver;
-import silver:analysis:typechecking:core;
-import silver:analysis:typechecking:concrete_syntax;
-import silver:analysis:typechecking:type:io;
-import silver:analysis:typechecking:type:anytype;
-
---JAVA TRANSLATION (still needs importing!)
-import silver:translation:java:driver;
-import silver:translation:java:core;
-import silver:translation:java:env;
-import silver:translation:java:concrete_syntax;
-import silver:translation:java:type:io;
-import silver:translation:java:type:anytype;
-import silver:extension:list:java; -- optional!
+import silver:extension:list:java;
+import silver:modification:let_fix:java;
+import silver:modification:collection:java;
+import silver:modification:autocopyattr:java;
 
 --EXTENSIONS (that do not add to concrete syntax (those in parser above))
-
 -- not yet used
 --import silver:extension:doc;
 --import silver:extension:doc:driver;
 --import silver:extension:doc:copper;
 
---MODIFICATIONS translations (syntax goes in parser above)
-import silver:modification:let_fix:java;
-import silver:modification:collection:java;
-import silver:modification:autocopyattr:java;
-
---PARSERS
 parser rParse::Root {
-  silver:definition:core;
-  silver:definition:concrete_syntax;
-  silver:definition:type:io;
-  silver:definition:type:higherorder;
-  silver:definition:type:productiontype;
-  silver:definition:type:decorated;
-  silver:definition:type:anytype;
+  silver:host;
 
   silver:extension:convenience;
   silver:extension:list;
@@ -72,9 +39,7 @@ parser cParse::Command {
 }
 
 parser iParse::aRootSpec {
-  silver:definition:env:parser;
-  silver:definition:core:env_parser;
-  silver:definition:concrete_syntax:env_parser;
+  silver:host:env;
 
   silver:modification:collection:env_parser;
   silver:modification:autocopyattr:env_parser;
---
+++ src/silver/host/env/Project.sv
@@ -0,0 +1,10 @@
+grammar silver:host:env;
+
+-- concrete syntax
+exports silver:definition:env:parser;
+exports silver:definition:core:env_parser;
+exports silver:definition:concrete_syntax:env_parser;
+
+-- symbols
+exports silver:definition:env;
+
