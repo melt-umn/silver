8e8696e0ffaa14c28d97332781e5b71d2a1d36a7
Ted Kaminski <tedinski@cs.umn.edu>
2010-08-12 01:04:06 +0000

    Initial checkin of type expressions for Silver.

---
+++ grammars/silver/definition/type/PrettyPrinting.sv
@@ -0,0 +1,104 @@
+grammar silver:definition:type;
+
+import silver:util;
+
+synthesized attribute typepp :: String occurs on TypeExp;
+autocopy attribute boundVariables :: [TyVar] occurs on TypeExp;
+
+function prettyType
+String ::= te::TypeExp
+{
+  te.boundVariables = te.freeVariables;
+  return te.typepp;
+}
+
+function prettyTypeWith
+String ::= te::TypeExp tvs::[TyVar]
+{
+  te.boundVariables = tvs;
+  return te.typepp;
+}
+--------------------------------------------------------------------------------
+aspect production varTypeExp
+top::TypeExp ::= tv::TyVar
+{
+  top.typepp = findAbbrevFor(tv, top.boundVariables);
+}
+
+aspect production intTypeExp
+top::TypeExp ::=
+{
+  top.typepp = "Integer";
+}
+
+aspect production boolTypeExp
+top::TypeExp ::=
+{
+  top.typepp = "Boolean";
+}
+
+aspect production floatTypeExp
+top::TypeExp ::=
+{
+  top.typepp = "Float";
+}
+
+aspect production stringTypeExp
+top::TypeExp ::=
+{
+  top.typepp = "String";
+}
+
+-- e.g. nonterminal (reference) and terminal types
+aspect production nominalTypeExp
+top::TypeExp ::= fn::String params::[TypeExp]
+{
+  top.typepp = fn ++ if !null(params) then "<" ++ folds(" ", mapTypePP(params, top.boundVariables)) ++ ">" else "";
+}
+
+-- e.g. decorated nonterminal types
+aspect production decoratedTypeExp
+top::TypeExp ::= fn::String params::[TypeExp]
+{
+  top.typepp = "Decorated " ++ fn ++ if !null(params) then "<" ++ folds(" ", mapTypePP(params, top.boundVariables)) ++ ">" else "";
+}
+
+aspect production functionTypeExp
+top::TypeExp ::= out::TypeExp params::[TypeExp]
+{
+  top.typepp =   "Function(" ++ out.typepp ++ " ::= " ++ folds(" ", mapTypePP(params, top.boundVariables)) ++ ")" ;
+}
+
+aspect production productionTypeExp
+top::TypeExp ::= out::TypeExp params::[TypeExp]
+{
+  top.typepp = "Production(" ++ out.typepp ++ " ::= " ++ folds(" ", mapTypePP(params, top.boundVariables)) ++ ")" ;
+}
+
+--------------------------------------------------------------------------------
+function findAbbrevFor
+String ::= tv::TyVar  bv::[TyVar]
+{
+  return findAbbrevHelp(tv, bv, ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p"]);
+}
+
+function findAbbrevHelp
+String ::= tv::TyVar  bv::[TyVar]  vn::[String]
+{
+  return if null(vn) || null(bv) then "V_" ++ toString(case tv of tyVar(i) -> i end)
+         else if tyVarEqual(tv, head(bv))
+              then head(vn)
+              else findAbbrevHelp(tv, tail(bv), tail(vn));
+}
+
+-- TODO: oh crap is this stupid
+function mapTypePP
+[String] ::= tes::[TypeExp] bv::[TyVar]
+{
+  local attribute fst :: TypeExp;
+  fst = head(tes);
+  fst.boundVariables = bv;
+  
+  return if null(tes) then []
+         else fst.typepp :: mapTypePP(tail(tes), bv);
+}
