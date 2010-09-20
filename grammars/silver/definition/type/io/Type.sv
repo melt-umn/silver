bd6ba8fccc757c9c386a899841543aaf94c7f075
ted <ted@localhost>
2010-09-20 21:23:06 +0000

    Add the IO type back.  Also, include a missing file from an earlier commit

---
+++ grammars/silver/definition/type/io/Type.sv
@@ -0,0 +1,20 @@
+grammar silver:definition:type:io;
+import silver:definition:core;
+import silver:definition:env;
+
+terminal IO_kwd /IO/ lexer classes {KEYWORD};
+
+concrete production ioType
+top::Type ::= 'IO'
+{
+  top.pp = "IO";
+  top.location = loc(top.file, $1.line, $1.column);
+  
+  top.typerep = ioTypeExp();
+
+  top.errors := [];
+  top.warnings := [];
+  
+  top.lexicalTypeVariables = [];
+}
+
