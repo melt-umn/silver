a426890b83fae57722efec81e28eff908969579c
Ted Kaminski <tedinski@cs.umn.edu>
2010-09-20 22:02:46 +0000

    introduce the 'minimal' composed grammar. We'll be working with this for the time being.

---
+++ grammars/silver/composed/minimal/Main.sv
@@ -0,0 +1,29 @@
+grammar silver:composed:minimal;
+
+import silver:host;
+import silver:host:env;
+import silver:translation:java;
+import silver:driver;
+import silver:util:command;
+
+parser rParse::Root {
+  silver:host;
+
+  silver:translation:java:concrete_syntax:copper;
+}
+
+parser cParse::Command {
+  silver:util:command;
+}
+
+parser iParse::aRootSpec {
+  silver:host:env;
+
+  silver:translation:java:concrete_syntax:copper:env_parser;
+}
+
+function main 
+IO ::= args::String i::IO {
+  -- please note that run in BuildProcess.sv will call exit(), so we may not "get back here"
+  return (decorate run(i, args) with {rParser = rParse; cParser = cParse; iParser = iParse;}).io;
+}
