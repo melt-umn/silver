c3825f7c1ae96548ff0f35c1ba65dda2d9e896f8
Ted Kaminski <tedinski@cs.umn.edu>
2010-09-27 20:01:31 +0000

    new list core file

--- test/core/List.sv
+++ test/core/List.sv
@@ -1,47 +1,92 @@
 grammar core;
 
-nonterminal AnyTypeList ;
+nonterminal List<'a>;
 
-synthesized attribute empty_AnyTypeList :: Boolean ;
-synthesized attribute head_AnyTypeList :: AnyType ;
-synthesized attribute tail_AnyTypeList :: Decorated AnyTypeList ;
-synthesized attribute length_AnyTypeList :: Integer ;
+synthesized attribute i_headList<'a> :: 'a;
+attribute i_headList<'a> occurs on List<'a>;
+synthesized attribute i_tailList<'a> :: List<'a>;
+attribute i_tailList<'a> occurs on List<'a>;
+synthesized attribute i_emptyList :: Boolean;
+attribute i_emptyList occurs on List<'a>;
+synthesized attribute i_lengthList :: Integer;
+attribute i_lengthList occurs on List<'a>;
 
-attribute empty_AnyTypeList occurs on AnyTypeList ;
-attribute head_AnyTypeList occurs on AnyTypeList ;
-attribute tail_AnyTypeList occurs on AnyTypeList ;
-attribute length_AnyTypeList occurs on AnyTypeList ;
-
-function nil_AnyTypeList
-Decorated AnyTypeList ::= {
-  return decorate internal_nil_AnyTypeList() with {};
-}
-
-abstract production internal_nil_AnyTypeList
-l::AnyTypeList ::= {
-  l.empty_AnyTypeList = true ;
-  l.length_AnyTypeList = 0 ;
-  l.head_AnyTypeList = error ("Accessing head on value of type [ ] not allowed.\n" );
-  l.tail_AnyTypeList = error ("Accessing tail on value of type [ ] not allowed.\n" );
-}
-
-function cons_AnyTypeList
-Decorated AnyTypeList ::= h::AnyType t::Decorated AnyTypeList {
-  return decorate internal_cons_AnyTypeList(h, t) with{}; 
-}
-abstract production internal_cons_AnyTypeList
-l::AnyTypeList ::= h::AnyType t::Decorated AnyTypeList
+abstract production i_nilList
+l::List<'a> ::=
 {
-  l.empty_AnyTypeList = false ;
-  l.length_AnyTypeList = 1 + t.length_AnyTypeList ;
-  l.head_AnyTypeList = h ;
-  l.tail_AnyTypeList = t ;
+  l.i_emptyList = true;
+  l.i_lengthList = 0;
+  l.i_headList = error("requested head of nil");
+  l.i_tailList = error("requested tail of nil");
 }
-function append_AnyTypeList
-Decorated AnyTypeList ::= l1::Decorated AnyTypeList l2::Decorated AnyTypeList
+
+abstract production i_consList
+l::List<'a> ::= h::'a  t::List<'a>
 {
-  return if l1.empty_AnyTypeList
-           then l2 
-           else cons_AnyTypeList ( l1.head_AnyTypeList ,
-                                   append_AnyTypeList ( l1.tail_AnyTypeList , l2 ) ) ;
+  l.i_emptyList = false;
+  l.i_lengthList = t.lengthList + 1;
+  l.i_headList = h;
+  l.i_tailList = t;
 }
+
+--------------------------------------------------------------------------------
+
+function nil
+['a] ::=
+{
+  return i_nilList();
+} foreign {
+  "java" : return "common.ConsCell.nil";
+}
+
+function cons
+['a] ::= h::'a  t::['a]
+{
+  return i_consList(h, t);
+} foreign {
+  "java" : return "new common.ConsCell(%h%, %t%)";
+}
+
+function append
+['a] ::= l1::['a] l2::['a]
+{
+  return if l1.i_emptyList
+         then l2
+         else i_consList(l1.i_headList, append(l1.i_tailList, l2));
+} foreign {
+  "java" : return "new common.AppendCell(%l1%, %l2%)";
+}
+
+
+function null
+Boolean ::= l::['a]
+{
+  return l.i_emptyList;
+} foreign {
+  "java" : return "%l%.nil()";
+}
+
+function listLength  -- not called 'length' since this is a builtin language feature, but that's how you should call it.
+Integer ::= l::['a]
+{
+  return l.i_lengthList;
+} foreign {
+  "java" : return "new Integer(%l%.length)";
+}
+
+function head
+'a ::= l::['a]
+{
+  return l.i_headList;
+} foreign {
+  "java" : return "%l%.head()";
+}
+
+function tail
+['a] ::= l::['a]
+{
+  return l.i_tailList;
+} foreign {
+  "java" : return "%l%.tail()";
+}
+
