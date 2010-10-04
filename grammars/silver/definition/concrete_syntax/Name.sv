e301676c3e7ac0695414f909155e0aa0f2ae513f
ted <ted@localhost>
2010-10-04 20:00:58 +0000

    Huh. Odd terminal just sitting there for no reason. Remove it.

--- grammars/silver/definition/concrete_syntax/Name.sv
+++
@@ -1,9 +0,0 @@
-grammar silver:definition:concrete_syntax;
-
-terminal Parse_kwd /parse/ lexer classes {KEYWORD};
-
-concrete production nameParse
-top::Name ::= 'parse'
-{
-   forwards to nameId(terminal(Id_t, "parse", $1)); 
-}
