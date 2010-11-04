93370c53ab47931af70aec1c2d345313268ad5ed
ted <ted@localhost>
2010-11-04 17:28:48 +0000

    Correct the tutorials:dc grammar, and comment it a bit more.

--- grammars/tutorials/dc/DC.sv
+++ grammars/tutorials/dc/DC.sv
@@ -1,37 +1,46 @@
 grammar tutorials:dc ;
--- export tutorials:dc ;
+
+{--
+ - "Pretty print" - unparse the tree to a string
+ -}
+synthesized attribute pp :: String ;
 
 -- Concrete Syntax --
 ---------------------
 
-nonterminal Root_c with pp ;
+{--
+ - The abstract syntax tree.  This is a "higher order attribute."
+ -}
+synthesized attribute ast_Root :: Root;
 
-synthesized attribute ast_Root :: Root occurs on Root_c ;
+-- Method #1 for declaring than an attribute occurs on a nonterminal: 'with'
+nonterminal Root_c with pp, ast_Root;
 
+-- 'concrete' means when this grammar is used to generate a parser, this production
+-- will be sent to the parser generator.
 concrete production root_c
 r::Root_c ::= e::Expr_c 
 {
- r.pp = e.pp ;
- r.ast_Root = root(e.ast_Expr);
+  r.pp = e.pp;
+  r.ast_Root = root(e.ast_Expr); -- 'root' is the abstract production below.
 }
-
 
 -- Abstract Syntax --
 ---------------------
 
-nonterminal Root ;
+{--
+ - The type of the abstract syntax tree for Root_c.
+ -}
+nonterminal Root;
 
-synthesized attribute pp :: String ;
+-- Method #2 for declaring than an attribute occurs on a nonterminal: 'occurs on'
+attribute pp occurs on Root; -- "stand alone" occurs.
+synthesized attribute value :: Integer occurs on Root; -- declaration and occurs.
 
-attribute pp occurs on Root ;
-
-synthesized attribute value :: Integer ;
-
-attribute value occurs on Root ;
-
+-- 'abstract' is not 'concrete'.  The parser generator will not be told about this production.
 abstract production root
 r::Root ::= e::Expr
 {
- r.pp = e.pp ;
- r.value = e.value ;
+  r.pp = e.pp ;
+  r.value = e.value ;
 }
