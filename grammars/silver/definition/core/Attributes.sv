19713f41abb58fa6c634f4e5a4583dda4e039df2
ted <ted@localhost>
2010-11-12 22:00:30 +0000

    (minor)slightly more coherent declaration ordering

--- grammars/silver/definition/core/Attributes.sv
+++ grammars/silver/definition/core/Attributes.sv
@@ -1,24 +1,25 @@
 grammar silver:definition:core;
 
-autocopy attribute expected :: Expected;
 synthesized attribute name :: String;
-autocopy attribute grammarName :: String;
 synthesized attribute pp :: String;
 synthesized attribute location :: Decorated Location;
-
 autocopy attribute file :: String;
+autocopy attribute grammarName :: String;
+
+
+autocopy attribute globalImports :: Decorated Env;
+autocopy attribute compiledGrammars :: [Decorated RootSpec];
+
+
+autocopy attribute expected :: Expected;
 
 synthesized attribute names :: [String];
 synthesized attribute importedDefs :: Defs;
-
-autocopy attribute globalImports :: Decorated Env;
 
 autocopy attribute env :: Decorated Env;
 
 synthesized attribute errors :: [Decorated Message] with ++;
 synthesized attribute warnings :: [Decorated Message] with ++;
-
-autocopy attribute compiledGrammars :: [Decorated RootSpec];
 
 autocopy attribute expectedInputTypes :: [TypeExp];
 synthesized attribute exprs :: [Decorated Expr];
@@ -29,3 +30,4 @@ autocopy attribute signature :: Decorated NamedSignature;
 synthesized attribute productionAttributes :: Defs;
 
 autocopy attribute decoratingnt :: TypeExp;
+
