22eece0a02bdeb869159d1723b3d2011f2358432
Eric Van Wyk <evw@cs.umn.edu>
2010-11-16 09:44:04 +0000

    reorganizing dc

--- grammars/tutorials/dc/Terminals.sv
+++ grammars/tutorials/dc/Terminals.sv
@@ -1,25 +1,26 @@
 grammar tutorials:dc ;
 
--- Between forward slashes is a regular expression:
-
+{- Regular expressions associated with a terminal are written after
+   the terminal name between forward slashes. -}
 terminal IntLit_t /[0-9]+/ ; 
 
--- Inside single quotes is a string literal:
-
--- Note that we're not specifying associativity or precedence in this example,
--- but if we were, an example of that syntax is commented out:
-
-terminal Plus_t   '+' ; -- precedence = 1, association = left;
-terminal Dash_t   '-' ; -- precedence = 1, association = left;
-terminal Star_t   '*' ; -- precedence = 2, association = left;
-terminal Slash_t  '/' ; -- precedence = 2, association = left;
-terminal Hat_t    '^' ; -- precedence = 3, association = right;
+{- Regular expressions that are constant strings may be represented
+   using single quotes. -}
+terminal Plus_t   '+' ; 
+terminal Dash_t   '-' ; 
+terminal Star_t   '*' ; 
+terminal Slash_t  '/' ; 
+terminal Hat_t    '^' ;
+ 
 terminal LParen_t '(' ;
 terminal RParen_t ')' ;
 
--- 'ignore' terminals are permitted to exist between any tokens in productions.
+{- 'ignore' terminals are recognized by the scanner but then discarded
+   and not returned to the parser.  This is useful for specifying the
+   white-space that may appear between lexical constructs and for
+   comments.  
+-}
 
 ignore terminal WhiteSpace_t /[\t\n\ ]+/  ;
-
 ignore terminal LineComment_P  /[\/][\/].*/  ;
 
