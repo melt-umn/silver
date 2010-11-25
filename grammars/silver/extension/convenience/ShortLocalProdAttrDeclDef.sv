116a8d1ed25b5053d7cff5676afe9fb4669e040f
evw <evw@localhost>
2010-11-25 16:50:09 +0000

    Adding productionStmtAppend - an analog to existing productionStmtsAppend and extension for abbreviated form of declaration and definition of local and production attributes

---
+++ grammars/silver/extension/convenience/ShortLocalProdAttrDeclDef.sv
@@ -0,0 +1,54 @@
+grammar silver:extension:convenience;
+
+import silver:definition:core;
+import silver:definition:type;
+import silver:definition:type:syntax;
+
+concrete production shortLocalDecl
+top::ProductionStmt ::= lk::'local' a::Name ht::'::' te::Type 
+                        eq::'=' v::Expr sm::';'
+{
+ forwards to
+    productionStmtAppend (
+       localAttributeDcl (lk, ak, a, ht, te, sm ) ,
+       valueDef ( qNameId(a), eq, v, sm ) ) ;
+
+ --local attribute lk :: Local_kwd ;
+ --lk = terminal(Local_kwd, "local");
+
+ local attribute ak :: Attribute_kwd ;
+ ak = terminal(Attribute_kwd, "attribute");
+}
+
+concrete production shortLocalDeclwKwds
+top::ProductionStmt ::= lk::'local' ak::'attribute' a::Name ht::'::' te::Type 
+                        eq::'=' v::Expr sm::';'
+{
+ forwards to
+    productionStmtAppend (
+       localAttributeDcl (lk, ak, a, ht, te, sm ) ,
+       valueDef ( qNameId(a), eq, v, sm ) ) ;
+}
+
+concrete production shortProductionDecl
+top::ProductionStmt ::= pk::'production' a::Name ht::'::' te::Type 
+                        eq::'=' v::Expr sm::';'
+{
+ forwards to
+    productionStmtAppend (
+       productionAttributeDcl (pk, ak, a, ht, te, sm ) ,
+       valueDef ( qNameId(a), eq, v, sm ) ) ;
+
+ local attribute ak :: Attribute_kwd ;
+ ak = terminal(Attribute_kwd, "attribute");
+}
+
+concrete production shortProductionDeclwKwds
+top::ProductionStmt ::= pk::'production' ak::'attribute' 
+                        a::Name ht::'::' te::Type eq::'=' v::Expr sm::';'
+{
+ forwards to
+    productionStmtAppend (
+       productionAttributeDcl (pk, ak, a, ht, te, sm ) ,
+       valueDef ( qNameId(a), eq, v, sm ) ) ;
+}
