6d196bfc5af4c6571c0bca1e57b7856dc919ecfa
ted <ted@localhost>
2011-01-02 01:00:33 +0000

    Remove the ParserDcl nonterminal. There is no reason for its existance, it only complicates things.

--- grammars/silver/translation/java/concrete_syntax/copper/ParserDcl.sv
+++ grammars/silver/translation/java/concrete_syntax/copper/ParserDcl.sv
@@ -1,33 +1,16 @@
 grammar silver:translation:java:concrete_syntax:copper;
 
-attribute javaClasses occurs on ParserDcl;
-attribute disambiguationGroupDcls,parserAttrDcls occurs on ParserDcl, ModuleList, ModuleName, Module, ModuleExportedDefs;
-
 aspect production parserDcl
-top::AGDcl ::= p::ParserDcl
+top::AGDcl ::= 'parser' n::Name '::' t::Type '{' m::ModuleList '}'
 {
-  top.javaClasses = p.javaClasses;
-  -- note that ParserDcl has things like disambig, parsattrs, rules, etc. These do not appear here
-  -- putting them here would be tanamount to "export all concrete syntax that appear in a parser dcl"
-}
-
--- TODO: ParserDcl should be turned into a spec like all the rest. It's inconsistent.
--- LOL there is a ParserSpec. This is a mess!
-aspect production parserStmt
-top::ParserDcl ::= 'parser' n::Name '::' t::Type '{' m::ModuleList '}'
-{
-
   local attribute className :: String;
   className = "P" ++ n.name;
 
   local attribute packageName :: String;
   packageName = makeName(top.grammarName);
 
-  local attribute fullClassName :: String;
-  fullClassName = packageName ++ "." ++ className;
-
   local attribute parserName :: String;
-  parserName = makeParserName(top.fullName);
+  parserName = makeParserName(fName);
 
   top.javaClasses = [[className,
                       generateFunctionClassString(top.grammarName, n.name, namedSig, parseResult)
@@ -42,38 +25,9 @@ top::ParserDcl ::= 'parser' n::Name '::' t::Type '{' m::ModuleList '}'
 "\t\t} catch(Throwable t) {\n" ++
 "\t\t\tthrow new RuntimeException(\"An error occurs while parsing\", t);\n" ++
 "\t\t}\n";
-
-  top.disambiguationGroupDcls = m.disambiguationGroupDcls;
-  top.parserAttrDcls = m.parserAttrDcls;
 }
 
-aspect production moduleListOne
-top::ModuleList ::= c1::ModuleName ';'
-{
-  top.disambiguationGroupDcls = c1.disambiguationGroupDcls;
-  top.parserAttrDcls = c1.parserAttrDcls;
-}
-
-aspect production moduleListCons
-top::ModuleList ::= c1::ModuleName ';' c2::ModuleList
-{
-  top.disambiguationGroupDcls = c1.disambiguationGroupDcls ++ c2.disambiguationGroupDcls;
-  top.parserAttrDcls = c1.parserAttrDcls ++ c2.parserAttrDcls;
-}
-
-aspect production moduleName
-top::ModuleName ::= pkg::QName
-{
-  top.disambiguationGroupDcls = m.disambiguationGroupDcls;
-  top.parserAttrDcls = m.parserAttrDcls;
-}
-
-aspect production module 
-top::Module ::= c::[Decorated RootSpec] g::Decorated QName a::String o::[String] h::[String] w::[[String]]
-{
-  top.disambiguationGroupDcls = med.disambiguationGroupDcls;		  
-  top.parserAttrDcls = med.parserAttrDcls;		  
-}
+attribute disambiguationGroupDcls,parserAttrDcls occurs on ModuleExportedDefs;
 
 aspect production moduleExportedDefs
 top::ModuleExportedDefs ::= compiled::[Decorated RootSpec] need::[String] seen::[String]
--- grammars/silver/translation/java/concrete_syntax/copper/ParserSpec.sv
+++ grammars/silver/translation/java/concrete_syntax/copper/ParserSpec.sv
@@ -2,15 +2,8 @@ grammar silver:translation:java:concrete_syntax:copper;
 
 attribute disambiguationGroupDcls,parserAttrDcls occurs on ParserSpec;
 
-aspect production i_parserSpec
-top::ParserSpec ::= ps::Decorated ParserDcl
-{
-  top.disambiguationGroupDcls = ps.disambiguationGroupDcls;
-  top.parserAttrDcls = ps.parserAttrDcls;
-}
-
 aspect production i_parserSpecFL
-top::ParserSpec ::= name::String start::String mods::[String] grams::[Decorated RootSpec]
+top::ParserSpec ::= locat::Decorated Location name::String start::String mods::[String] grams::[Decorated RootSpec]
 {
   top.disambiguationGroupDcls = med.disambiguationGroupDcls;
   top.parserAttrDcls = med.parserAttrDcls;
