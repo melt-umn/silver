603fabca0bc72206c850b5d7806efce4b3b6bbfa
evw <evw@localhost>
2011-01-28 15:58:27 +0000

    tweaks showing problem

--- grammars/lib/langproc/tests/Expr.sv
+++ grammars/lib/langproc/tests/Expr.sv
@@ -1,11 +1,11 @@
 grammar lib:langproc:tests ;
 
 nonterminal Expr with pp, rename_Expr, 
-  transform_Expr, rwrule_Expr,    -- passing down single rules
-  transforms_Expr,  rwrule_Root ; -- passing down a list of rules
+  transform_Expr, rwrule_Expr, rwrule_Root,     -- passing down single rules
+  transforms_Expr, rwrules_Expr, rwrules_Root ; -- passing down a list of rules
 nonterminal Root with pp, rename_Root, 
-  transform_Root, rwrule_Expr, 
-  transforms_Root,  rwrule_Root ;
+  transform_Root, rwrule_Expr, rwrule_Root ,
+  transforms_Root, rwrules_Expr, rwrules_Root ;
 
 synthesized attribute pp :: String ;
 synthesized attribute rename_Expr :: Expr ;
@@ -25,18 +25,65 @@ autocopy attribute rwrules_Expr  :: [ RewriteRule_v2<Decorated Expr  Expr> ] ;
 
 terminal Id_t /[a-zA-Z]([a-zA-Z0-9_])*/ ;
 
+-- A rewrite rule that rewrites variables x and a with a suffix.
+abstract production addSuffix_v2
+r::RewriteRule_v2<Decorated Expr Expr> ::= suffix::String
+{ r.match_v2 = pieces.isJust ;
+  r.rewrite_v2 = case pieces of
+                  just(id) -> idRef( terminal(Id_t, id.lexeme ++ "_" ++ suffix,
+                                              id.line, id.column) ) 
+                | _ -> error ("accessing rewrite on failed match")
+                end ;
+  local attribute pieces::Maybe<Id_t> ;
+  pieces = case r.dtree_v2 of 
+             idRef(idx) -> (if (idx.lexeme == "x" || idx.lexeme == "a")
+                               --&& ! null(r.dtree_v2.rwrules_Expr)
+                            then just(idx) else nothing())
+           | _ -> nothing()
+           end ;
+}
+
+synthesized attribute rename1_Expr :: Expr occurs on Expr ;
+synthesized attribute rename2_Expr :: Expr occurs on Expr ;
+synthesized attribute rename3_Expr :: Expr occurs on Expr ;
+synthesized attribute rename4_Expr :: Expr occurs on Expr ;
+
 abstract production idRef
 e::Expr ::= id::Id_t
 { e.pp = id.lexeme ;
-   e.rename_Expr = applyRewriteRule_v2( addSuffix_v2("XX"), e, idRef(id)) ;
+  e.rename_Expr = e.rename1_Expr ;
+
+-- This compiles - but 'e' does not have its inh. attrs.
+  e.rename1_Expr = applyRewriteRule_v2( addSuffix_v2("XX"), 
+                    decorate e with {}, idRef(id)) ;
+
+-- This compiles as well - it seems to convert 'e' to a decorated tree. 
+  e.rename2_Expr = applyRewriteRule_v2( addSuffix_v2("XX"), e, idRef(id)) ;
+
+-- This one fails to compile - but rw1 and addSuffix_v2("XX") have same type.
+-- How is this different from the rename2 case above?
+--  e.rename3_Expr = applyRewriteRule_v2( rw1, e, idRef(id)) ;
+
+  local attribute rw1 :: RewriteRule_v2<Decorated Expr  Expr> ;
+  rw1 = addSuffix_v2("XX") ;
+
+-- This fails, for similar reasons?
+--  e.rename4_Expr = applyRewriteRule_v2( e.rwrule_Expr, e, idRef(id)) ;
+
+  local attribute rw2 :: RewriteRule_v2<Decorated Expr  Expr> ;
+  rw2 = e.rwrule_Expr ;
+
+
+-- No reason to read below here...
+
 
 -- Not OK, but why?  The error reported by Silver complains about the 2nd parameter
 -- but the use above is exactly the same in the second parameter.
-   e.transform_Expr = applyRewriteRule_v2( e.rwrule_Expr, e, idRef(id)) ;
+--   e.transform_Expr = applyRewriteRule_v2( e.rwrule_Expr, e, idRef(id)) ;
 
 -- This one is OK, but why do I have to decorate e?  shouldn't its type be 
 -- Decorated Expr?
---   e.transform_Expr = applyRewriteRule_v2( e.rwrule_Expr, decorate e with {}, idRef(id)) ;
+   e.transform_Expr = applyRewriteRule_v2( e.rwrule_Expr, decorate e with {}, idRef(id)) ;
 
   e.transforms_Expr = idRef(id) ;
 }
@@ -74,20 +121,15 @@ r::Root ::= e::Expr
 }
 
 
--- A rewrite rule that rewrites variables x and a with a suffix.
-abstract production addSuffix_v2
-r::RewriteRule_v2<Decorated Expr Expr> ::= suffix::String
-{ r.match_v2 = pieces.isJust ;
-  r.rewrite_v2 = case pieces of
-                  just(id) -> idRef( terminal(Id_t, id.lexeme ++ "_" ++ suffix,
-                                              id.line, id.column) ) 
-                | _ -> error ("accessing rewrite on failed match")
-                end ;
-  local attribute pieces::Maybe<Id_t> ;
-  pieces = case r.dtree_v2 of 
-             idRef(idx) -> (if idx.lexeme == "x" || idx.lexeme == "a"
-                            then just(idx) else nothing())
-           | _ -> nothing()
-           end ;
-}
 
+function simpleRenameExpr
+Expr ::= de::Decorated Expr
+{ return if ! null(de.rwrules_Expr)
+         then idRef(terminal(Id_t,"NEW"))
+         else new(de) ;
+
+  local attribute isId :: Boolean ;
+  isId = case de of
+           idRef(_) -> true 
+         | _ -> false end ;
+}
