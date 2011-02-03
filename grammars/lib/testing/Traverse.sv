01d99f5bead0d463c5d1f71dd6dc979da856225f
evw <evw@localhost>
2011-02-03 14:52:01 +0000

    Avoid testing the 'generated' directory.  The testing code is slow grinding through all those directories without test files and it will fail if the JVM is not given a sufficiently large stack.  There is a better fix for this.

--- grammars/lib/testing/Traverse.sv
+++ grammars/lib/testing/Traverse.sv
@@ -20,10 +20,28 @@ IOVal<a> ::= startDir::String paths::[String]
                   sortBy ( stringLte, 
                            prependAll ( head(paths), dirContents.iovalue ) ) ) ;
 
- local headIsDir::IOVal<Boolean> = isDirectory( head(paths), ioIn.io );
- local skipIt::IOVal<Boolean> = if headIsDir.iovalue 
-                                then skipDir(head(paths), headIsDir.io)
-                                else ioval(headIsDir.io, false) ;
+ {- Thought links were a bug, they seem not to be.  It maybe the 
+    size of the Java stack that was the problem.
+ local bashValueForTrue::Integer = 0 ;
+ local headIsLink_Bash::IOVal<Integer> 
+   = system("[ -L " ++ head(paths) ++ " ];", ioIn.io ); 
+ local headIsLink::IOVal<Boolean> 
+  = d ioval(headIsLink_Bash.io, headIsLink_Bash.iovalue == bashValueForTrue) ;
+ -}
+ local headIsLink::IOVal<Boolean> = ioval(ioIn.io, false) ;  -- maybe add later.
+
+ local headIsDir::IOVal<Boolean> = isDirectory( head(paths), headIsLink.io );
+
+ local skipIt::IOVal<Boolean> 
+  = if   endsWith("/generated",head(paths))
+    then ioval(headIsLink.io, true) 
+    else
+    if   headIsLink.iovalue 
+    then ioval(headIsLink.io, true) 
+    else
+    if   headIsDir.iovalue 
+    then skipDir(head(paths), headIsDir.io)
+    else ioval(headIsDir.io, false) ;
 
  local dirContents::IOVal< [String] > = listContents ( head(paths), 
                                                        headIsDir.io ) ;
