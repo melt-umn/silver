6ff63fce2fda0e6ef8b2e7b628f92b9605ca9714
Ted Kaminski <tedinski@cs.umn.edu>
2011-02-03 22:56:56 +0000

    Improve the XML library support. (Better AST, bug fixes..., unparsing)

--- grammars/lib/xml/test/XMLTest.sv
+++ grammars/lib/xml/test/XMLTest.sv
@@ -9,6 +9,6 @@ IOVal<Integer> ::= args::[String] ioin::IO
   local attribute pr :: ParseResult<XMLDocument>;
   pr = parseXMLFileAsAST(head(args));
   
-  return ioval( print( hackUnparse(pr), ioin ), 0);
+  return ioval( print(pr.parseTree.xmlUnparse ++ "\n\n", print( hackUnparse(pr) ++ "\n\n", ioin )), 0);
 
 }
