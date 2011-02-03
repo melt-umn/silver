7706984fc33aea8295d38108b963ee0461d01fcc
Ted Kaminski <tedinski@cs.umn.edu>
2011-02-03 22:57:25 +0000

    Fix bugs, adopt the new AST, and generally improve XML support.

--- runtime/java/src/common/xml/Util.java
+++ runtime/java/src/common/xml/Util.java
@@ -31,6 +31,19 @@ public final class Util {
 		// Create the parser, if we haven't already. Why do they create these things so complicated?
 		if(parser == null) {
 			try {
+				dbFactory.setCoalescing(true); // Eliminate CDATA into text nodes...
+				dbFactory.setIgnoringComments(true); // Eliminate comment nodes...
+				//dbFactory.setIgnoringElementContentWhitespace(true); //REQUIRES VALIDATION MODE
+				dbFactory.setValidating(false); // Make sure we're not validating...
+				
+				// I.. what...
+				// Whoever is responsible for this being necessary SHOULD BE SHOT IN THE FACE
+				// Apparently the w3c gets GBPS eaten up by this buggy crap!
+				dbFactory.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+				// No, no I don't want my language tools to stop, and go download a dtd
+				// from the damn internet you fools. Especially when I ALREADY TOLD YOU
+				// NOT TO VALIDATE!
+				
 				parser = dbFactory.newDocumentBuilder();
 			} catch(ParserConfigurationException pce) {
 				throw new SilverInternalError("Unexpected error initializing XML parser", pce);
@@ -73,10 +86,10 @@ public final class Util {
 	
 	private static final lib.xml.ast.NXMLNodeList parseXMLNodes(final NodeList nl) {
 		
-		common.ConsCell l = common.ConsCell.nil; // < lib.xml.NXMLNode >
+		lib.xml.ast.NXMLNodeList l = new lib.xml.ast.PxmlNodeListNil();
 		
-		for(int i = 0; i < nl.getLength(); i ++ ) {
-			Node n = nl.item(i);
+		for(int i = nl.getLength() - 1; i >= 0; i -- ) {
+			final Node n = nl.item(i);
 			
 			switch(n.getNodeType()) {
 			case Node.TEXT_NODE:
@@ -85,43 +98,42 @@ public final class Util {
 				common.StringCatter val = new common.StringCatter(n.getNodeValue());
 				
 				// Add this element to the list
-				l = new common.ConsCell( new lib.xml.ast.PxmlTextBuilder(val), l);
+				l = new lib.xml.ast.PxmlNodeListCons( new lib.xml.ast.PxmlNodeText(val), l);
 				break;
 			case Node.ELEMENT_NODE:
+			case Node.ENTITY_NODE: // treating the same as an element for now!
 				// Convert the name
 				common.StringCatter name = new common.StringCatter(n.getNodeName());
 				// Convert the attributes
-				lib.xml.ast.NXMLAttributeList attrs = parseElementAttributes(n.getAttributes());
+				common.ConsCell/*lib.xml.ast.NXMLAttribute*/ attrs = parseElementAttributes(n.getAttributes());
 				// Convert the children
 				lib.xml.ast.NXMLNodeList children = parseXMLNodes(n.getChildNodes());
 				
-				// Create the Element
-				lib.xml.ast.NXMLElement elem = new lib.xml.ast.PxmlElement(name, attrs, children);
-				
 				// Create the Node
-				l = new common.ConsCell( new lib.xml.ast.PxmlNodeElement(elem), l);
+				l = new lib.xml.ast.PxmlNodeListCons( new lib.xml.ast.PxmlNodeElement(name, attrs, children), l);
 				break;
 			case Node.COMMENT_NODE:
+			case Node.DOCUMENT_TYPE_NODE: // for now: wtf?
 				break; // ignoring comments!
 			default:
-				throw new SilverError("Unexpected type of XML node! " + n.getNodeName() + n.toString());
+				throw new SilverError("Unexpected type of XML node! " + n.getNodeType() + n.getNodeName() + n.toString());
 			}
 			
 		}
-		return new lib.xml.ast.PxmlNodeListBuilder(l);
+		return l;
 	}
 	
-	private static final lib.xml.ast.NXMLAttributeList parseElementAttributes(final NamedNodeMap al) {
+	private static final common.ConsCell/*lib.xml.ast.NXMLAttribute*/ parseElementAttributes(final NamedNodeMap al) {
 		
-		common.ConsCell l = common.ConsCell.nil; // < lib.xml.NXMLAttribute >
+		common.ConsCell/*lib.xml.ast.NXMLAttribute*/ l = common.ConsCell.nil;
 		
 		for(int i = 0; i < al.getLength(); i++ ) {
-			Node n = al.item(i);
-			common.StringCatter key = new common.StringCatter(n.getNodeName());
-			common.StringCatter value = new common.StringCatter(n.getNodeValue());
+			final Node n = al.item(i);
+			final common.StringCatter key = new common.StringCatter(n.getNodeName());
+			final common.StringCatter value = new common.StringCatter(n.getNodeValue());
 			l = new common.ConsCell( new lib.xml.ast.PxmlAttribute(key, value), l);
 		}
 		
-		return new lib.xml.ast.PxmlAttributeListBuilder(l);
+		return l;
 	}
 }
