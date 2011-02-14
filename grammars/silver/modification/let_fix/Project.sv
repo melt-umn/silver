db872a8027b61745ac4eb8c982f005f36552a5fe
ted <ted@localhost>
2011-02-14 03:01:06 +0000

    Separate out lexical locals (lets) from local attributes.

--- grammars/silver/modification/let_fix/Project.sv
+++ grammars/silver/modification/let_fix/Project.sv
@@ -1,4 +1,10 @@
 grammar silver:modification:let_fix;
 
+imports silver:definition:core;
+imports silver:definition:env;
+imports silver:definition:type;
+imports silver:definition:type:syntax;
+imports silver:analysis:typechecking:core;
+
 build silver:modification:let_fix:java with silver:translation:java;
 
