5a2cb9bd2a96c8e39a5a114cd8b9b77bd08c3f51
ted <ted@localhost>
2011-02-24 23:05:14 +0000

    add some new xpath query stuff

--- grammars/lib/xml/test/XMLTest.sv
+++ grammars/lib/xml/test/XMLTest.sv
@@ -1,6 +1,7 @@
 grammar lib:xml:test;
 
 import lib:xml;
+import lib:xml:foreigntypes;
 
 function main
 IOVal<Integer> ::= args::[String] ioin::IO
@@ -9,6 +10,15 @@ IOVal<Integer> ::= args::[String] ioin::IO
   local attribute pr :: ParseResult<XMLDocument>;
   pr = parseXMLFileAsAST(head(args));
   
-  return ioval( print(pr.parseTree.xmlUnparse ++ "\n\n", print( hackUnparse(pr) ++ "\n\n", ioin )), 0);
+  local attribute io1:: IO;
+  io1 = print(pr.parseTree.xmlUnparse ++ "\n\n", print( hackUnparse(pr) ++ "\n\n", ioin ));
+  
+  local attribute doc :: XML_Document;
+  doc = parseXMLFileAsForeignType("books.xml").parseTree;
+  
+  local attribute io2 :: IO;
+  io2 = print( hackUnparse(nodeSetXPathQuery("//book[author=\"Neal Stephenson\"]", doc)) ++ "\n\n", io1);
+  
+  return ioval( io2, 0);
 
 }
