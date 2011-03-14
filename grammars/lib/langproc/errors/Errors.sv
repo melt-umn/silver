7c4d00d84fb9eb4707c8bc12e80e48a2d55672d1
Eric Van Wyk <evw@cs.umn.edu>
2011-03-14 19:01:07 +0000

    Experimenting with warnings in the errors lib

--- grammars/lib/langproc/errors/Errors.sv
+++ grammars/lib/langproc/errors/Errors.sv
@@ -8,10 +8,34 @@ e::Error ::= m::String
 { e.msg = m ;
 }
 
+abstract production mkWarning
+e::Error ::= m::String
+{ e.msg = m ;
+}
+
 function showErrors
 String ::= errs::[Error]
-{
- return if null(errs)
-        then ""
-        else head(errs).msg ++ "\n" ++ showErrors(tail(errs)) ;
+{ return if null(errs)
+         then ""
+         else head(errs).msg ++ "\n" ++ showErrors(tail(errs)) ;
+}
+
+function getErrors
+[Error] ::= errs::[Error]
+{ return if null(errs)
+         then [ ] 
+         else case head(errs) of
+                mkError(_) -> [ head(errs) ]
+              | _ -> [ ] end
+              ++ getErrors(tail(errs)) ;
+}
+
+function getWarnings
+[Error] ::= errs::[Error]
+{ return if null(errs)
+         then [ ] 
+         else case head(errs) of
+                mkWarning(_) -> [ head(errs) ]
+              | _ -> [ ] end
+              ++ getWarnings(tail(errs)) ;
 }
