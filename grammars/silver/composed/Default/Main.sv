47432c845d278df8d2bada862b727196ed7907e8
ted <ted@localhost>
2011-06-28 06:05:02 +0000

    more dead code elimination in pattern matching code

--- grammars/silver/composed/Default/Main.sv
+++ grammars/silver/composed/Default/Main.sv
@@ -25,7 +25,6 @@ parser rParse::Root {
   silver:modification:let_fix;
   silver:modification:collection;
   silver:modification:patternmatching;
-  silver:modification:patternmatching:copper;
   silver:modification:autocopyattr;
   silver:modification:autocopyattr:convenience; -- TODO: we need a condition export, perhaps? Why didn't we include this?
   silver:modification:ffi;
