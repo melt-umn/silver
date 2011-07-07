957e3b44cc081c159c7ff01a00fdd7c08f4c9257
Ted Kaminski <tedinski@cs.umn.edu>
2011-07-07 09:40:54 +0000

    Removing RealThunk, replacing with Thunk (which has a ever so slightly better interface)

--- runtime/java/src/common/Closure.java
+++ runtime/java/src/common/Closure.java
@@ -2,10 +2,11 @@ package common;
 
 /**
  * Closure is used to (lazily) pass arguments to functions/productions.
- * These aren't quite Thunks, since we don't remember the result. 
+ * These aren't quite {@link Thunk}s, since we don't remember the result (here). 
  * 
  * @author tedinski
- * @see RealThunk
+ * @see Thunk
+ * @see FromLazy
  */
 public abstract class Closure {
 	protected final DecoratedNode context;
@@ -14,5 +15,31 @@ public abstract class Closure {
 		context = con;
 	}
 	
+	/**
+	 * Overriden by the generated code to implement the evaluation behavior, using the member variable
+	 * {@link #context}.  This closure is not parameterized (i.e. it's a "fully-applied" function)
+	 * 
+	 * @return The result of evaluating this closure
+	 */
 	public abstract Object eval();
+	
+	/**
+	 * For the case where we must construct a closure from a {@link Lazy}. Should be used as sparingly as
+	 * possible! (Leads to a proliferation of small allocations, if used too much.)
+	 * 
+	 * @author tedinski
+	 * @see Closure
+	 * @see Thunk
+	 */
+	public static class FromLazy extends Closure {
+		protected final Lazy fp;
+		protected FromLazy(DecoratedNode con, Lazy l) {
+			super(con);
+			fp = l;
+		}
+		@Override
+		public Object eval() {
+			return fp.eval(context);
+		}
+	}
 }
