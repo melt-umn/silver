c8b076dcaf9515879fa3b49e71800181a476c3f5
Ted Kaminski <tedinski@cs.umn.edu>
2011-07-07 09:46:08 +0000

    Update the code to emit Thunk rather than RealThunk, which I have removed for a better interface

--- grammars/silver/translation/java/core/GlobalDcl.sv
+++ grammars/silver/translation/java/core/GlobalDcl.sv
@@ -3,5 +3,5 @@ grammar silver:translation:java:core;
 aspect production globalValueDclConcrete
 top::AGDcl ::= 'global' id::Name '::' t::Type '=' e::Expr ';'
 {
-  top.initValues := "\tpublic static final common.RealThunk " ++ id.name ++ " = new common.RealThunk(common.TopNode.singleton, " ++ wrapLazy(e) ++ ");\n";
+  top.initValues := "\tpublic static final common.Thunk " ++ id.name ++ " = new common.Thunk(" ++ wrapThunkText("common.TopNode.singleton", e.translation) ++ ");\n";
 }
