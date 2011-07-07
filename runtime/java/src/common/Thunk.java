3855d14aa582dc9344bf02d7de53ddc48592268c
ted <ted@localhost>
2011-07-07 09:40:54 +0000

    Removing RealThunk, replacing with Thunk (which has a ever so slightly better interface)

---
+++ runtime/java/src/common/Thunk.java
@@ -0,0 +1,32 @@
+package common;
+
+/**
+ * Wrapper around a closure, to ensure it is not evaluated more than once.
+ * 
+ * @author tedinski
+ * @see Closure
+ */
+public class Thunk {
+	private Object data;
+	
+	/**
+	 * Construct a (real) thunk, from either just a value, or a closure.
+	 * 
+	 * @param cls {@link Closure} or other value
+	 */
+	public Thunk(final Object cls) {
+		data = cls;
+	}
+	
+	/**
+	 * Either evaluates the closure to obtain a value, or returns the already evaluated result.
+	 * 
+	 * @return The value wrapped by this thunk.
+	 */
+	public Object eval() {
+		if(data instanceof Closure) {
+			data = ((Closure)data).eval();
+		}
+		return data;
+	}
+}
