60ed36e2c3f6f8197faeafa51f33ecdf06824d5d
stousig <stousig@localhost>
2011-07-25 22:14:26 +0000

    removed xrobots from the wrong silver directory

--- grammars/xrobots/host/Main.sv
+++
@@ -1,8 +0,0 @@
-grammar xrobots:host ;
-
---exports tutorials:xrobots:terminals ;
---exports tutorials:xrobots:concretesyntax ;
---exports tutorials:xrobots:abstractsyntax ;
-
-import xrobots:concretesyntax only Root_c ;
-
