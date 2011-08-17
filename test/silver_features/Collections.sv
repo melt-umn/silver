3a756e3125855d8ff08bc40074eb94a49e378e4d
ted <ted@localhost>
2011-08-17 23:16:43 +0000

    Introduce || and && to collections. Also, fix function/productions that are polymorphic being used as operators.

--- test/silver_features/Collections.sv
+++ test/silver_features/Collections.sv
@@ -1,4 +1,6 @@
 
+-- Basic collection tests
+--------------------------------------------------------------------------------
 
 synthesized attribute colSyn::String with ++;
 inherited attribute colInh::String with ++;
@@ -60,6 +62,8 @@ top::ColNT ::=
 equalityTest ( colFwdNode(colProdLeaf()).colSyn, " j  k  j  k  b  q ", String, silver_tests ) ;
 equalityTest ( colNode(colProdLeaf(), colLeaf()).colSyn, " j  k ( d  e ) b ", String, silver_tests ) ;
 
+-- Tests involving collection/noncollection assignments
+--------------------------------------------------------------------------------
 
 wrongCode "is a collection attribute" {
   aspect production colNode
@@ -86,4 +90,82 @@ wrongCode "can only be used for collections" {
   top::ColNT ::= c1::ColNT c2::ColNT
   { top.colNotCol <- ""; }
 }
+
+-- Different types of collection operators
+--------------------------------------------------------------------------------
+
+-- Note: we've already tested strings
+
+synthesized attribute colList :: [String] with ++;
+synthesized attribute colOr :: Boolean with ||;
+synthesized attribute colAnd :: Boolean with &&;
+synthesized attribute colFun :: Maybe<Integer> with orElse;
+synthesized attribute colProd :: ColNT with colNode;
+
+attribute colList, colOr, colAnd, colFun, colProd occurs on ColNT;
+
+abstract production colTest1
+top::ColNT ::=
+{
+  top.colList := ["one"];
+  top.colList <- ["two"];
+  top.colOr := false;
+  top.colOr <- true;
+  top.colAnd := true;
+  top.colAnd <- false;
+  top.colFun := nothing();
+  top.colFun <- just(1);
+  top.colProd := colLeaf();
+  top.colProd <- colLeaf();
+}
+
+equalityTest( colTest1().colList, ["one", "two"], [String], silver_tests );
+equalityTest( colTest1().colOr, true, Boolean, silver_tests );
+equalityTest( colTest1().colAnd, false, Boolean, silver_tests );
+equalityTest( colTest1().colFun.isJust, true, Boolean, silver_tests );
+equalityTest( fromMaybe(2, colTest1().colFun), 1, Integer, silver_tests );
+equalityTest( colTest1().colProd.colSyn, "( a  c )( d  e ) b ", String, silver_tests );
+
+abstract production colTest2
+top::ColNT ::=
+{
+  top.colList := ["one", "two"];
+  top.colList <- [];
+  top.colOr := true;
+  top.colOr <- false;
+  top.colAnd := true;
+  top.colAnd <- true;
+  top.colFun := nothing();
+  top.colFun <- nothing();
+  top.colProd := colProdLeaf();
+  top.colProd <- colLeaf();
+}
+
+equalityTest( colTest2().colList, ["one", "two"], [String], silver_tests );
+equalityTest( colTest2().colOr, true, Boolean, silver_tests );
+equalityTest( colTest2().colAnd, true, Boolean, silver_tests );
+equalityTest( colTest2().colFun.isJust, false, Boolean, silver_tests );
+equalityTest( colTest2().colProd.colSyn, " j  k ( d  e ) b ", String, silver_tests );
+
+abstract production colTest3
+top::ColNT ::=
+{
+  top.colList := [];
+  top.colList <- ["one", "two"];
+  top.colOr := false;
+  top.colOr <- false;
+  top.colAnd := false;
+  top.colAnd <- true;
+  top.colFun := just(1);
+  top.colFun <- just(2);
+  top.colProd := colLeaf();
+  top.colProd <- colProdLeaf();
+}
+
+equalityTest( colTest3().colList, ["one", "two"], [String], silver_tests );
+equalityTest( colTest3().colOr, false, Boolean, silver_tests );
+equalityTest( colTest3().colAnd, false, Boolean, silver_tests );
+equalityTest( colTest3().colFun.isJust, true, Boolean, silver_tests );
+equalityTest( fromMaybe(4, colTest3().colFun), 1, Integer, silver_tests );
+equalityTest( colTest3().colProd.colSyn, "( a  c ) j  k  b ", String, silver_tests );
 
