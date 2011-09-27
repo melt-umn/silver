116c7f049a8a21cce59e199ca421a4ba6001b0c6
Ted Kaminski <tedinski@cs.umn.edu>
2011-09-27 04:04:31 +0000

    Reorganize some of the Silver runtime.

---
+++ runtime/java/src/common/rawlib/RawTreeMap.java
@@ -0,0 +1,37 @@
+package common.rawlib;
+
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+import common.ConsCell;
+import common.NodeFactory;
+import common.javainterop.ConsCellCollection;
+import common.javainterop.SilverComparator;
+
+public final class RawTreeMap {
+	public static TreeMap<Object,ConsCell> empty(NodeFactory<Integer> cmp) {
+		return new TreeMap<Object,ConsCell>(new SilverComparator<Object>(cmp));
+	}
+	public static TreeMap<Object, ConsCell> addList(ConsCell l, TreeMap<Object, ConsCell> t) {
+		TreeMap<Object,ConsCell> ret = (TreeMap<Object,ConsCell>)t.clone();
+		for(core.NPair elem : new ConsCellCollection<core.NPair>(l)) {
+			assert(elem instanceof core.Ppair); // document as an assert why not
+			ConsCell existing = lookup(elem.getChild(0), ret);
+			ret.put(elem.getChild(0), new ConsCell(elem.getChild(1), existing));
+		}
+		return ret;
+	}
+	public static ConsCell toList(TreeMap<Object,ConsCell> t) {
+		ConsCell ret = ConsCell.nil;
+		for(Object key : t.descendingKeySet()) {
+			for(Object value : new ConsCellCollection<Object>(t.get(key))) {
+				ret = new ConsCell(new core.Ppair(key, value), ret);
+			}
+		}
+		return ret;
+	}
+	public static ConsCell lookup(Object k, TreeMap<Object,ConsCell> t) {
+		Object r = t.get(k);
+		return r == null ? ConsCell.nil : (ConsCell) r;
+	}
+}
