17dd3a2834a696b277da08ea6d67a883f31e651f
Ted Kaminski <tedinski@cs.umn.edu>
2011-11-03 01:49:25 +0000

    Add some test for type decls.

--- test/silver_features/Types.sv
+++ test/silver_features/Types.sv
@@ -1,5 +1,5 @@
 
-
+------------------------------------- Number of parameters to type constructors
 terminal ATerminalType 'doesnotmatter';
 
 wrongCode "ATerminalType has 0 type variables" {
@@ -28,5 +28,29 @@ wrongCode "NTZero' is already bound" {
 
 --nonterminal IO; -- parse error
 
+
+-------------------------------------- Type Decls
+
+type MyType<a> = String;
+
+global astr1 :: MyType<Integer> = "hi";
+global astr2 :: MyType<String> = "yo";
+global astr4 :: MyType<Integer> = astr2;
+
+type MyType2 = Integer;
+
+global anum1 :: MyType2 = 2;
+global astr3 :: MyType<MyType2> = toString(anum1);
+
+wrongCode "MyType has 1 type variables, but there are 0 supplied here." {
+ global t :: MyType = error("");
+}
+wrongCode "MyType has 1 type variables, but there are 2 supplied here" {
+ global t :: MyType<Integer IntegeR> = error("");
+}
+-- For the moment, errors ignore type names
+wrongCode "Operands to == must be the same type. Instead they are String and Integer" {
+ global t :: Boolean = astr1 == anum1;
+}
 
 
