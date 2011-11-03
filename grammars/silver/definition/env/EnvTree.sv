b985e98cb62f1b2c542a978c25682988de889b23
ted <ted@localhost>
2011-11-03 21:18:40 +0000

    Let's try out rawtreemap.

--- grammars/silver/definition/env/EnvTree.sv
+++ grammars/silver/definition/env/EnvTree.sv
@@ -4,91 +4,59 @@ grammar silver:definition:env;
 -- buildTree       EnvTree ::= eis::[Decorated EnvItem]
 -- collapseEnvTree [Decorated DclInfo] ::= et::EnvTree
 
+import silver:util:raw:treemap as rtm;
+--import silver:util:fixedmap as fm;
 
-nonterminal EnvTree with searchKey, searchResult, treeCollapse;
-
-autocopy attribute searchKey :: String;
-synthesized attribute searchResult :: [Decorated DclInfo];
-synthesized attribute treeCollapse :: [Decorated DclInfo];
-
-abstract production emptyEnvTree
-top::EnvTree ::= 
-{
-  top.searchResult = [];
-  top.treeCollapse = [];
-}
-
-abstract production envTree
-top::EnvTree ::= iname::String eis::[Decorated DclInfo] l::EnvTree r::EnvTree
-{
-  top.searchResult = if top.searchKey <= iname
-                     then if top.searchKey == iname
-                          then eis
-                          else l.searchResult
-                     else r.searchResult;
-  top.treeCollapse = l.treeCollapse ++ (if iname == head(eis).fullName then eis else []) ++ r.treeCollapse;
-}
+type EnvTree = rtm:Map<String Decorated DclInfo>;
 
 function searchEnvTree
 [Decorated DclInfo] ::= search::String et::EnvTree
 {
-  et.searchKey = search;
-  return et.searchResult;
+  return rtm:lookup(search, et);
 }
 
 function collapseEnvTree
 [Decorated DclInfo] ::= et::EnvTree
 {
-  return et.treeCollapse;
+  -- preserve only full name env items.
+  return map(myGetSnd, filter(isSameKey, rtm:toList(et)));
 }
-
--- Function for building EnvTree from list of Decorated EnvItems
+function myGetSnd
+b ::= p::Pair<a b>
+{ return p.snd; }
 
 function buildTree
 EnvTree ::= eis::[Decorated EnvItem]
 {
-  return buildTreeFromCollected(groupEnvItems(sortEnvItems(explodeEnvItems(eis)))) ;
+  return directBuildTree( explodeEnvItems(eis) );
 }
+
+function directBuildTree
+EnvTree ::= eis::[Pair<String Decorated DclInfo>]
+{
+  return rtm:add(eis, rtm:empty(compareString));
+--  return fm:create( eis );
+}
+
+
+-- Utility functions
+
+function isSameKey
+Boolean ::= p::Pair<String Decorated DclInfo>
+{ return p.fst == p.snd.fullName; }
 
 -- Take (shortName, fullName) and turns it into [(shortName, fullName), (fullName, fullName)]
 -- So lookups see both.
 function explodeEnvItems
-[Decorated EnvItem] ::= eis::[Decorated EnvItem]
+[Pair<String Decorated DclInfo>] ::= eis::[Decorated EnvItem]
 {
   local attribute h :: Decorated EnvItem;
   h = head(eis);
   
   return if null(eis) then [] else
-         if h.itemName == h.dcl.fullName
-         then h :: explodeEnvItems(tail(eis))
-         else h :: (renamedEnvItem(h.dcl.fullName, h.dcl) :: explodeEnvItems(tail(eis)));
-}
-
-function buildTreeFromCollected
-EnvTree ::= collected::[[Decorated EnvItem]]
-{
-  return buildTreeFromCollectedHelp(collected, length(collected));
-}
-
-function buildTreeFromCollectedHelp
-EnvTree ::= collected::[[Decorated EnvItem]] upTo::Integer
-{
-  return  if upTo == 0 then emptyEnvTree()
-          else if upTo == 1
-          then envTree(head(head(collected)).itemName, mapGetDcls(head(collected)), emptyEnvTree(), emptyEnvTree())
-          else envTree(head(head(right_list)).itemName, mapGetDcls(head(right_list)), ltree, rtree);
-
-  local attribute ltree :: EnvTree;
-  ltree = buildTreeFromCollectedHelp(collected, middle);
-
-  local attribute rtree :: EnvTree;
-  rtree = buildTreeFromCollectedHelp(tail(right_list), upTo - middle - 1);
-
-  -- this in fact includes the current as well as the right side.
-  local attribute right_list :: [[Decorated EnvItem]];
-  right_list = drop(middle,collected);
- 
-  local attribute middle :: Integer;
-  middle = toInt(toFloat(upTo) / 2.0);
+         pair(h.dcl.fullName, h.dcl) ::
+           (if h.itemName == h.dcl.fullName
+            then explodeEnvItems(tail(eis))
+            else pair(h.itemName, h.dcl) :: explodeEnvItems(tail(eis)));
 }
 
