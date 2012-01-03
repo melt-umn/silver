c6cd159ac1b73b836762ea4dcb0825ea701f6e0a
Kevin Williams <kwill@cs.umn.edu>
2012-01-03 16:54:27 -0600

    Updating types in cst for lambda

--- tutorials/lambda/ConcreteSyntax.sv
+++ tutorials/lambda/ConcreteSyntax.sv
@@ -7,7 +7,8 @@ synthesized attribute ast_Root::Root;
 nonterminal Root_c with pp,ast_Root;
 
 synthesized attribute ast_Type::Type;
-nonterminal Type_c with pp,ast_Type;
+nonterminal TypeList_c with pp,ast_Type;
+nonterminal TypeItem_c with pp,ast_Type;
 
 synthesized attribute ast_Expr::Expr;
 nonterminal Expr_c with pp,ast_Expr;
@@ -31,9 +32,9 @@ concrete productions r::Root_c
 concrete productions e::Expr_c
  | e1::Expr_funct_c { e.pp = e1.pp;
                       e.ast_Expr = expr_expr_f(e1.ast_Expr_funct); }
- | 'let' ic::Id_t ':' ty::Type_c '=' e1::Expr_c 'in' e2::Expr_c
+ | 'let' ic::Id_t ':' ty::TypeList_c '=' e1::Expr_c 'in' e2::Expr_c
                     { e.pp = concat([text("let"), text(ic.lexeme), text(":"), ty.pp, text("="), e1.pp, text("in"), e2.pp]); }
- | 'lambda' i::Id_t ':' ty::Type_c '.' e1::Expr_c
+ | 'lambda' i::Id_t ':' ty::TypeList_c '.' e1::Expr_c
                     { e.pp = concat([text("lambda"), text(i.lexeme), text(":"), ty.pp, text("."), e1.pp]); }
 
 concrete productions e::Expr_funct_c
@@ -61,11 +62,29 @@ concrete productions e::Factor_c
  | i::IntLit_t { e.pp = text(i.lexeme);
                  e.ast_Factor = factor_int(i.lexeme); }
 
+-- Types
+concrete productions tl::TypeList_c
+ | ti::TypeItem_c '->' tl1::TypeList_c { tl.pp = concat([ti.pp, text("->"), tl1.pp]);
+                                         tl.ast_Type = arrow(ti.ast_Type,tl1.ast_Type); }
+ | ti::TypeItem_c { tl.pp = ti.pp;
+                    tl.ast_Type = ti.ast_Type; }
+
+concrete productions ti::TypeItem_c
+ | 'int' { ti.pp = text("int");
+           ti.ast_Type = int(); }
+ | '(' tl::TypeList_c ')' { ti.pp = parens(tl.pp);
+                            ti.ast_Type = tl.ast_Type; }
+{-
 concrete productions ty::Type_c
+ | t1::Type_c '->' t2::Type_c { ty.pp = concat([t1.pp, text("->"), t2.pp]);
+                                ty.ast_Type = arrow(t1,t2); }
+ | 'int' { ty.pp = text("int");
+           ty.ast_Type = int(); }
+
  | 'int' '->' t::Type_c { ty.pp = concat([text("int"), text("->"), t.pp]); }
  | i::Integer_t         { ty.pp = text("int"); }
  | '(' t::Type_c ')' '->' 'int' { ty.pp = cat(parens(t.pp), text("->int")); }
  | '(' t::Type_c ')'            { ty.pp = parens(t.pp); }
-
+-}
 
 
