5b615969bb79f8a227dddcfede130a3ca366d864
Eric Van Wyk <evw@cs.umn.edu>
2012-01-16 18:34:32 -0600

    fixing the rest of issue #17 for even better type checking on equalityTest

--- test/silver_features/EqualityTest.sv
+++ test/silver_features/EqualityTest.sv
@@ -7,3 +7,15 @@ wrongCode "Type of first and second experssions in equalityTest do not match." {
 equalityTest ( intTestProd(0), 0, Integer, silver_tests ) ;
 
 }
+
+wrongCode "Type of initial expression does not match specified type (3rd argument)" {
+
+equalityTest ( 3.1415, 1, Integer, silver_tests ) ;
+
+}
+
+wrongCode "Type of second expression does not match specified type (3rd argument)" {
+
+equalityTest ( 3, 1.0, Integer, silver_tests ) ;
+
+}
