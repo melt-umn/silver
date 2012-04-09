01f0f91a546f2750ac98d4164d4d2ae2eac0d3ab
Ted Kaminski <tedinski@cs.umn.edu>
2012-04-09 14:18:02 -0500

    Refactor the copper AST. Also parameterize the grammar name, rather than use a constant.

--- grammars/silver/definition/concrete_syntax/ast/CstAst.sv
+++ grammars/silver/definition/concrete_syntax/ast/CstAst.sv
@@ -15,28 +15,26 @@ nonterminal SyntaxRoot with cstErrors, cstNormal, xmlCopper, {-TODO:debugging-}u
 synthesized attribute cstNormal :: SyntaxRoot; -- TODO basically just a debugging thing
 synthesized attribute xmlCopper :: String;
 
-{--
- - We need to give a name to the "grammar" all the syntax information goes
- - into on Copper's side. For now, just call it host, because we're not
- - taking advantage of multiple Copper grammars.
- -}
-global copperGrammarId :: String = "host";
-
 abstract production cstRoot
 top::SyntaxRoot ::= parsername::String  startnt::String  s::Syntax
 {
   s.cstEnv = directBuildTree(s.cstDcls);
   s.cstNTProds = directBuildTree(s.cstProds);
+  s.containingGrammar = "host";
   
   -- Move productions under their nonterminal, and sort the declarations
   local attribute s2 :: Syntax;
   s2 = foldr(consSyntax, nilSyntax(), sortBy(syntaxDclLte, s.cstNormalize));
   s2.cstEnv = directBuildTree(s.cstDcls);
+  s2.containingGrammar = "host";
   
   -- This should be on s1, because the s2 transform assumes everything is well formed.
   -- In particular, it drops productions it can't find an NT for.
   top.cstErrors := s.cstErrors;
   top.cstNormal = cstRoot(parsername, startnt, s2);
+  
+  local startFound :: [Decorated SyntaxDcl] = searchEnvTree(startnt, s2.cstEnv);
+  -- TODO check if this is found!!
 
   local attribute univLayout :: String;
   univLayout = implode("", map(xmlCopperRef, s2.allIgnoreTerminals));
@@ -48,8 +46,8 @@ top::SyntaxRoot ::= parsername::String  startnt::String  s::Syntax
 "<CopperSpec xmlns=\"http://melt.cs.umn.edu/copper/xmlns\">\n" ++
 "  <Parser id=\"" ++ makeCopperName(parsername) ++ "\" isUnitary=\"true\">\n" ++
 "    <PP>" ++ parsername ++ "</PP>\n" ++
-"    <Grammars><GrammarRef id=\"" ++ copperGrammarId ++ "\"/></Grammars>\n" ++
-"    <StartSymbol>" ++ xmlCopperNontermRef(startnt) ++ "</StartSymbol>\n" ++
+"    <Grammars><GrammarRef id=\"" ++ s2.containingGrammar ++ "\"/></Grammars>\n" ++
+"    <StartSymbol>" ++ xmlCopperRef(head(startFound)) ++ "</StartSymbol>\n" ++
 -- The layout before and after the root nonterminal. For now, universal layout.
 "    <StartLayout>" ++ univLayout ++ "</StartLayout>\n" ++
 -- TODO fix: ?
@@ -71,8 +69,8 @@ top::SyntaxRoot ::= parsername::String  startnt::String  s::Syntax
 --"    <SemanticActionAuxiliaryCode><Code><![CDATA[  ]]></Code></SemanticActionAuxiliaryCode>\n" ++
 "  </Parser>\n\n" ++
 
-"  <Grammar id=\"" ++ copperGrammarId ++ "\">\n\n" ++
-"    <PP>" ++ copperGrammarId ++ "</PP>\n\n" ++
+"  <Grammar id=\"" ++ s2.containingGrammar ++ "\">\n\n" ++
+"    <PP>" ++ s2.containingGrammar ++ "</PP>\n\n" ++
 -- Default layout for production, unless otherwise specified.
 "    <Layout>" ++ univLayout ++ "</Layout>\n\n" ++
 "    <Declarations>\n" ++
