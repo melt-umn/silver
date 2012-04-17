a65368a9a35bddf866e0eb746988febf2ff5c522
Ted Kaminski <tedinski@cs.umn.edu>
2012-04-17 17:30:42 -0500

    Add a missing conditional export

--- grammars/silver/modification/ffi/Project.sv
+++ grammars/silver/modification/ffi/Project.sv
@@ -2,3 +2,5 @@ grammar silver:modification:ffi;
 
 exports silver:modification:ffi:java with silver:translation:java:core;
 
+exports silver:modification:ffi:java with silver:translation:java:type;
+
