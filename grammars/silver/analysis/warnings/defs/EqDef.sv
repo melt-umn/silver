272a9a7c36f4e64ac534623dd3694b527145f618
Ted Kaminski <tedinski@cs.umn.edu>
2012-10-17 13:21:25 -0500

    Fix a bug with inherited attributes to locals and the orphaned equations check. Plus misc.

--- grammars/silver/analysis/warnings/defs/EqDef.sv
+++ grammars/silver/analysis/warnings/defs/EqDef.sv
@@ -33,16 +33,24 @@ top::ProductionStmt ::= dl::DefLHS '.' attr::Decorated QName '=' e::Expr
   local attribute prodDefGram :: String;
   prodDefGram = substring(0, lastIndexOf(":", top.signature.fullName), top.signature.fullName);
 
+  -- Some facts to help understand the code.
+  -- Synthesized attribute are always being defined on the LHS of a production.
+  --  (i.e. no concerns about functions or local attributes or etc)
+  -- Synthesized attributes MAY be contributions in a collection (<-)
+  --  (we do not care about these! They only matter w.r.t flow types.)
+
+  -- Synthesized attributes MAY appear in "default" productions.
+  
+  local exportedBy :: [String] = 
+    if top.blockContext.hasPartialSignature
+    then [prodDefGram, occursCheck.dcl.sourceGrammar]
+    else [occursCheck.dcl.sourceGrammar]; -- defaults can only be listed together with occurs.
+
   top.errors <-
     if null(occursCheck.errors ++ attr.lookupAttribute.errors)
     && (top.config.warnAll || top.config.warnEqdef)
     && $4.lexeme != "<-" -- hack to omit collections
-    && !contains(top.grammarName,
-         computeDependencies(
-           if top.blockContext.hasPartialSignature -- is not a default production
-           then [prodDefGram, occursCheck.dcl.sourceGrammar]
-           else [occursCheck.dcl.sourceGrammar],
-           top.compiledGrammars))
+    && !contains(top.grammarName, computeDependencies(exportedBy, top.compiledGrammars))
     then [wrn(top.location, "Orphaned equation: " ++ attr.pp ++ " (occurs from " ++ occursCheck.dcl.sourceGrammar ++ ") on " ++ top.signature.fullName)]
     -- Now, check for duplicate equations!
     else if top.blockContext.hasPartialSignature
@@ -60,17 +68,26 @@ top::ProductionStmt ::= dl::DefLHS '.' attr::Decorated QName '=' e::Expr
   local attribute prodDefGram :: String;
   prodDefGram = substring(0, lastIndexOf(":", top.signature.fullName), top.signature.fullName);
 
+  -- Some facts to help understand the code.
+  -- Inherited attributes may be given to RHS, locals, or forwards.
+  -- Inherited equations may NOT appear in default productions.
+  -- Inherited equations MAY be collection contributions!
+  -- Equations can appear in functions or productions.
+
+  local exportedBy :: [String] = 
+    case dl of
+    -- Exported by the declaration of the thing we're giving inh to, or to the occurs
+    | localDefLHS(q) -> [q.lookupValue.dcl.sourceGrammar, occursCheck.dcl.sourceGrammar]
+    -- For rhs or forwards, that's the production.
+    | _ -> [prodDefGram, occursCheck.dcl.sourceGrammar]
+    end;
+
   top.errors <-
-   if null(occursCheck.errors ++ attr.lookupAttribute.errors)
+    if null(occursCheck.errors ++ attr.lookupAttribute.errors ++ dl.errors)
     && (top.config.warnAll || top.config.warnEqdefInh)
     && $4.lexeme != "<-" -- hack to omit collections
-    && !contains(top.grammarName,
-         computeDependencies(
-           if top.blockContext.hasPartialSignature
-           then [prodDefGram, occursCheck.dcl.sourceGrammar]
-           else [occursCheck.dcl.sourceGrammar],
-           top.compiledGrammars))
-    then [wrn(top.location, "Orphaned inherited equation: " ++ attr.pp ++ " (occurs from " ++ occursCheck.dcl.sourceGrammar ++ ") on " ++ top.signature.fullName)]
+    && !contains(top.grammarName, computeDependencies(exportedBy, top.compiledGrammars))
+    then [wrn(top.location, "Orphaned inherited equation: " ++ attr.pp ++ " (occurs from " ++ occursCheck.dcl.sourceGrammar ++ ") in " ++ top.signature.fullName ++ " on " ++ dl.pp)]
     -- Now, check for duplicate equations!
     else case dl of
          | childDefLHS(q) ->
@@ -86,6 +103,7 @@ top::ProductionStmt ::= dl::DefLHS '.' attr::Decorated QName '=' e::Expr
              then [wrn(top.location, "Duplicate equation for " ++ attr.pp ++ " on " ++ q.lookupValue.fullName)]
              else []
          | _ -> [] -- TODO : this isn't quite extensible... more better way eventually, plz
+         -- Probably put a "lookupEq" on DefLHS?
          end;
 
 }
