d58eae5ce665f0307415691da250eeb21b7919fe
Ted Kaminski <tedinski@cs.umn.edu>
2012-11-19 15:37:52 -0600

    De-IO-ize Main and Init class creation.

--- grammars/silver/translation/java/core/RootSpec.sv
+++ grammars/silver/translation/java/core/RootSpec.sv
@@ -2,101 +2,65 @@ grammar silver:translation:java:core;
 
 import silver:driver:util;
 
-attribute javaClasses,setupInh,initProd,initValues,postInit,initWeaving,valueWeaving occurs on RootSpec, GrammarPart, Grammar;
-
-
--- TODO: Should have made javaClasses a collection, too, while I was at it.
--- TODO: also, make it a pair now that we can!
-
-{--
- - Java classes to generate. (filename, contents)
- -}
-synthesized attribute javaClasses :: [[String]];
-{--
- - Early initializers: occurs.add, local's inh attr map creation, decorators.add, collection object creation
- -}
-synthesized attribute setupInh :: String with ++;
-{--
- - Initialize the attributes maps for each production.
- - note to be confused with "production attribute" dcls.
- -}
-synthesized attribute initProd :: String with ++;
-{--
- - Global values.
- -}
-synthesized attribute initValues :: String with ++;
-{--
- - Late initializers. Decorator application (late because we want all attribute equations to be posted first!!)
- -}
-synthesized attribute postInit :: String with ++;
-
-synthesized attribute translation :: String;
-{--
- - Initial values for early weaving. e.g. counter for # attributes on NT
- -}
-synthesized attribute initWeaving :: String with ++;
-{--
- - Values computed by early weaving. e.g. index of attribute in NT arrays
- -}
-synthesized attribute valueWeaving :: String with ++;
+attribute javaClasses occurs on RootSpec;
 
 aspect production interfaceRootSpec
 top::RootSpec ::= _
 {
   top.javaClasses = [];
-  top.setupInh := "";
-  top.initProd := "";
-  top.initValues := "";
-  top.postInit := "";
-  top.initWeaving := "";
-  top.valueWeaving := "";
 }
 
 aspect production grammarRootSpec
-top::RootSpec ::= c1::Grammar  _
+top::RootSpec ::= g::Grammar  _
 {
-  top.javaClasses = c1.javaClasses;
-  top.setupInh := c1.setupInh;
-  top.initProd := c1.initProd;
-  top.initValues := c1.initValues;
-  top.postInit := c1.postInit;
-  top.initWeaving := c1.initWeaving;
-  top.valueWeaving := c1.valueWeaving;
+  top.javaClasses = g.javaClasses ++
+  [["Init",
+"package " ++ makeName(g.declaredName) ++ ";\n\n" ++
+
+"public class Init{\n\n" ++
+
+"\tprivate static boolean preInit = false;\n" ++
+"\tprivate static boolean init = false;\n" ++
+"\tprivate static boolean postInit = false;\n\n" ++
+
+"\tpublic static void initAllStatics(){\n" ++
+"\t\tif(preInit) return;\n" ++
+"\t\tpreInit = true;\n\n" ++
+  makeOthers(top.allGrammarDependencies, "initAllStatics") ++ "\n" ++
+"\t}\n\n" ++
+
+"\tpublic static void init(){\n" ++
+"\t\tif(init) return;\n" ++
+"\t\tinit = true;\n\n" ++
+"\t\tsetupInheritedAttributes();\n\n" ++
+  makeOthers(top.allGrammarDependencies, "init") ++ "\n" ++
+"\t\tinitProductionAttributeDefinitions();\n" ++
+"\t}\n\n" ++
+
+"\tpublic static void postInit(){\n" ++
+"\t\tif(postInit) return;\n" ++
+"\t\tpostInit = true;\n\n" ++
+  makeOthers(top.allGrammarDependencies, "postInit") ++ "\n\n" ++
+  g.postInit ++
+"\t}\n\n" ++
+
+"\tprivate static void setupInheritedAttributes(){\n" ++
+  g.setupInh ++
+"\t}\n\n" ++
+
+"\tprivate static void initProductionAttributeDefinitions(){\n" ++
+  g.initProd ++
+"\t}\n\n" ++
+
+  g.initWeaving ++ 
+  g.valueWeaving ++
+  g.initValues ++
+"}\n"]];
 }
 
-aspect production grammarPart
-top::GrammarPart ::= c1::Root  fn::String
+function makeOthers
+String ::= others::[String] nme::String
 {
-  top.javaClasses = c1.javaClasses;
-  top.setupInh := c1.setupInh;
-  top.initProd := c1.initProd;
-  top.initValues := c1.initValues;
-  top.postInit := c1.postInit;
-  top.initWeaving := c1.initWeaving;
-  top.valueWeaving := c1.valueWeaving;
-}
-
-aspect production nilGrammar
-top::Grammar ::=
-{
-  top.javaClasses = [];
-  top.setupInh := "";
-  top.initProd := "";
-  top.initValues := "";
-  top.postInit := "";
-  top.initWeaving := "";
-  top.valueWeaving := "";
-}
-
-aspect production consGrammar
-top::Grammar ::= c1::GrammarPart  c2::Grammar
-{
-  top.javaClasses = c1.javaClasses ++ c2.javaClasses;
-  top.setupInh := c1.setupInh ++ c2.setupInh;  
-  top.initProd := c1.initProd ++ c2.initProd;  
-  top.initValues := c1.initValues ++ c2.initValues;
-  top.postInit := c1.postInit ++ c2.postInit;
-  top.initWeaving := c1.initWeaving ++ c2.initWeaving;
-  top.valueWeaving := c1.valueWeaving ++ c2.valueWeaving;
+  return if null(others) then "" else "\t\t" ++ makeName(head(others)) ++ ".Init."++nme++"();\n" ++ makeOthers(tail(others),nme);
 }
 
