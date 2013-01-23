615fa46bce223f85c63aa399704f4968c7459610
Ted Kaminski <tedinski@cs.umn.edu>
2013-01-23 16:31:13 -0600

    Tweak whitespace in foreign type code.

--- grammars/silver/modification/ffi/TypeExp.sv
+++ grammars/silver/modification/ffi/TypeExp.sv
@@ -10,12 +10,14 @@ top::TypeExp ::= fn::String params::[TypeExp]
   top.typepp = fn ++ if !null(params) then "<" ++ implode(" ", mapTypePP(params, top.boundVariables)) ++ ">" else "";
 
   -- Unification.sv
-  top.unify = case top.unifyWith of
-               foreignTypeExp(ofn, op) -> if fn == ofn
-                                          then unifyAll( params, op )
-                                          else errorSubst("Tried to unify conflicting foreign types " ++ fn ++ " and " ++ ofn)
-             | _ -> errorSubst("Tried to unify foreign type " ++ fn ++ " with " ++ prettyType(top.unifyWith))
-              end;
+  top.unify = 
+    case top.unifyWith of
+    | foreignTypeExp(ofn, op) ->
+        if fn == ofn
+        then unifyAll( params, op )
+        else errorSubst("Tried to unify conflicting foreign types " ++ fn ++ " and " ++ ofn)
+    | _ -> errorSubst("Tried to unify foreign type " ++ fn ++ " with " ++ prettyType(top.unifyWith))
+    end;
 
   -- env
   top.unparse = "foreigntype('" ++ fn ++ "', " ++ unparseTypes(params, top.boundVariables) ++ ")";
