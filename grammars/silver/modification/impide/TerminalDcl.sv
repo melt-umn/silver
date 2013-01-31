7c298321104310471686aa42923cdae9edce7837
Ted Kaminski <tedinski@cs.umn.edu>
2013-01-30 18:27:10 -0600

    Introduce font modifiers to lexer classes, not just terminals.

--- grammars/silver/modification/impide/TerminalDcl.sv
+++ grammars/silver/modification/impide/TerminalDcl.sv
@@ -1,6 +1,7 @@
 grammar silver:modification:impide;
 
 import silver:modification:impide:cstast;
+import silver:modification:copper;
 
 -- terminal's font in IDE
 terminal Font_kwd  'font';
@@ -14,3 +15,15 @@ top::TerminalModifier ::= 'font' '=' fontName::IdLower_t
   top.terminalModifiers = [termFont(fontName.lexeme)];
   top.errors := [];
 }
+
+-- Allows fonts on lexer classes, too!
+concrete production lexerClassModifierFont
+top::LexerClassModifier ::= 'font' '=' fontName::IdLower_t
+{
+  top.pp = "font = " ++ fontName.lexeme;
+  top.location = loc(top.file, $1.line, $1.column);
+
+  top.lexerClassModifiers = [lexerClassFont(fontName.lexeme)];
+  top.errors := [];
+}
+
