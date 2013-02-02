854dc68e162d744433a8692fdb9ebdf1807355e7
Ted Kaminski <tedinski@cs.umn.edu>
2013-02-01 18:28:14 -0600

    Introduce a new grammar search mechanism that allows prefixes to be single directories.

--- grammars/silver/driver/util/Util.sv
+++ grammars/silver/driver/util/Util.sv
@@ -19,13 +19,36 @@ String ::= g::String
 function findGrammarLocation
 IOVal<Maybe<String>> ::= path::String searchPaths::[String] iIn::IO
 {
-  local attribute exists :: IOVal<Boolean>;
-  exists = isDirectory(head(searchPaths) ++ path, iIn);
+  local exists :: IOVal<Maybe<String>> =
+    findGrammarInLocation(path, head(searchPaths), iIn);
 
-  return if null(searchPaths)
-         then ioval(iIn, nothing())
-         else if exists.iovalue
-              then ioval(exists.io, just(head(searchPaths) ++ path))
-              else findGrammarLocation(path, tail(searchPaths), exists.io);
+  return 
+    if null(searchPaths) then ioval(iIn, nothing())
+    else if exists.iovalue.isJust then exists
+    else findGrammarLocation(path, tail(searchPaths), exists.io);
+}
+
+{--
+ - Looks to see if the grammar can be found in 'inPath'
+ - Tries (in order) for edu:umn:cs
+ - edu/umn/cs/
+ - edu.umn/cs/
+ - edu.umn.cs/
+ -}
+function findGrammarInLocation
+IOVal<Maybe<String>> ::= gram::String inPath::String iIn::IO
+{
+  -- Find the first / in the grammar name (turned path) we're looking for.
+  local idx :: Integer = indexOf("/", gram);
+  
+  -- Replace the first / with a .
+  local nextGram :: String = substring(0, idx, gram) ++ "." ++ substring(idx + 1, length(gram), gram);
+  
+  local exists :: IOVal<Boolean> = isDirectory(inPath ++ gram, iIn);
+  
+  return 
+    if idx == -1 then ioval(iIn, nothing())
+    else if exists.iovalue then ioval(exists.io, just(inPath ++ gram))
+    else findGrammarInLocation(nextGram, inPath, exists.io);
 }
 
