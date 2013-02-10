d64b69ac5d12175946fbe4aef23d2d358f9f5a7b
Ted Kaminski <tedinski@cs.umn.edu>
2013-02-09 19:49:22 -0600

    Mature tests a bit, fix bugs related to annotations.

--- test/silver_features/anno/Anno.sv
+++ test/silver_features/anno/Anno.sv
@@ -48,8 +48,40 @@ wrongCode "'what' is not appropriate" {
   global aasdf :: AnnoNT = ordinaryFun(what=2);
 }
 
+wrongCode "'asfd' is not appropriate" {
+  global aasdf :: AnnoNT = anAnnoNT(asfd=2);
+}
+
 wrongCode "expected Integer" {
   global aasdf :: AnnoNT = anAnnoNT(what="wrong type");
 }
+
+abstract production moreAnnoNT
+top::AnnoNT ::= s::String
+{
+}
+
+global fun2 :: (AnnoNT ::= String Integer) = moreAnnoNT(_, what=_);
+global fun3 :: (AnnoNT ::= Integer) = moreAnnoNT("hi", what=_);
+global fun4 :: (AnnoNT ::= String) = moreAnnoNT(_, what=7);
+
+global nt3 :: AnnoNT = fun2("hi", 5);
+global nt4 :: AnnoNT = fun3(6);
+global nt5 :: AnnoNT = fun4("str");
+
+equalityTest ( nt3.what, 5, Integer, silver_tests ) ;
+equalityTest ( nt4.what, 6, Integer, silver_tests ) ;
+equalityTest ( nt5.what, 7, Integer, silver_tests ) ;
+
+wrongCode "Missing named parameters" {
+  global aasdf :: AnnoNT = moreAnnoNT("tmp");
+}
+wrongCode "Too few arguments provided " {
+  global aasdf :: AnnoNT = moreAnnoNT(what=2);
+}
+
+-- Let's ensure we can still pattern match alright
+global grabstr :: String = case nt5 of moreAnnoNT(s) -> s end;
+equalityTest ( grabstr, "str", String, silver_tests ) ;
 
 
