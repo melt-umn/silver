f58e01a1b9964af4ad8e0f3985a5cb7ae6c7c671
Ted Kaminski <tedinski@cs.umn.edu>
2013-02-19 17:33:43 -0600

    Fix bug with localEq... it was being turned into an inhEq... flow information in interface files now agrees with grammars.

--- grammars/silver/definition/flow/env_parser/Parser.sv
+++ grammars/silver/definition/flow/env_parser/Parser.sv
@@ -20,8 +20,8 @@ terminal NtRefDefTerm 	 'ntRefFlowDef' lexer classes {C_1};
 terminal NonHostSynTerm  'nonHostSyn' lexer classes {C_1}; 
 terminal LocalTerm 	 'local' lexer classes {C_1}; 
 terminal LocalInhTerm 	 'localInh' lexer classes {C_1};
-terminal FwdInhTerm 	 'fwdInh' lexer classes {C_1};  
-terminal ExtraTerm 	 'extra' lexer classes {C_1};  
+terminal FwdInhTerm 	 'fwdInh' lexer classes {C_1};
+terminal ExtraTerm 	 'extra' lexer classes {C_1};
 
 attribute flowDefs occurs on IRoot, IRootPart;
 
@@ -67,19 +67,16 @@ nonterminal IFlowVertexes with flowDeps;
 nonterminal IFlowVertexesInner with flowDeps;
 nonterminal IFlowVertex with flowDeps;
 
--- Start change --
 concrete production aFlowsNone_vertex
 top::IFlowVertexes ::= '[' ']'
 {
   top.flowDeps = [];
 }
-
 concrete production aFlowsSome_vertex
 top::IFlowVertexes ::= '[' l::IFlowVertexesInner ']'
 {
   top.flowDeps = l.flowDeps;
 }
-
 concrete production aFlowsOne_vertex
 top::IFlowVertexesInner ::= l::IFlowVertex
 {
@@ -91,41 +88,34 @@ top::IFlowVertexesInner ::= l::IFlowVertexesInner ',' r::IFlowVertex
   top.flowDeps = l.flowDeps ++ r.flowDeps;
 }
 
+
 concrete production aFlowSyn_vertex
-top::IFlowVertex ::= 'lhsSynV' '('  attr::IName ')'
+top::IFlowVertex ::= 'lhsSynV' '(' attr::IName ')'
 {
-  top.flowDeps = [lhsSynVertex( attr.aname)];
+  top.flowDeps = [lhsSynVertex(attr.aname)];
 }
-
 concrete production aFlowInh_vertex
-top::IFlowVertex ::= 'lhsInhV' '('  attr::IName ')'
+top::IFlowVertex ::= 'lhsInhV' '(' attr::IName ')'
 {
-  top.flowDeps = [lhsInhVertex( attr.aname)];
+  top.flowDeps = [lhsInhVertex(attr.aname)];
 }
-
 concrete production aFlowRhs_vertex
-top::IFlowVertex ::= 'rhsV' '('  sigName::IName ',' attr::IName ')'
+top::IFlowVertex ::= 'rhsV' '(' sigName::IName ',' attr::IName ')'
 {
   top.flowDeps = [rhsVertex(sigName.aname, attr.aname)];
 }
-
 concrete production aFlowlocalEqV_vertex
-top::IFlowVertex ::= 'localEqV' '('  fname::IName ')'
+top::IFlowVertex ::= 'localEqV' '(' fname::IName ')'
 {
-  top.flowDeps = [localEqVertex( fname.aname)];
+  top.flowDeps = [localEqVertex(fname.aname)];
 }
-
 concrete production aFlowlocalV_vertex
-top::IFlowVertex ::= 'localV' '('  fname::IName ',' attr::IName ')'
+top::IFlowVertex ::= 'localV' '(' fname::IName ',' attr::IName ')'
 {
-  top.flowDeps = [localVertex( fname.aname,attr.aname)];
+  top.flowDeps = [localVertex(fname.aname, attr.aname)];
 }
 
 
-
-
-
--- End change
 
 
 concrete production aFlowsNone
@@ -133,13 +123,11 @@ top::IFlows ::= '[' ']'
 {
   top.flowDefs = [];
 }
-
 concrete production aFlowsSome
 top::IFlows ::= '[' l::IFlowsInner ']'
 {
   top.flowDefs = l.flowDefs;
 }
-
 concrete production aFlowsOne
 top::IFlowsInner ::= l::IFlow
 {
@@ -151,75 +139,65 @@ top::IFlowsInner ::= l::IFlowsInner ',' r::IFlow
   top.flowDefs = l.flowDefs ++ r.flowDefs;
 }
 
+
 concrete production aFlowSyn
 top::IFlow ::= 'syn' '(' prod::IName ',' attr::IName ',' fv::IFlowVertexes ',' a::IBool ')'
 {
-  top.flowDefs = [synEq(prod.aname, attr.aname,fv.flowDeps,a.bval)];
+  top.flowDefs = [synEq(prod.aname, attr.aname, fv.flowDeps, a.bval)];
 }
-
 concrete production aFlowDef
 top::IFlow ::= 'def' '(' nt::IName ',' attr::IName ',' fv::IFlowVertexes')'
 {
-  top.flowDefs = [defEq(nt.aname, attr.aname,fv.flowDeps)];
+  top.flowDefs = [defEq(nt.aname, attr.aname, fv.flowDeps)];
 }
-
 concrete production aFlowFwd
 top::IFlow ::= 'fwd' '(' prod::IName ',' fv::IFlowVertexes ',' a::IBool')'
 {
-  top.flowDefs = [fwdEq(prod.aname,fv.flowDeps,a.bval)];
+  top.flowDefs = [fwdEq(prod.aname, fv.flowDeps, a.bval)];
 }
-
 concrete production aFlowProd
 top::IFlow ::= 'prod' '(' nt::IName ',' prod::IName ')'
 {
   top.flowDefs = [prodFlowDef(nt.aname, prod.aname)];
 }
-
 concrete production aFlowRefFlowDef
 top::IFlow ::= 'ntRefFlowDef' '(' nt::IName ',' prod::INames ')'
 {
   top.flowDefs = [ntRefFlowDef(nt.aname, prod.names)];
 }
-
 concrete production aFlowImplicitFwdAffects
 top::IFlow ::= 'implicitFwdAffects' '(' prd::IName ',' attrs::INames ')'
 {
   top.flowDefs = [implicitFwdAffects(prd.aname, attrs.names)];
 }
-
 concrete production aFlowNonHostSyn
-top::IFlow ::= 'nonHostSyn' '(' attr::IName ',' nt::IName  ')'
+top::IFlow ::= 'nonHostSyn' '(' attr::IName ',' nt::IName ')'
 {
-  top.flowDefs = [nonHostSynDef(attr.aname,nt.aname)];
+  top.flowDefs = [nonHostSynDef(attr.aname, nt.aname)];
 }
-
 concrete production aFlowInh
-top::IFlow ::= 'inh' '('  prod::IName ',' sigName::IName ',' attr::IName ',' fv::IFlowVertexes  ')'
+top::IFlow ::= 'inh' '(' prod::IName ',' sigName::IName ',' attr::IName ',' fv::IFlowVertexes ')'
 {
-  top.flowDefs = [inhEq(prod.aname, sigName.aname,attr.aname,fv.flowDeps)];
+  top.flowDefs = [inhEq(prod.aname, sigName.aname, attr.aname, fv.flowDeps)];
 }
-
 concrete production aFlowFwdInh
-top::IFlow ::= 'fwdInh' '('  prod::IName ',' attr::IName ',' fv::IFlowVertexes ')'
+top::IFlow ::= 'fwdInh' '(' prod::IName ',' attr::IName ',' fv::IFlowVertexes ')'
 {
-  top.flowDefs = [fwdInhEq(prod.aname,attr.aname,fv.flowDeps)];
+  top.flowDefs = [fwdInhEq(prod.aname, attr.aname, fv.flowDeps)];
 }
-
-
 concrete production aFlowLocalEq
-top::IFlow ::= 'local' '('  prod::IName ',' fname::IName ',' typeName::IName ',' fv::IFlowVertexes  ')'
+top::IFlow ::= 'local' '(' prod::IName ',' fname::IName ',' typeName::IName ',' fv::IFlowVertexes ')'
 {
-  top.flowDefs = [inhEq(prod.aname, fname.aname,typeName.aname,fv.flowDeps)];
+  top.flowDefs = [localEq(prod.aname, fname.aname, typeName.aname, fv.flowDeps)];
 }
-
 concrete production aFlowLocalInhEq
-top::IFlow ::= 'localInh' '('  prod::IName ',' fname::IName ',' attr::IName ',' fv::IFlowVertexes    ')'
+top::IFlow ::= 'localInh' '(' prod::IName ',' fname::IName ',' attr::IName ',' fv::IFlowVertexes ')'
 {
-  top.flowDefs = [localInhEq(prod.aname, fname.aname,attr.aname,fv.flowDeps)];
+  top.flowDefs = [localInhEq(prod.aname, fname.aname, attr.aname, fv.flowDeps)];
 }
-
 concrete production aFlowExtra
 top::IFlow ::= 'extra' '(' prod::IName ',' ifv::IFlowVertex ',' fv::IFlowVertexes ',' a::IBool ')'
 {
-  top.flowDefs = [extraEq(prod.aname, head(ifv.flowDeps),fv.flowDeps,a.bval)];
+  top.flowDefs = [extraEq(prod.aname, head(ifv.flowDeps), fv.flowDeps, a.bval)];
 }
+
