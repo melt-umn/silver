6894c3a5d92728fa4f46689a478a5a922fb3b184
Ming Zhou <zhoux738@umn.edu>
2013-04-17 17:31:50 -0500

    IDE: added ide:ant function which calls ant tool embedded in Eclipse; added exporter for Silver IDE.

--- runtime/imp/src/edu/umn/cs/melt/ide/util/Util.java
+++ runtime/imp/src/edu/umn/cs/melt/ide/util/Util.java
@@ -1,27 +1,68 @@
 package edu.umn.cs.melt.ide.util;
 
+import org.eclipse.ant.core.AntRunner;
+import org.eclipse.core.runtime.CoreException;
+
 import edu.umn.cs.melt.ide.silver.misc.ConsoleLoggingStream;
 
+/**
+ * This utility class is an API collection used mainly by Silver code (through
+ * foreign code block). 
+ */
 public final class Util {
 
-	private static ConsoleLoggingStream logger;
+	/* IDE Console */
+	
+	private static ConsoleLoggingStream CONSOLE_LOGGER;
 	
 	public static void setConsoleLoggingStream(ConsoleLoggingStream cls){
-		logger = cls;
+		CONSOLE_LOGGER = cls;
 	}
 	
 	public static Object info(String msg){
-		if(logger!=null){
-			logger.info(msg);
+		if(CONSOLE_LOGGER!=null){
+			CONSOLE_LOGGER.info(msg);
 		}
 		return null;
 	}
 	
 	public static Object error(String msg){
-		if(logger!=null){
-			logger.error(msg);
+		if(CONSOLE_LOGGER!=null){
+			CONSOLE_LOGGER.error(msg);
 		}
 		return null;
 	}
 	
+	/* Apache Ant tool */
+	
+	public static Object ant(String buildFile, String arguments, String target){
+		
+		ANT_RUNNER.ant(buildFile, arguments, target);
+
+		/*
+		AntRunner runner = new AntRunner();
+		runner.setBuildFileLocation(buildFile);
+		runner.setArguments(arguments);
+		if(target != null && !"".equals(target.trim())){
+			runner.setExecutionTargets(new String[]{target});
+		}
+		try {
+			runner.run();
+		} catch (CoreException e) {
+			e.printStackTrace();
+		}
+		*/
+		
+		return null;
+	}
+	
+	private static IAntRunnable ANT_RUNNER;
+	
+	public static void setAntRunnable(IAntRunnable iat){
+		ANT_RUNNER = iat;
+	}
+	
+	public static interface IAntRunnable {
+		void ant(String buildFile, String arguments, String target);
+	}
 }
