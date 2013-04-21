27834b2ad8adb27be0b3a693aeb3367e9053a403
Ted Kaminski <tedinski@cs.umn.edu>
2013-04-21 11:23:40 -0500

    A few more tests for the templating.

--- test/silver_features/Templating.sv
+++ test/silver_features/Templating.sv
@@ -1,12 +1,21 @@
 
+-- empty
 equalityTest ( template """""", "", String, silver_tests ) ;
-
+-- water only
 equalityTest ( template """hello""", "hello", String, silver_tests ) ;
 
 global aStrValue :: String = template """
 	$$""";
-
+-- escape only
 equalityTest ( template """${aStrValue}""", "\n\t$", String, silver_tests ) ;
-
+-- full expression
 equalityTest ( template """${toString(1)}""", "1", String, silver_tests ) ;
+-- water-escape
+equalityTest ( template """hello${toString(1)}""", "hello1", String, silver_tests ) ;
+-- escape-water
+equalityTest ( template """${toString(1)}hello""", "1hello", String, silver_tests ) ;
+-- e-w-e
+equalityTest ( template """${toString(1)}a${aStrValue}""", "1a\n\t$", String, silver_tests ) ;
+-- w-e-w
+equalityTest ( template """a${toString(1)}b""", "a1b", String, silver_tests ) ;
 
