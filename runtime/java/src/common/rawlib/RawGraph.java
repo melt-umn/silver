7ff9439bb367f1eee98b1b9f79b4c2778da08e82
Ted Kaminski <tedinski@cs.umn.edu>
2013-05-13 22:32:40 -0500

    Introduce transitiveClosure.

--- runtime/java/src/common/rawlib/RawGraph.java
+++ runtime/java/src/common/rawlib/RawGraph.java
@@ -1,6 +1,8 @@
 package common.rawlib;
 
+import java.util.ArrayDeque;
 import java.util.Map.Entry;
+import java.util.Queue;
 import java.util.TreeMap;
 import java.util.TreeSet;
 
@@ -27,17 +29,22 @@ public final class RawGraph {
 		for(core.NPair elem : new ConsCellCollection<core.NPair>(l)) {
 			assert(elem instanceof core.Ppair); // document as an assert why not
 			final Object key = elem.getChild(0);
-			final TreeSet<Object> setToModify = 
-					// If we've mutated it, then it exists and we can just change it
-					mutated.contains(key) ? ret.get(key) :
-					// If it existed, unmutated, we shallow copy it to be able to mutate it
-					ret.containsKey(key) ? (TreeSet<Object>)ret.get(key).clone() :
-					// Otherwise, we create a new empty set
-					new TreeSet<Object>(g.comparator());
-			// Add this to the set
+			
+			TreeSet<Object> setToModify = ret.get(key);
+			
+			// If we've mutated it before, we're okay to do it again.
+			if(!mutated.contains(key)) {
+				// Otherwise, either create the set or clone it so we can mutate.
+				if(setToModify == null) {
+					setToModify = new TreeSet<Object>(g.comparator());
+				} else {
+					setToModify = (TreeSet<Object>)ret.get(key).clone();
+				}
+				ret.put(key, setToModify);
+				mutated.add(key);
+			}
+
 			setToModify.add(elem.getChild(1));
-			// And update our graph
-			ret.put(key, setToModify);
 		}
 		return ret;
 	}
@@ -68,6 +75,40 @@ public final class RawGraph {
 				ret = new ConsCell(new core.Ppair(key, value), ret);
 			}
 		}
+		return ret;
+	}
+	
+	// transitiveClosure :: (Graph<a> ::= Graph<a>)
+	public static TreeMap<Object,TreeSet<Object>> transitiveClosure(TreeMap<Object,TreeSet<Object>> g) {
+
+		final TreeMap<Object,TreeSet<Object>> ret = (TreeMap<Object,TreeSet<Object>>)g.clone();
+
+		// For transitive closure we're going to presume that we mutate everything.
+		for(Object key : ret.keySet()) {
+			final TreeSet<Object> set = (TreeSet<Object>)ret.get(key).clone(); 
+			ret.put(key, set);
+			
+			// This is somewhat inefficient because we sort of recompute
+			// the transitive closure of many vertices.
+			// But I'm just going for simple correctness for the moment.
+			final ArrayDeque<Object> need = new ArrayDeque<Object>(set);
+			while(!need.isEmpty()) {
+				// Get work item
+				final Object focus = need.pop();
+				// Find out what this item adds
+				TreeSet<Object> diff = ret.get(focus);
+				// Many vertexes may have nothing coming out of them!
+				if(diff == null)
+					continue;
+				diff = (TreeSet<Object>)diff.clone();
+				diff.removeAll(set);
+				// Add it to the work list
+				need.addAll(diff);
+				// Add it to the set of deps
+				set.addAll(diff);
+			}
+		}
+		
 		return ret;
 	}
 }
