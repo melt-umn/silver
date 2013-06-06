8da355b06a436c07e1b5bd323cb68e099bc206b3
Kevin Williams <kwill@cs.umn.edu>
2013-06-06 02:40:31 +0100

    Adding lookup for IdeProperty

--- grammars/ide/IdeProperty.sv
+++ grammars/ide/IdeProperty.sv
@@ -13,3 +13,21 @@ top::IdeProperty ::= propName::String propType::String propValue::String
   top.propType = propType;
   top.propValue = propValue;
 }
+
+{-- lookupIdeProperty
+ - prop : name of property to lookup
+ - args : list of IdePropertys 
+ - 
+ - output : if the named property was found, then a just containing the string
+ -          representing the value, otherwise a nothing
+ -}
+function lookupIdeProperty
+Maybe<String> ::= prop::String args::[IdeProperty]
+{
+  return
+    if null(args)
+    then nothing()
+    else if head(args).propName == prop
+         then just( head( args ).propValue )
+         else lookupIdeProperty( prop, tail(args) );
+}
