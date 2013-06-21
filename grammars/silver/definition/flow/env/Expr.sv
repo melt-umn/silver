06f32c382acc5a285c7f3d46f9b067742a1f1101
Ted Kaminski <tedinski@cs.umn.edu>
2013-06-20 19:31:42 -0500

    Instead of pattern matching, switch over to using ExprVertexInfo about expressions. Should not change behavior, except for some bug fixes.

--- grammars/silver/definition/flow/env/Expr.sv
+++ grammars/silver/definition/flow/env/Expr.sv
@@ -7,8 +7,19 @@ import silver:modification:primitivepattern;
 import silver:extension:patternmatching only Arrow_kwd, Vbar_kwd; -- TODO remove
 import silver:modification:let_fix;
 
-synthesized attribute flowDeps :: [FlowVertex] occurs on Expr, ExprInhs, ExprInh, Exprs, AppExprs, AppExpr, AnnoAppExprs, AnnoExpr;
-attribute flowEnv occurs on Expr, ExprInhs, ExprInh, Exprs, AppExprs, AppExpr, AnnoAppExprs, AnnoExpr;
+{--
+ - Direct (potential) dependencies this expression has on nodes in the production flow graph.
+ -}
+synthesized attribute flowDeps :: [FlowVertex];
+{--
+ - Determines whether this expression corresponds to a node in the flow graph, and how
+ - to treat it specially if so.
+ -}
+synthesized attribute flowVertexInfo :: ExprVertexInfo;
+
+attribute flowDeps, flowEnv occurs on Expr, ExprInhs, ExprInh, Exprs, AppExprs, AppExpr, AnnoAppExprs, AnnoExpr;
+attribute flowVertexInfo occurs on Expr;
+
 
 function inhsForTakingRef
 [String] ::= nt::String  flowEnv::Decorated FlowEnv
@@ -26,6 +37,15 @@ function depsForTakingRef
   return map(f, inhsForTakingRef(nt, flowEnv));  
 }
 
+aspect default production
+top::Expr ::=
+{
+  -- We go with using default here because
+  -- (a) it's safe. vertexInfo is for being less conservative and more precise.
+  -- (b) only a few productions actually provide it.
+  top.flowVertexInfo = noVertex();
+}
+
 aspect production errorReference
 top::Expr ::= q::Decorated QName
 {
@@ -34,41 +54,53 @@ top::Expr ::= q::Decorated QName
 aspect production childReference
 top::Expr ::= q::Decorated QName
 {
-  -- TODO: there might be a problem! with this not detecting some decorations.
-  
-  -- Notes: q should find the actual type listed in the signature. Note that's different
-  -- than childReference's reported typerep (which is that either/or type). So that's okay.
-  
-  -- What kind of problem? Damn my comments, maybe that's been fixed.
-
+  -- Note that q should find the actual type written in the signature, and so
+  -- isDecorable on that indeed tells us whether it's something autodecorated.
   top.flowDeps =
     if q.lookupValue.typerep.isDecorable && !performSubstitution(top.typerep, top.finalSubst).isDecorable
     then depsForTakingRef(rhsVertex(q.lookupValue.fullName, _), q.lookupValue.typerep.typeName, top.flowEnv)
     else [];
+  top.flowVertexInfo = simpleHasVertex(rhsVertex(q.lookupValue.fullName, _));
 }
 aspect production lhsReference
 top::Expr ::= q::Decorated QName
 {
+  -- Always a decorable type, so just check how it's being used:
   top.flowDeps =
-    if {-always decorable, so just check final:-} !performSubstitution(top.typerep, top.finalSubst).isDecorable
+    if !performSubstitution(top.typerep, top.finalSubst).isDecorable
     then depsForTakingRef(lhsInhVertex, q.lookupValue.typerep.typeName, top.flowEnv)
     else [];
+  top.flowVertexInfo = hasVertex(lhsSynVertex, lhsInhVertex, []);
 }
 aspect production localReference
 top::Expr ::= q::Decorated QName
 {
+  -- Again, q give the actual type written.
   top.flowDeps = [localEqVertex(q.lookupValue.fullName)] ++
     if q.lookupValue.typerep.isDecorable && !performSubstitution(top.typerep, top.finalSubst).isDecorable
     then depsForTakingRef(localVertex(q.lookupValue.fullName, _), q.lookupValue.typerep.typeName, top.flowEnv)
     else [];
+    
+  top.flowVertexInfo =
+    hasVertex(
+      localVertex(q.lookupValue.fullName, _),
+      localVertex(q.lookupValue.fullName, _),
+      [localEqVertex(q.lookupValue.fullName)]); -- aw, shucks. not a simpleHasVertex :(
 }
 aspect production forwardReference
 top::Expr ::= q::Decorated QName
 {
+  -- Again, always a decorable type.
   top.flowDeps = [forwardEqVertex()]++
-    if {-always decorable, so just check final:-} !performSubstitution(top.typerep, top.finalSubst).isDecorable
+    if !performSubstitution(top.typerep, top.finalSubst).isDecorable
     then depsForTakingRef(forwardVertex, q.lookupValue.typerep.typeName, top.flowEnv)
     else [];
+    
+  top.flowVertexInfo =
+    hasVertex(
+      forwardVertex,
+      forwardVertex,
+      [forwardEqVertex()]); -- aw, shucks. not a simpleHasVertex :(
 }
 aspect production productionReference
 top::Expr ::= q::Decorated QName
@@ -121,24 +153,18 @@ aspect production synDecoratedAccessHandler
 top::Expr ::= e::Decorated Expr  q::Decorated QNameAttrOccur
 {
   top.flowDeps = 
-    case e of
-    | childReference(lq) -> [rhsVertex(lq.lookupValue.fullName, q.attrDcl.fullName)]
-    | lhsReference(lq) -> [lhsSynVertex(q.attrDcl.fullName)]
-    | localReference(lq) -> [localVertex(lq.lookupValue.fullName, q.attrDcl.fullName)]
-    | forwardReference(lq) -> [forwardVertex(q.attrDcl.fullName)]
-    | _ -> e.flowDeps
+    case e.flowVertexInfo of
+    | hasVertex(sVT, _, r) -> sVT(q.attrDcl.fullName) :: r
+    | noVertex() -> e.flowDeps
     end;
 }
 aspect production inhDecoratedAccessHandler
 top::Expr ::= e::Decorated Expr  q::Decorated QNameAttrOccur
 {
   top.flowDeps = 
-    case e of
-    | childReference(lq) -> [rhsVertex(lq.lookupValue.fullName, q.attrDcl.fullName)]
-    | lhsReference(lq) -> [lhsInhVertex(q.attrDcl.fullName)]
-    | localReference(lq) -> [localVertex(lq.lookupValue.fullName, q.attrDcl.fullName)]
-    | forwardReference(lq) -> [forwardVertex(q.attrDcl.fullName)]
-    | _ -> e.flowDeps
+    case e.flowVertexInfo of
+    | hasVertex(_, iVT, r) -> iVT(q.attrDcl.fullName) :: r
+    | noVertex() -> e.flowDeps
     end;
 }
 aspect production errorDecoratedAccessHandler
@@ -162,11 +188,14 @@ aspect production decorateExprWith
 top::Expr ::= 'decorate' e::Expr 'with' '{' inh::ExprInhs '}'
 {
   top.flowDeps = e.flowDeps ++ inh.flowDeps;
+  -- TODO top.flowVertexInfo = simpleHasVertex(rhsVertex(q.lookupValue.fullName, _));
+  -- anon vertex here
 }
 aspect production decorateExprWithIntention
 top::Expr ::= e::Expr  inh::ExprInhs  intention::[String]
 {
   top.flowDeps = e.flowDeps ++ inh.flowDeps;
+  -- TODO decide if keeping this or not?? maybe remove this case now
 }
 
 aspect production exprInh
@@ -250,6 +279,8 @@ aspect production ifThenElse
 top::Expr ::= 'if' e1::Expr 'then' e2::Expr 'else' e3::Expr
 {
   top.flowDeps = e1.flowDeps ++ e2.flowDeps ++ e3.flowDeps;
+  -- TODO: it's possible we could create an anon flow vertex type here
+  -- that duplicates all vertexes to both 'then' and 'else'...
 }
 aspect production intConst
 top::Expr ::= i::Int_t
@@ -385,6 +416,7 @@ top::Expr ::= e::Decorated Expr
   -- So definitely don't consider making this []!
   
   top.flowDeps = e.flowDeps;
+  top.flowVertexInfo = e.flowVertexInfo;
 }
 
 
@@ -422,15 +454,11 @@ top::Expr ::= 'toString' '(' e1::Expr ')'
 aspect production newFunction
 top::Expr ::= 'new' '(' e1::Expr ')'
 {
-  -- accommodate using 'new' to undecorate children/locals/etc without penalty.
-  -- If we see it's a direct new of one of those, suppress the flow deps.
+  -- Emit nothing except the keepDeps, for a vertex node
   top.flowDeps =
-    case (case e1 of exprRef(e2) -> e2 | _ -> e1 end) of -- TODO: Here we have a dumb hack to deal with exprRef!
-    | childReference(_) -> []
-    | lhsReference(_) -> []
-    | localReference(_) -> []
-    | forwardReference(_) -> []
-    | _ -> e1.flowDeps
+    case e1.flowVertexInfo of
+    | hasVertex(sVT, _, r) -> r
+    | noVertex() -> e1.flowDeps
     end;
 }
 
@@ -497,12 +525,14 @@ aspect production assignExpr
 top::AssignExpr ::= id::Name '::' t::Type '=' e::Expr
 {
   top.flowDeps = e.flowDeps;
+  -- We should be doing something to store e.flowVertexInfo in 'id' in the env
 }
 
 aspect production lexicalLocalReference
 top::Expr ::= q::Decorated QName
 {
   top.flowDeps = [];
+  -- TODO: should be setting flowVertexInfo to whatever that expr had
 }
 
 
@@ -518,13 +548,13 @@ top::Expr ::= e::Expr t::Type pr::PrimPatterns f::Expr
   -- Let's make sure for decorated types, we only demand what's necessary for forward
   -- evaluation.
   top.flowDeps = pr.flowDeps ++ f.flowDeps ++
-    case (case e of exprRef(e1) -> e1 | _ -> e end) of -- TODO: Here we have a dumb hack to deal with exprRef!
-    | childReference(lq) -> [rhsForwardVertex(lq.lookupValue.fullName)]
-    | lhsReference(lq) -> [forwardEqVertex()] -- weirdos!
-    | localReference(lq) -> [localForwardVertex(lq.lookupValue.fullName)]
-    | forwardReference(lq) -> [forwardForwardVertex()] -- actually less weird!
-    | _ -> e.flowDeps
+    case e.flowVertexInfo of
+    | hasVertex(sVT, _, r) -> sVT("forward") :: r
+    | noVertex() -> e.flowDeps
     end;
+
+  -- TODO: again, like 'if', we could be doing a kind of union flowVertexInfo
+  -- thingy here.
 }
 
 aspect production onePattern
