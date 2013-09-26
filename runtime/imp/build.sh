aa08b2335eae5d2982216c0af1673c9fd9252785
Eric Van Wyk <eric.r.van.wyk@gmail.com>
2013-09-26 16:15:29 -0500

    Modified IMP build.sh script to fix where jars are copied to (per Ming's email) also changed script to use 'mvn' from wherever it is installed instead of Ming's hardcoded path

--- runtime/imp/build.sh
+++ runtime/imp/build.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
-BUILD_COMM="/home/patrick/maven3/bin/mvn"
+# On CS machines, load the module 'java/maven'
+# On personal machines, make sure 'mvn' is in your path.
+BUILD_COMM="mvn"
+
+if [ ! -d ../../jars ]; then
+  echo "In wrong directory?  Run from silver/runtime/imp as ./build.sh"
+  exit 1
+fi
+
 CURR_DIR=`pwd`
 
 cd edu.umn.cs.melt.copper
 $BUILD_COMM clean install
-cd $CURR_DIR
+cd ..
 
 cd edu.umn.cs.melt.silver  
 $BUILD_COMM clean install
-cd $CURR_DIR
+cd ..
 
 cd main
 $BUILD_COMM clean package
-cp target/edu.umn.cs.melt.ide.copper-*.jar $CURR_DIR
+cp target/edu.umn.cs.melt.ide.copper-*.jar ../../../jars
 
-cd $CURR_DIR
