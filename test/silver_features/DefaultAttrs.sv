7080f858b1830b561e4f9c704b07decd7edf38ee
Ted Kaminski <tedinski@cs.umn.edu>
2014-06-25 16:46:34 -0500

    Make default productions able to reference the LHS.

--- test/silver_features/DefaultAttrs.sv
+++ test/silver_features/DefaultAttrs.sv
@@ -1,8 +1,8 @@
 
-nonterminal DefaultAttrNT with defaultsyn;
+nonterminal DefaultAttrNT with defaultsyn, defaultsyn2;
 
 synthesized attribute defaultsyn :: Integer;
-synthesized attribute defaultNT :: DefaultAttrNT;
+synthesized attribute defaultsyn2 :: Integer;
 
 abstract production defaultattr1
 top::DefaultAttrNT ::=
@@ -21,21 +21,29 @@ top::DefaultAttrNT ::=
 {
 }
 
+abstract production defaultfwd
+top::DefaultAttrNT ::=
+{
+  top.defaultsyn = 3;
+  forwards to defaultattrn();
+}
+
 aspect default production
 top::DefaultAttrNT ::=
 {
   top.defaultsyn = 0;
+  top.defaultsyn2 = top.defaultsyn;
 }
 
 equalityTest ( defaultattr1().defaultsyn, 1, Integer, silver_tests ) ;
 equalityTest ( defaultattr2().defaultsyn, 2, Integer, silver_tests ) ;
 equalityTest ( defaultattrn().defaultsyn, 0, Integer, silver_tests ) ;
 equalityTest ( defaultattr1().defaultsyn, 1, Integer, silver_tests ) ;
+equalityTest ( defaultfwd().defaultsyn, 3, Integer, silver_tests ) ;
 
-wrongCode "LHS" {
-  aspect default production
-  top::DefaultAttrNT ::=
-  {
-    top.defaultNT = top;
-  }
-}
+equalityTest ( defaultattr1().defaultsyn2, 1, Integer, silver_tests ) ;
+equalityTest ( defaultattr2().defaultsyn2, 2, Integer, silver_tests ) ;
+equalityTest ( defaultattrn().defaultsyn2, 0, Integer, silver_tests ) ;
+equalityTest ( defaultfwd().defaultsyn2, 0, Integer, silver_tests ) ; -- Difference from above
+
+
