d76fe3f5b93a78f619753a517522cad26e86e87c
Ted Kaminski <tedinski@cs.umn.edu>
2015-01-20 14:30:59 -0600

    purge initializer and startuphook. use the plugin activator

--- grammars/silver/composed/idetest/plugin/src/silver/composed/idetest/imp/SILVERPlugin.java
+++ grammars/silver/composed/idetest/plugin/src/silver/composed/idetest/imp/SILVERPlugin.java
@@ -10,6 +10,16 @@ import org.osgi.framework.BundleContext;
 
 import edu.umn.cs.melt.ide.silver.env.SilverEnv;
 
+import org.eclipse.ant.core.AntRunner;
+import org.eclipse.core.runtime.CoreException;
+
+import edu.umn.cs.melt.ide.util.ISilverTypeWrapper;
+import edu.umn.cs.melt.ide.util.ProjectUtil;
+import edu.umn.cs.melt.ide.util.Util;
+import edu.umn.cs.melt.ide.util.Util.IAntRunnable;
+import edu.umn.cs.melt.ide.util.cst.Locator;
+import edu.umn.cs.melt.ide.util.cst.DefinitionFinder;
+
 public class SILVERPlugin extends PluginBase {
 
     public static final String kPluginID = "SILVER_IDE";
@@ -33,10 +43,55 @@ public class SILVERPlugin extends PluginBase {
 
     public void start(BundleContext context) throws Exception {
         super.start(context);
+
+			silver.composed.idetest.Init.initAllStatics();
+			silver.composed.idetest.Init.init();
+			silver.composed.idetest.Init.postInit();
         
         SilverEnv.initialize(context.getBundle());
 		common.Util.environment.put("SILVER_HOME", SilverEnv.getSilverHome().getAbsolutePath());
 		common.Util.environment.put("SILVER_GEN", SilverEnv.getGeneratedFolder().getAbsolutePath());
+
+		edu.umn.cs.melt.ide.impl.SVRegistry.register(new silver.composed.idetest.SVIdeInterface());
+
+        //Set up ant runner
+        Util.setAntRunnable(new IAntRunnable(){
+
+			@Override
+			public void ant(String buildFile, String arguments, String target) {
+				AntRunner runner = new AntRunner();
+				runner.setBuildFileLocation(buildFile);
+				runner.setArguments(arguments);
+				if(target != null && !"".equals(target.trim())){
+					runner.setExecutionTargets(new String[]{target});
+				}
+				try {
+					runner.run();
+				} catch (CoreException e) {
+					e.printStackTrace();
+				}
+			}
+        	
+        });
+
+        ProjectUtil.setSilverTypeWrapper(new ISilverTypeWrapper(){
+			@Override
+			public Object wrapInIOVal(Object io, Object arg) {
+				return new core.Pioval(io, arg);
+			}
+
+			@Override
+			public Object wrapInMaybe(Object arg) {
+				if(arg!=null){
+					return new core.Pjust(arg);
+				} else {
+					return new core.Pnothing(); 
+				}
+			}
+        });
+    
+        //Set up definition locator
+        Locator.setDefinitionFinder(new DefinitionFinder());
     }
 
     @Override
