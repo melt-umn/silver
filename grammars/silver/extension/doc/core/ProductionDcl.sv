f742e55e6f23699f319bfd20bc6155723e01f513
Lucas Kramer <krame505@umn.edu>
2016-02-03 13:35:09 -0600

    Adding doc output path option I forgot to push yesterday...

--- grammars/silver/extension/doc/core/ProductionDcl.sv
+++ grammars/silver/extension/doc/core/ProductionDcl.sv
@@ -5,6 +5,7 @@ import silver:util;
 aspect production productionDcl
 top::AGDcl ::= 'abstract' 'production' id::Name ns::ProductionSignature body::ProductionBody
 {
+  top.docs := [s"""production ${id.name}"""];
 }
 
 
