012a38529dadb18e2af15e52c491b0332c462fdd
Lucas Kramer <krame505@umn.edu>
2016-04-07 13:08:24 -0500

    A couple more tests...

--- test/silver_features/Lambda.sv
+++ test/silver_features/Lambda.sv
@@ -12,3 +12,11 @@ equalityTest(addThree(4), 7, Integer, silver_tests);
 global sumTwo::(Integer ::= Integer Integer) = lambda x::Integer y::Integer -> x + y;
 
 equalityTest(sumTwo(4, 5), 9, Integer, silver_tests);
+
+global curriedSum::((Integer ::= Integer) ::= Integer) = lambda x::Integer -> lambda y::Integer -> x + y;
+
+equalityTest(curriedSum(1)(2), 3, Integer, silver_tests);
+
+global differentTypes::((String ::= String) ::= Integer) = lambda x::Integer -> lambda x::String -> x;
+
+equalityTest(differentTypes(1)("abcd"), "abcd", String, silver_tests);
