2ee8175ed67ff90b283cab65e6b66cf6b86a81ce
Lucas Kramer <krame505@umn.edu>
2016-04-08 17:18:15 -0500

    Adding monad instances for maybe and list

---
+++ grammars/core/monad/Maybe.sv
@@ -0,0 +1,17 @@
+grammar core:monad;
+
+function bindMaybe
+Maybe<b> ::= m::Maybe<a> fn::(Maybe<b> ::= a)
+{
+  return case m of
+    just(x) -> fn(x)
+  | nothing() -> nothing()
+  end;
+}
+
+--global returnMaybe::(Maybe<a> ::= a) = just; -- TODO: Why doesn't this work?
+function returnMaybe
+Maybe<a> ::= x::a
+{
+  return just(x);
+}
