7bec541dbb6388236da7916042ad5cd22c2c8cd0
Ted Kaminski <tedinski@cs.umn.edu>
2016-04-10 18:11:38 -0500

    Simplify test case and fix Thunk behavior

--- test/silver_features/Lambda.sv
+++ test/silver_features/Lambda.sv
@@ -17,13 +17,16 @@ equalityTest(curriedSum(1)(2), 3, Integer, silver_tests);
 global differentTypes::((String ::= String) ::= Integer) = \ x::Integer -> \ x::String -> x;
 equalityTest(differentTypes(1)("abcd"), "abcd", String, silver_tests);
 
+-- Test triggers bug when parameter is a thunk and not a literal, as all the above were.
 global param::Integer = 4;
 equalityTest(addThree(param), 7, Integer, silver_tests);
 
-function foo
-Integer ::= a::Integer b::Integer
-{
-  return b;
-}
+-- These worked originally
+equalityTest( (\x::Integer -> [x])(4), [4], [Integer], silver_tests);
+equalityTest(map(\x::Integer -> x, [1,2,3]), [1,2,3], [Integer], silver_tests);
+-- This one triggered the issue with 'context' being mutated to null by Thunk.
+-- This resulted in another thunk believing it had already been evaluated (since
+-- its context was null) and thus returning 'data' which was null, since no
+-- evaluation had occurred.
+equalityTest(head(map(\x::Integer -> [x], [4])), [4], [Integer], silver_tests);
 
-equalityTest(foldr(foo, 0, head(map(\b::Integer -> [b], [4]))), 4, Integer, silver_tests);
