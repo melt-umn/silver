ec2ff5fe3e0cd1976de7f804d30d21dd3c80f0cc
Lucas Kramer <krame505@umn.edu>
2016-04-10 15:02:13 -0500

    Found another failing test case...

--- test/silver_features/Lambda.sv
+++ test/silver_features/Lambda.sv
@@ -6,20 +6,18 @@ function makeInc
 }
 
 global addThree::(Integer ::= Integer) = makeInc(3);
-
 equalityTest(addThree(4), 7, Integer, silver_tests);
 
 global sumTwo::(Integer ::= Integer Integer) = \ x::Integer y::Integer -> x + y;
-
 equalityTest(sumTwo(4, 5), 9, Integer, silver_tests);
 
 global curriedSum::((Integer ::= Integer) ::= Integer) = \ x::Integer -> \ y::Integer -> x + y;
-
 equalityTest(curriedSum(1)(2), 3, Integer, silver_tests);
 
 global differentTypes::((String ::= String) ::= Integer) = \ x::Integer -> \ x::String -> x;
-
 equalityTest(differentTypes(1)("abcd"), "abcd", String, silver_tests);
 
 global param::Integer = 4;
 equalityTest(addThree(param), 7, Integer, silver_tests);
+
+equalityTest(foldr(\a::Integer b::Integer -> b, 0, head(map(\b::Integer -> [b], [4]))), 4, Integer, silver_tests);
