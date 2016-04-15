6e09c4f4051f68434fa01f6f5f4d0f5e79ac4c2d
Lucas Kramer <krame505@umn.edu>
2016-04-15 16:32:01 -0500

    Added more tests

--- test/silver_features/Monad.sv
+++ test/silver_features/Monad.sv
@@ -1,11 +1,41 @@
 import core:monad;
 
-global monadRes1::[Integer] = do (bindList, returnList) {
+-- Test Maybe
+global monadRes1::Maybe<Integer> = do (bindMaybe, returnMaybe) {
+  a::Integer <- just(1);
+  if a > 0
+  then return 2;
+  else nothing();
+};
+
+equalityTest(monadRes1.isJust, true, Boolean, silver_tests);
+
+-- Test list
+global monadRes2::[Integer] = do (bindList, returnList) {
   a::Integer <- [1, 2, 3];
   b::Integer <- [4, 5, 6];
   return a * b;
 };
 
-global res::Integer = foldr(\a::Integer b::Integer -> a + b, 0, monadRes1);
+equalityTest(foldr(\a::Integer b::Integer -> a + b, 0, monadRes2), 90, Integer, silver_tests);
 
-equalityTest(res, 90, Integer, silver_tests);
+-- Test State
+global monadRes3::State<Integer Integer> = do (bindState, returnState) {
+  a::Integer <- getState();
+  setState(a + 1);
+  return a * 2;
+};
+
+global res3::Pair<Integer Integer> = runState(monadRes3, 2);
+
+equalityTest(res3.fst, 3, Integer, silver_tests);
+equalityTest(res3.snd, 4, Integer, silver_tests);
+
+-- Test IO
+global monadRes4::IOMonad<String> = do (bindState, returnState) {
+  writeFileM("test_out.txt", "Hello");
+  appendFileM("test_out.txt", ", World!");
+  readFileM("test_out.txt");
+};
+
+equalityTest(evalIO(monadRes4, unsafeIO()).iovalue, "Hello, World!", String, silver_tests);
