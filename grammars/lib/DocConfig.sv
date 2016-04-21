785902a8e8e9bd9987ba43d410f4c03c0a8b03e4
Charles Hofer <charlie.drummer.hofer@gmail.com>
2016-04-21 11:09:34 -0500

    Added extra grammar to generate documentation for libraries

--- grammars/lib/DocConfig.sv
+++
@@ -1,6 +0,0 @@
-grammar lib;
-
-{@config
-  header:"---\nlayout: sv_wiki\ntitle: Libraries\nmenu_title: Libraries\nmenu_weight: 100\n---"
-@}
-
--- grammars/silver/DocConfig.sv
+++
@@ -1,6 +0,0 @@
-grammar silver;
-
-{@config
-  header:"---\nlayout: sv_wiki\ntitle: Silver\nmenu_title: Silver\nmenu_weight: 100\n---"
-@}
-
