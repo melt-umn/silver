e7899572a8e40f6e8c5bb6d8c4fd6d0be67acdfd
Eric Van Wyk <evw@cs.umn.edu>
2016-05-22 15:48:33 -0500

    testing

--- grammars/silver/extension/doc/core/DclComment.sv
+++ grammars/silver/extension/doc/core/DclComment.sv
@@ -36,7 +36,7 @@ top::DclCommentComponent ::= '@link' '[' id::QName ']'
 layout {}
 {
   local dclInfo::DocDclInfo = head(treeLookup(id.lookupValue.fullName, top.docEnv));
-  top.body = "XX [" ++ dclInfo.id ++ "](" ++ dclInfo.path ++ ")";
+  top.body = "X2 " ++ dclInfo.path ++ " [" ++ dclInfo.id ++ "](" ++ dclInfo.path ++ ")";
 }
 
 concrete production componentText
