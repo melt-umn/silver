402702437966495a6135b803e8e9ffd7ba9b7693
Eric Van Wyk <evw@umn.edu>
2016-05-24 14:04:31 -0500

    more tweaks

--- grammars/core/DocConfig.sv
+++ grammars/core/DocConfig.sv
@@ -1,6 +1,23 @@
 grammar core;
 
 {@config
-  header:"---\nlayout: sv_wiki\ntitle: Core\nmenu_title: Core\nmenu_weight: 100\n---"
+  header:"---\nlayout: sv_wiki\ntitle: Core\nmenu_title: Core library\nmenu_weight: 20\n---"
+@}
+
+{@comment  
+
+   something
+
+   `head`
+
+```
+asdf 
+asdf
+asdf
+```
+
++ asdf
++ qwer
+
 @}
 
