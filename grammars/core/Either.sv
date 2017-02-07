4cc6bdeae84b275299329fb42ae1780f6e9554aa
Nathaniel Ringo <remexre@gmail.com>
2017-02-07 16:01:45 -0600

    Adds is(Left|Right)(Or)? functions.
    Alternatively, is{Left,Right}{Or,}.

--- grammars/core/Either.sv
+++ grammars/core/Either.sv
@@ -38,3 +38,51 @@ Pair<[a] [b]> ::= l::[Either<a b>]
   end;
 }
 
+}
+
+{--
+ - Returns the left value, raising an exception if there is none.
+ -}
+function toLeft
+a ::= e::Either<a b>
+{
+  return case e of
+  | left(a) -> a
+  end;
+}
+
+{--
+ - Returns the left value, or the default if there is no left value.
+ -}
+function toLeftOr
+a ::= e::Either<a b> o::a
+{
+  return case e of
+  | left(a) -> a
+  | right(_) -> o
+  end;
+}
+
+{--
+ - Returns the right value, raising an exception if there is none.
+ -}
+function toRight
+b ::= e::Either<a b>
+{
+  return case e of
+  | right(b) -> b
+  end;
+}
+
+{--
+ - Returns the right value, or the default if there is no right value.
+ -}
+function toRightOr
+b ::= e::Either<a b> o::b
+{
+  return case e of
+  | left(_) -> o
+  | right(b) -> b
+  end;
+}
+
