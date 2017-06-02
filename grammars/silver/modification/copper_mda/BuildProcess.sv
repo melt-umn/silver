f74a1b489422aadc108fee6bfd144833b229adf4
StephenPatrick <sythe2o0@gmail.com>
2017-06-02 14:55:52 -0500

    Copper build process overhaul, pre-testing

--- grammars/silver/modification/copper_mda/BuildProcess.sv
+++ grammars/silver/modification/copper_mda/BuildProcess.sv
@@ -6,88 +6,52 @@ import silver:translation:java:core only makeParserName, makeName;
 
 import silver:util:cmdargs;
 
--- TODO CRIPES this is all bad, bad, bad code.
-
 aspect production compilation
 top::Compilation ::= g::Grammars  _  buildGrammar::String  benv::BuildEnv
 {
-  top.postOps <- [generateMdaSpecs(g.compiledGrammars, grammarsToTranslate, benv.silverGen)]; 
+  top.postOps <-
+    foldr(\ a::Decorated RootSpec b::[DriverAction] ->
+      map(generateMdaSpec(g.compiledGrammars, _, benv.silverGen), a.mdaSpecs) ++ b,
+      [], grammarsToTranslate);
 
-  local targets :: String = mdaBuildTarget(grammarsToTranslate, benv.silverGen);
+  local targets :: String = 
+    foldr(\ a::Decorated RootSpec b::String ->
+      foldr(\ c::MdaSpec d::String -> 
+        mdaBuildSpecTarget(c, benv.silverGen) ++ d,
+        "", a.mdaSpecs) ++ b,
+      "", grammarsToTranslate);
+
   extraTopLevelDecls <- if length(targets) != 0 then ["  <target name='copper_mda'>\n" ++ targets ++ "  </target>\n"] else [];
   extraGrammarsDeps <- if length(targets) != 0 then ["copper_mda"] else [];
 }
 
-abstract production generateMdaSpecs
-top::DriverAction ::= grams::EnvTree<Decorated RootSpec>  searchgrams::[Decorated RootSpec]  silvergen::String
+abstract production generateMdaSpec
+top::DriverAction ::= grams::EnvTree<Decorated RootSpec>  spec::MdaSpec  silvergen::String
 {
-  top.io = generateMdaSpec(grams, searchgrams, silvergen ++ "src/", top.ioIn);
-  top.code = 0;
+  spec.compiledGrammars = grams;
+
+  local ast::SyntaxRoot = spec.cstAst;
+  local parserName::String = makeParserName(spec.fullName);
+  local copperFile::String = silvergen ++ grammarToPath(spec.sourceGrammar) ++ parserName ++ ".copper";
+  
+  local err :: IO = 
+    print("CST Errors while Testing MDA" ++ spec.fullName ++ ":\n" ++
+      foldr(\ a::String b::String -> 
+        a ++ "\n" ++ b, "", ast.cstErrors) ++
+      "\n", top.ioIn);
+
+  local printio::IO = print("MDA test file: " ++ spec.fullName ++ "\n", top.ioIn);
+  local writeio::IO = writeFile(copperFile, ast.xmlCopper, printio);
+
+  top.io = if null(ast.cstErrors) then writeio else err;
+  top.code = if null(ast.cstErrors) then 0 else 1;
   top.order = 5;
 }
 
-function generateMdaSpec
-IO ::= grams::EnvTree<Decorated RootSpec>  searchgrams::[Decorated RootSpec]  silvergen::String  i::IO
+function mdaBuildSpecTarget
+String ::= spec::MdaSpec  silvergen::String
 {
-  return if null(searchgrams) then i
-         else if null(head(searchgrams).mdaSpecs) then generateMdaSpec(grams, tail(searchgrams), silvergen, i)
-         else generateMdaSpec(grams, tail(searchgrams), silvergen, noReallyGenerateMdaSpecs(grams, head(searchgrams).mdaSpecs, silvergen, i));
+  return "    <copper useSkin='XML' runMDA='true' warnUselessNTs='false'>\n" ++
+         "      <inputs file='${src}/" ++ grammarToPath(spec.sourceGrammar) ++ 
+           makeParserName(spec.fullName) ++ ".copper'/>\n    </copper>\n";
 }
-
-function noReallyGenerateMdaSpecs
-IO ::= grams::EnvTree<Decorated RootSpec>  specs::[MdaSpec]  silvergen::String  i::IO
-{
-  local attribute p :: MdaSpec;
-  p = head(specs);
-  p.compiledGrammars = grams;
-  
-  local attribute ast :: SyntaxRoot;
-  ast = p.cstAst;
-  
-  local attribute parserName :: String;
-  parserName = makeParserName(p.fullName);
-
-  local attribute copperFile :: String;
-  copperFile = silvergen ++ grammarToPath(p.sourceGrammar) ++ parserName ++ ".copper";
-
-  local attribute printio :: IO;
-  printio = print("MDA test file: " ++ p.fullName ++ "\n", i);
-  
-  local attribute writeio :: IO;
-  writeio = writeFile(copperFile, ast.xmlCopper, printio);
-  
-  return if null(specs) then i
-         else noReallyGenerateMdaSpecs(grams, tail(specs), silvergen, writeio);
-}
-
-function mdaBuildTarget
-String ::= searchgrams::[Decorated RootSpec]  silvergen::String
-{
-  return if null(searchgrams) then ""
-         else if null(head(searchgrams).mdaSpecs) then mdaBuildTarget(tail(searchgrams), silvergen)
-         else noReallyMdaBuildTarget(head(searchgrams).mdaSpecs, silvergen) ++ mdaBuildTarget(tail(searchgrams), silvergen);
-}
-
-function noReallyMdaBuildTarget
-String ::= searchgrams::[MdaSpec]  silvergen::String
-{
-  local attribute p :: MdaSpec;
-  p = head(searchgrams);
-
-  local attribute parserName :: String;
-  parserName = makeParserName(p.fullName);
-  
-  local attribute packagename :: String;
-  packagename = makeName(p.sourceGrammar);
-  
-  local attribute packagepath :: String;
-  packagepath = grammarToPath(p.sourceGrammar);
-
-  local target :: String =
-"    <copper useSkin='XML' runMDA='true' warnUselessNTs='false'>\n" ++
-"      <inputs file='${src}/" ++ packagepath ++ parserName ++ ".copper'/>\n    </copper>\n";
-
-  return if null(searchgrams) then ""
-         else target ++ noReallyMdaBuildTarget(tail(searchgrams), silvergen);
-}
-
