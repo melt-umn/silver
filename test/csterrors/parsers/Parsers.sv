7fa5f91f2c560dd272eeebf639a23011098a27f4
StephenPatrick <sythe2o0@gmail.com>
2017-06-20 12:33:57 -0500

    Removed noConcrete test

--- test/csterrors/parsers/Parsers.sv
+++ test/csterrors/parsers/Parsers.sv
@@ -4,9 +4,7 @@ imports silver:testing;
 imports lib:extcore;
 imports disamb as dis;
 imports prodMod as pro;
---imports recNt as rec;
 imports startMissing as sta;
-imports noConcrete as ncc;
 imports mda as mda;
 exports host;
 
@@ -14,7 +12,5 @@ mainTestSuite copper_tests;
 
 equalityTest(dis:extendedParser("","").parseSuccess, false, Boolean, copper_tests );
 equalityTest(pro:extendedParser("","").parseSuccess, false, Boolean, copper_tests );
---equalityTest(rec:extendedParser("","").parseSuccess, false, Boolean, copper_tests );
 equalityTest(sta:extendedParser("","").parseSuccess, false, Boolean, copper_tests );
-equalityTest(ncc:extendedParser("","").parseSuccess, false, Boolean, copper_tests );
 equalityTest(mda:extendedParser("","").parseSuccess, false, Boolean, copper_tests );
