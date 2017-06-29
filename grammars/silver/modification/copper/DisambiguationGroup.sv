45ef0bc76ba5a25f4721f095498c76ef1a893f8d
StephenPatrick <sythe2o0@gmail.com>
2017-06-29 14:55:24 -0500

    Notes, split TermList to its own file

--- grammars/silver/modification/copper/DisambiguationGroup.sv
+++ grammars/silver/modification/copper/DisambiguationGroup.sv
@@ -7,8 +7,7 @@ top::AGDcl ::= 'disambiguate' terms::TermList acode::ActionCode_c
 {
   top.pp = "disambiguate " ++ terms.pp ++ " " ++ acode.pp;
 
-  top.errors := terms.errors;
-  top.errors <- acode.errors;
+  top.errors := terms.errors ++ acode.errors;
 
   acode.env = newScopeEnv(disambigLexemeDef(top.grammarName, top.location) ::
                             acode.defs ++ terms.defs, top.env);
@@ -19,55 +18,4 @@ top::AGDcl ::= 'disambiguate' terms::TermList acode::ActionCode_c
   acode.frame = disambiguationContext();
 
   top.syntaxAst = [syntaxDisambiguationGroup(fName,terms.termList,acode.actionCode)];
-}
-
-
-nonterminal TermList with config, grammarName, pp, location, termList, defs, errors, env;
-
-synthesized attribute termList :: [String];
-
-concrete production termListOne
-terms::TermList ::= t::QName
-{
-   forwards to termList(t,termListNull(location=t.location), location=t.location);
-}
-
-concrete production termListCons
-terms::TermList ::= t::QName ',' terms_tail::TermList
-{
-   forwards to termList(t,terms_tail,location=terms.location);
-}
-
-
-abstract production termList
-top::TermList ::= h::QName t::TermList
-{
-  top.pp = if t.pp == ""
-             then h.pp
-             else h.pp ++ ", " ++ t.pp;
-
-  production fName::String = h.lookupType.dcl.fullName;
-
-  top.termList = [fName] ++ t.termList ;
-  
-  -- Itd be nice if we didnt need this guard
-  top.defs = if null(h.lookupType.dcls) then t.defs 
-             else pluckTermDef(top.grammarName, h.location, fName) :: t.defs;
-  top.errors := t.errors;
-  
-  -- Since were looking it up in two ways, do the errors ourselves
-  top.errors <- if null(h.lookupType.dcls)
-                then [err(h.location, "Undeclared terminal '" ++ h.name ++ "'.")]
-                else if length(h.lookupType.dcls) > 1
-                then [err(h.location, "Ambiguous reference to terminal '" ++ h.name ++ "'. Possibilities are:\n" ++ printPossibilities(h.lookupType.dcls))]
-                else [];
-}
-
-abstract production termListNull
-top::TermList ::=
-{
-  top.termList = [];
-  top.defs = [];
-  top.pp = "";
-  top.errors := [];
 }
