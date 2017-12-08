7f00a3a2f7710abbea2d171c0c99d9d5a7a2f29b
Patrick Stephen <patrick.d.stephen@gmail.com>
2017-12-07 18:17:56 -0600

    Fiddling with fakeAspectProduction

--- grammars/silver/extension/bidirtransform/AspectProduction.sv
+++ grammars/silver/extension/bidirtransform/AspectProduction.sv
@@ -11,10 +11,9 @@ top::AGDcl ::= 'aspect' 'production' id::QName ns::AspectProductionSignature bod
 
   top.defs = [];
 
-  production namedSig :: NamedSignature = ns.namedSignature;
+  local namedSig :: NamedSignature = ns.namedSignature;
 
-  production attribute realSig :: NamedSignature;
-  realSig = if null(id.lookupValue.errors)
+  local attribute realSig :: NamedSignature = if null(id.lookupValue.errors)
             then id.lookupValue.dcl.namedSignature
             else bogusNamedSignature();
 
@@ -22,68 +21,43 @@ top::AGDcl ::= 'aspect' 'production' id::QName ns::AspectProductionSignature bod
 
   top.errors := id.lookupValue.errors ++ ns.errors ++ body.errors;
 
-  production attribute sigDefs :: [Def] with ++;
+  local attribute sigDefs :: [Def] with ++;
   sigDefs := ns.defs;
 
   ns.signatureName = id.lookupValue.fullName;
   ns.env = newScopeEnv(sigDefs, top.env);  
   ns.realSignature = if null(id.lookupValue.dcls) then [] else [realSig.outputElement] ++ realSig.inputElements;
 
-  local attribute prodAtts :: [Def];
-  prodAtts = if null(id.lookupValue.errors)
+  local prodAtts :: [Def] = if null(id.lookupValue.errors)
              then defsFromPADcls(getProdAttrs(id.lookupValue.fullName, top.env), namedSig)
              else [];
 
   body.env = newScopeEnv(body.defs ++ sigDefs, newScopeEnv(prodAtts, top.env));
   body.frame = aspectProductionContext(namedSig, myFlowGraph); -- graph from flow:env
-}
 
-aspect production fakeAspectProductionDcl
-top::AGDcl ::= 'aspect' 'production' id::QName ns::AspectProductionSignature body::ProductionBody
-{
   top.setupInh := body.setupInh;
   top.initProd := "\t\t//ASPECT PRODUCTION " ++ id.name ++ " " ++ ns.pp ++ "\n" ++ body.translation;
   top.valueWeaving := body.valueWeaving;
-}
 
-aspect production fakeAspectProductionDcl
-top::AGDcl ::= 'aspect' 'production' id::QName ns::AspectProductionSignature body::ProductionBody
-{
   top.docs := [bodilessDclCommentItem("aspect production", id.name, ns.pp, id.location.filename)];
-}
 
-aspect production fakeAspectProductionDcl
-top::AGDcl ::= 'aspect' 'production' id::QName ns::AspectProductionSignature body::ProductionBody 
-{
-    body.prodOutput = ns.namedSignature.outputElement;
-}
+  body.prodOutput = ns.namedSignature.outputElement;
 
-aspect production fakeAspectProductionDcl
-top::AGDcl ::= 'aspect' 'production' id::QName ns::AspectProductionSignature body::ProductionBody 
-{
-  production attribute allLexicalTyVars :: [String];
-  allLexicalTyVars = makeSet(ns.lexicalTypeVariables);
+  local allLexicalTyVars :: [String] = makeSet(ns.lexicalTypeVariables);
   
   sigDefs <- addNewLexicalTyVars_ActuallyVariables(top.grammarName, top.location, allLexicalTyVars);
-}
 
-aspect production fakeAspectProductionDcl
-top::AGDcl ::= 'aspect' 'production' id::QName ns::AspectProductionSignature body::ProductionBody 
-{
   -- TODO: bit of a hack, isn't it?
   local myGraphs :: EnvTree<ProductionGraph> = head(searchEnvTree(top.grammarName, top.compiledGrammars)).productionFlowGraphs;
 
   {-- Used by core to send down with .frame -}
-  production myFlowGraph :: ProductionGraph = 
+  local myFlowGraph :: ProductionGraph = 
     findProductionGraph(id.lookupValue.fullName, myGraphs);
 
   top.flowDefs = body.flowDefs;
-}
 
-aspect production fakeAspectProductionDcl
-top::AGDcl ::= 'aspect' 'production' id::QName ns::AspectProductionSignature body::ProductionBody 
-{
-  local attribute errCheck1 :: TypeCheck; errCheck1.finalSubst = ns.finalSubst;
+  local attribute errCheck1 :: TypeCheck; 
+  errCheck1.finalSubst = ns.finalSubst;
 
   errCheck1 = check(id.lookupValue.typerep, namedSig.typerep);
   top.errors <-
