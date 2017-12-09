be0b9f73b2f69277859febe8df49ca0c0c335cdc
Patrick Stephen <patrick.d.stephen@gmail.com>
2017-12-09 16:53:28 -0600

    Sanity check

--- grammars/silver/extension/bidirtransform/TransformDcl.sv
+++ grammars/silver/extension/bidirtransform/TransformDcl.sv
@@ -388,8 +388,8 @@ ag::AGDcls ::= 'transform' qn::QName '::' transType::TypeExpr
     --toForward.env = toEnv(nestedAgs.defs);
     --nestedAgs.env = toEnv(toForward.defs);
     
-    --ag.defs = nestedAgs.defs; 
-    ag.defs = toForward.defs ++ nestedAgs.defs;
+    ag.defs = nestedAgs.defs; 
+    --ag.defs = toForward.defs ++ nestedAgs.defs;
 
     --forwards to consAGDcls(toForward, nestedAgs, location=ag.location);
 }
