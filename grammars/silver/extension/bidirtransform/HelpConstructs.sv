2377703f284997ffde1122fc063da87ba68b345c
Patrick Stephen <patrick.d.stephen@gmail.com>
2017-12-10 16:19:19 -0600

    Disable origin attributes while refactoring

--- grammars/silver/extension/bidirtransform/HelpConstructs.sv
+++ grammars/silver/extension/bidirtransform/HelpConstructs.sv
@@ -125,8 +125,8 @@ top::AGDcl ::= dcl::Decorated NamedSignature fn::(ProductionStmts ::= Decorated 
     forwards to
         fakeAspectProductionDcl('aspect', 'production', 
         --aspectProductionDcl('aspect', 'production', 
-            qName(top.location, head(dcl).fullName), nsAspectProdSig(head(dcl), location=top.location), 
-            productionBody('{', fn(head(dcl)), '}', location=top.location), location=top.location);
+            qName(top.location, dcl.fullName), nsAspectProdSig(dcl, location=top.location), 
+            productionBody('{', fn(dcl), '}', location=top.location), location=top.location);
 
 }
 
