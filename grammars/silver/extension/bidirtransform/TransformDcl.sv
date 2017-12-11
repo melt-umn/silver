ed07a395d370a812947e534f7bca59aac7ac21c1
Patrick Stephen <patrick.d.stephen@gmail.com>
2017-12-11 00:15:59 -0600

    Figuring out where the error is coming from

--- grammars/silver/extension/bidirtransform/TransformDcl.sv
+++ grammars/silver/extension/bidirtransform/TransformDcl.sv
@@ -189,22 +189,22 @@ ag::AGDcls ::= 'transform' qn::QName '::' transType::TypeExpr
 
     -- restored$cncType attributes
     --
-    local agDcls9::AGDcl = foldl(\ agDcls::AGDcl lhs::String->
-        appendAGDcl(
-            fakeAspectProductionDcl('aspect', 'production',
-            --aspectProductionDcl('aspect', 'production', 
-            qName(ag.location, mkOriginName(lhs)), mkAspectProdSigDec("o", "Origin", "e", lhs, location=ag.location),
-                productionBody('{', foldl(\ stmts::ProductionStmts rhs::String ->
-                    if !hasRwID(newRwRules.rewriteRules, lhs, rhs) 
-                    then stmts -- this is also probably an error 
-                    else prdStmtList([
-                            attribDef("o", restoreNm(unFull(rhs)),
-                            --synAttrDef( "o", restoreNm(unFull(rhs)),  
-                                applyRw(rwID(newRwRules.rewriteRules, lhs, rhs), rhs, lhs, "e", location=ag.location), location=ag.location)
-                        ], location=ag.location),
-                productionStmtsNil(location=ag.location), cncNames), '}', location=ag.location), location=ag.location), agDcls, location=ag.location),
-        agDcls8, cncNames);
-    -- local agDcls14::AGDcl = agDcls13;
+    -- local agDcls9::AGDcl = foldl(\ agDcls::AGDcl lhs::String->
+    --     appendAGDcl(
+    --         fakeAspectProductionDcl('aspect', 'production',
+    --         --aspectProductionDcl('aspect', 'production', 
+    --         qName(ag.location, mkOriginName(lhs)), mkAspectProdSigDec("o", "Origin", "e", lhs, location=ag.location),
+    --             productionBody('{', foldl(\ stmts::ProductionStmts rhs::String ->
+    --                 if !hasRwID(newRwRules.rewriteRules, lhs, rhs) 
+    --                 then stmts -- this is also probably an error 
+    --                 else prdStmtList([
+    --                         attribDef("o", restoreNm(unFull(rhs)),
+    --                         --synAttrDef( "o", restoreNm(unFull(rhs)),  
+    --                             applyRw(rwID(newRwRules.rewriteRules, lhs, rhs), rhs, lhs, "e", location=ag.location), location=ag.location)
+    --                     ], location=ag.location),
+    --             productionStmtsNil(location=ag.location), cncNames), '}', location=ag.location), location=ag.location), agDcls, location=ag.location),
+    --     agDcls8, cncNames);
+    local agDcls9::AGDcl = agDcls8;
 
     -- Non-origin aspecting
 
