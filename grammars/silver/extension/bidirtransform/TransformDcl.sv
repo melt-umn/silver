bc8318d00ae22f179838582e87ccac5c9e37dbad
Patrick Stephen <patrick.d.stephen@gmail.com>
2017-12-11 00:24:53 -0600

    add back in agDcls13

--- grammars/silver/extension/bidirtransform/TransformDcl.sv
+++ grammars/silver/extension/bidirtransform/TransformDcl.sv
@@ -289,24 +289,22 @@ ag::AGDcls ::= 'transform' qn::QName '::' transType::TypeExpr
             location=ag.location), agDcls, location=ag.location),
         agDcls11, absProdDcls);
 
-    local agDcls13::AGDcl = agDcls12;
-
     -- top.transformed_$tName = ...
     --  if this abstract production has no transformation defined for it,
     --  then don't define this
     --  else if the rhs matches this transformation, 
     --    then true
     --    else false
-    -- local agDcls13::AGDcl = foldl(\ agDcls::AGDcl dcl::Decorated NamedSignature ->
-    --     if !hasTrans(trRules.transformRules, dcl, absGroup, cncGroup) then agDcls 
-    --     else appendAGDcl(aspectProdStmts(dcl,\ ns::Decorated NamedSignature ->
-    --         prdStmtList([
-    --             attribDef(ns.outputElement.elementName, transformNm(tName),
-    --             --synAttrDef( ns.outputElement.elementName, transformNm(tName),
-    --                 getTrans(trRules.transformRules, dcl, absGroup, cncGroup, location=ag.location).matchProd, location=ag.location)
-    --         ], location=ag.location),
-    --         location=ag.location), agDcls, location=ag.location),
-    --     agDcls12, absProdDcls);
+    local agDcls13::AGDcl = foldl(\ agDcls::AGDcl dcl::Decorated NamedSignature ->
+        if !hasTrans(trRules.transformRules, dcl, absGroup, cncGroup) then agDcls 
+        else appendAGDcl(aspectProdStmts(dcl,\ ns::Decorated NamedSignature ->
+            prdStmtList([
+                attribDef(ns.outputElement.elementName, transformNm(tName),
+                --synAttrDef( ns.outputElement.elementName, transformNm(tName),
+                    getTrans(trRules.transformRules, dcl, absGroup, cncGroup, location=ag.location).matchProd, location=ag.location)
+            ], location=ag.location),
+            location=ag.location), agDcls, location=ag.location),
+        agDcls12, absProdDcls);
 
     -- <rhs>.inhRedex_$tName = ...
     --  if this abstract production has no transformation defined for it,
