862ecb264e88d8daf5b9dc8a976898ee7845470a
Patrick Stephen <patrick.d.stephen@gmail.com>
2017-12-10 23:25:20 -0600

    Ok log the pp and the needAnnos

--- grammars/silver/extension/bidirtransform/HelpInject.sv
+++ grammars/silver/extension/bidirtransform/HelpInject.sv
@@ -6,45 +6,45 @@ top::Expr ::= toFill::Expr toInject::AnnoAppExprs needAnnos::[String]
     -- scan through e, find all applications,
     -- if they need these annotations, give them the annotations
     forwards to case toFill of        
-        -- | applicationEmpty(e, _, _) -> 
-        --     injectApplication(e, emptyAppExprs(location=toFill.location), emptyAnnoAppExprs(location=toFill.location),
-        --         toInject, needAnnos, location=toFill.location)
-        -- | applicationExpr(e, _, appexps, _) -> 
-        --     injectApplication(e, appexps, emptyAnnoAppExprs(location=toFill.location), 
-        --         toInject, needAnnos, location=toFill.location)
-        -- | application(e, _, appexps, _, annexps, _) ->
-        --     injectApplication(e, appexps, annexps, toInject, needAnnos, location=toFill.location)
-        -- --
-        -- --
-        -- | baseExpr(qn) -> toFill
-        -- | childReference(qn) -> toFill
-        -- | lhsReference(qn) -> toFill
-        -- | localReference(qn) -> toFill
-        -- | forwardReference(qn) -> toFill
-        -- | productionReference(qn) -> toFill
-        -- | functionReference(qn) -> toFill              
-        -- | globalValueReference(qn) -> toFill
-        -- | stringConst(s) -> toFill             
-        -- | intConst(i) -> toFill
-        -- --
-        -- --
-        -- | nestedExpr(_, e, _) -> injectAnnos(e,toInject, needAnnos, location=toFill.location) 
-        -- | terminalConstructor(a, b, c, d, e1, f, e2, g) ->  
-        --     terminalConstructor(a,b,c,d, 
-        --         injectAnnos(e1, toInject, needAnnos, location=toFill.location),
-        --         f,
-        --         injectAnnos(e2, toInject, needAnnos, location=toFill.location),
-        --         g, location=toFill.location)
-        -- | terminalFunction(a,b,c,d,ex,f) ->
-        --     terminalFunction(a,b,c,d,injectAnnos(ex, toInject, needAnnos, location=toFill.location), f, location=toFill.location)
-        -- | caseExpr(es, x, e, y) ->
-        --     caseExpr(
-        --         map(\ e2::Expr -> injectAnnos(e2, toInject, needAnnos, location=toFill.location), es),
-        --         x,
-        --         injectAnnos(e, toInject, needAnnos, location=toFill.location),
-        --         y, location=toFill.location)
-        -- | toStringFunction(a,b,e,c) -> toStringFunction(a,b,
-        --     injectAnnos(e,toInject, needAnnos,location=toFill.location), c, location=toFill.location)
+        | applicationEmpty(e, _, _) -> 
+            injectApplication(e, emptyAppExprs(location=toFill.location), emptyAnnoAppExprs(location=toFill.location),
+                toInject, needAnnos, location=toFill.location)
+        | applicationExpr(e, _, appexps, _) -> 
+            injectApplication(e, appexps, emptyAnnoAppExprs(location=toFill.location), 
+                toInject, needAnnos, location=toFill.location)
+        | application(e, _, appexps, _, annexps, _) ->
+            injectApplication(e, appexps, annexps, toInject, needAnnos, location=toFill.location)
+        --
+        --
+        | baseExpr(qn) -> toFill
+        | childReference(qn) -> toFill
+        | lhsReference(qn) -> toFill
+        | localReference(qn) -> toFill
+        | forwardReference(qn) -> toFill
+        | productionReference(qn) -> toFill
+        | functionReference(qn) -> toFill              
+        | globalValueReference(qn) -> toFill
+        | stringConst(s) -> toFill             
+        | intConst(i) -> toFill
+        --
+        --
+        | nestedExpr(_, e, _) -> injectAnnos(e,toInject, needAnnos, location=toFill.location) 
+        | terminalConstructor(a, b, c, d, e1, f, e2, g) ->  
+            terminalConstructor(a,b,c,d, 
+                injectAnnos(e1, toInject, needAnnos, location=toFill.location),
+                f,
+                injectAnnos(e2, toInject, needAnnos, location=toFill.location),
+                g, location=toFill.location)
+        | terminalFunction(a,b,c,d,ex,f) ->
+            terminalFunction(a,b,c,d,injectAnnos(ex, toInject, needAnnos, location=toFill.location), f, location=toFill.location)
+        | caseExpr(es, x, e, y) ->
+            caseExpr(
+                map(\ e2::Expr -> injectAnnos(e2, toInject, needAnnos, location=toFill.location), es),
+                x,
+                injectAnnos(e, toInject, needAnnos, location=toFill.location),
+                y, location=toFill.location)
+        | toStringFunction(a,b,e,c) -> toStringFunction(a,b,
+            injectAnnos(e,toInject, needAnnos,location=toFill.location), c, location=toFill.location)
         | _ -> errorExpr([err(toFill.location, "Unexpected expr type: " ++ toFill.ppDebug)], location=toFill.location)
     end;
 } 
@@ -55,6 +55,8 @@ top::Expr ::= toFill::Expr appExps::AppExprs annoExps::AnnoAppExprs
 {
     local baseAppExprs::AppExprs = injectAppExprs(appExps, toInject, needAnnos, location=toFill.location);
     local baseAnnoExprs::AnnoAppExprs = injectAnnoExprs(annoExps, toInject, needAnnos, location=toFill.location);
+
+    top.errors := [err(top.location, toFill.pp ++ foldl(\ s1::String s::String -> s1++"-"++s, needAnnos))];
 
     forwards to application(
         toFill, 
