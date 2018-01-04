f2d21b2d3c20f811daac62ea107b77cc9f651cde
Lucas Kramer <krame505@umn.edu>
2018-01-04 16:02:14 -0600

    Add an annotation to the reflection test

--- test/silver_features/Reflection.sv
+++ test/silver_features/Reflection.sv
@@ -3,7 +3,9 @@ import core:reflect;
 import silver:langutil;
 import silver:langutil:pp;
 
-nonterminal Expr;
+annotation lineNum::Integer;
+
+nonterminal Expr with lineNum;
 
 abstract production addExpr
 top::Expr ::= e1::Expr e2::Expr
@@ -21,10 +23,10 @@ abstract production decExpr
 top::Expr ::= d::Decorated Expr
 {}
 
-global testExpr::Expr = addExpr(intConstExpr(2), idExpr("asdf"));
+global testExpr::Expr = addExpr(intConstExpr(2, lineNum=1), idExpr("asdf", lineNum=2), lineNum=3);
 
 equalityTest(
-  show(80, reflectAST([testExpr, intConstExpr(5), decExpr(decorate testExpr with {})]).pp),
-  s"""[silver_features:addExpr(silver_features:intConstExpr(2), silver_features:idExpr("asdf")), silver_features:intConstExpr(5), silver_features:decExpr(<FOREIGN>)]""",
+  show(80, reflectAST([testExpr, intConstExpr(5, lineNum=4), decExpr(decorate testExpr with {}, lineNum=4)]).pp),
+  s"""[silver_features:addExpr(silver_features:intConstExpr(2, silver_features:lineNum=1), silver_features:idExpr("asdf", silver_features:lineNum=2), silver_features:lineNum=3), silver_features:intConstExpr(5, silver_features:lineNum=4), silver_features:decExpr(<FOREIGN>, silver_features:lineNum=4)]""",
   String, silver_tests);
 
