21a6921fe72e0104cdd8d1e55cc64b29757e4a2c
Joe Blanchard <blanc317@umn.edu>
2019-02-15 12:36:38 -0600

    Add support for empty productions in Silver by transforming the Treesitter grammar

--- grammars/silver/extension/treesitter/CstAst.sv
+++ grammars/silver/extension/treesitter/CstAst.sv
@@ -6,6 +6,7 @@ imports silver:definition:type;
 imports silver:definition:concrete_syntax;
 imports silver:definition:concrete_syntax:ast;
 imports silver:definition:regex;
+imports silver:util:raw:treemap;
 
 {-- Throughout the code the following abbreviations mean
   ts : Treesitter
@@ -39,6 +40,7 @@ synthesized attribute atom_scope :: String occurs on SyntaxDcl;
 -- A list of ts_prec_assoc_entrys
 synthesized attribute ts_prec_assoc_env :: [Pair<String Pair<Integer String>>] occurs on Syntax;
 synthesized attribute ts_extras :: String occurs on Syntax;
+synthesized attribute ts_conflicts :: String occurs on Syntax;
 synthesized attribute ts_terminal_rules :: String occurs on Syntax;
 synthesized attribute ts_nonterminal_rules :: [Pair<String [[String]]>] occurs on Syntax;
 synthesized attribute atom_scopes :: [String] occurs on Syntax;
@@ -81,8 +83,7 @@ lexer class atom_keyword_other;
 
 lexer class atom_markup;
 lexer class atom_markup_underline;
-lexer class atom_markup_underline_link;
-lexer class atom_markup_bold;
+
 lexer class atom_markup_heading;
 lexer class atom_markup_italic;
 lexer class atom_markup_list;
@@ -123,7 +124,6 @@ lexer class atom_variable_language;
 lexer class atom_variable_other;
 
 {-- ASPECT PRODUCTIONS --}
-
 aspect production cstRoot
 top::SyntaxRoot ::= parsername::String  startnt::String  s::Syntax  terminalPrefixes::[Pair<String String>]
 {
@@ -131,16 +131,27 @@ top::SyntaxRoot ::= parsername::String  startnt::String  s::Syntax  terminalPref
   -- the 'normalized' version from production attribute 's2'.  This groups productions with
   -- the same left hand side together as subdcls on nonterminals.
 
+  -- the empty string environment keeps track of which nonterminals can produce
+  -- the empty string. This is used to modify the grammar such that only the
+  -- nonterminal that can produce the empty string is the start nonterminal.
+  -- This is required for Treesitter grammars.
+  -- The environment is a list of nonterminal rule, true/false pairs where
+  -- true means the nonterminal can produce the emptty string.
+
   -- add precedence and associativity information
   -- this function also collapses the list of input elements for the production
   -- into a valid treesitter string
+  local attribute transformed_nts :: [Pair<String [[String]]>] =
+    transformEmptyStringRules(s2.ts_nonterminal_rules);
+
+  local attribute ts_startnt :: String = toTsDeclaration(startnt);
   local attribute nonterminal_rules :: [String] =
-    map(nonterminal_to_TS_string(s2.ts_prec_assoc_env, _), s2.ts_nonterminal_rules);
+    map(nonterminal_to_TS_string(s2.ts_prec_assoc_env, _), transformed_nts);
 
   -- partition the grammar rules so that the start nonterminal is the first rule
   -- listed in the tree-sitter grammar as needed.
   local attribute nonterminals_split :: Pair<[String] [String]> =
-    partition(isStartNonTerminalRule(toTsDeclaration(startnt), _), nonterminal_rules);
+    partition(isStartNonTerminalRule(ts_startnt, _), nonterminal_rules);
   local attribute start_rule :: [String] = nonterminals_split.fst;
   local attribute rest_rules :: [String] = nonterminals_split.snd;
 
@@ -153,6 +164,10 @@ module.exports = grammar({
 
   extras: $$ => [
     ${s2.ts_extras}
+  ],
+
+  conflicts: $$ => [
+    ${s2.ts_conflicts}
   ],
 
   rules: {
@@ -195,6 +210,7 @@ top::Syntax ::=
   top.ts_nonterminal_rules = [];
   top.ts_nonterminal_rules_rhs = [];
   top.ts_extras = "";
+  top.ts_conflicts = "";
   top.ts_prec_assoc_env = [];
   top.atom_scopes = [];
 }
@@ -204,8 +220,10 @@ top::Syntax ::= s1::SyntaxDcl s2::Syntax
 {
   -- consider an inherited attribute as the string used instead of "\n"
   -- six spaces of indentation after newline
+
+  -- only used for terminals for now
   top.ts_prec_assoc_env =
-    if (hasUsefulPrecAssocInfo(s1.ts_prec_assoc_entry.snd)) then
+    if (isTerminal(s1) && hasUsefulPrecAssocInfo(s1.ts_prec_assoc_entry.snd)) then
       cons(s1.ts_prec_assoc_entry, s2.ts_prec_assoc_env)
     else
       s2.ts_prec_assoc_env;
@@ -219,8 +237,8 @@ top::Syntax ::= s1::SyntaxDcl s2::Syntax
   -- this is used mainly for the Syntax nodes that group the productions
   -- underneath the nonterminals.
   top.ts_nonterminal_rules_rhs =
-    -- if it is a production with input elements
-    if (isProduction(s1) && !null(s1.ts_production_inputs)) then
+    -- treesitter should now work with empty productions
+    if (isProduction(s1)) then
       s1.ts_production_inputs :: s2.ts_nonterminal_rules_rhs
     else
       s2.ts_nonterminal_rules_rhs;
@@ -237,6 +255,12 @@ top::Syntax ::= s1::SyntaxDcl s2::Syntax
       TsDeclarationToIdentifier(s1.ts_lhs) ++ ",\n    " ++ s2.ts_extras
     else
       s2.ts_extras;
+
+  top.ts_conflicts =
+    if (isSyntaxDisambiguationGroup(s1)) then
+      s1.ts_lhs ++ ",\n    " ++ s2.ts_conflicts
+    else
+      s2.ts_conflicts;
 
   top.atom_scopes =
     if (isTerminal(s1)) then
@@ -303,6 +327,19 @@ top::SyntaxDcl ::= n::String modifiers::SyntaxLexerClassModifiers
 {
   -- irrelevant attributes
   top.ts_lhs = "";
+  top.ts_terminal_regex = "";
+  top.ts_production_inputs = [];
+  top.ts_nonterminal_rules_rhs = [];
+  top.ts_prec_assoc_entry = pair("", pair(0, "none"));
+  top.atom_scope = "";
+}
+
+
+aspect production syntaxDisambiguationGroup
+top::SyntaxDcl ::= n::String terms::[String] acode::String
+{
+  -- irrelevant attributes
+  top.ts_lhs = s"""[${implode(",", map(toTsIdentifier, terms))}]""";
   top.ts_terminal_regex = "";
   top.ts_production_inputs = [];
   top.ts_nonterminal_rules_rhs = [];
@@ -392,7 +429,7 @@ String ::= env::[Pair<String Pair<Integer String>>] nonterminalRHS::[[String]]
 function productions_to_TS_string
 String ::= env::[Pair<String Pair<Integer String>>] nonterminalProds::[[String]]
 {
-  return implode(",\n ", map(production_to_TS_string(env, _), nonterminalProds));
+  return implode(",\n    ", map(production_to_TS_string(env, _), nonterminalProds));
 }
 
 {--
@@ -403,7 +440,7 @@ function production_to_TS_string
 String ::= env::[Pair<String Pair<Integer String>>] prod_inputs::[String]
 {
   local attribute prec_assoc_info :: Maybe<Pair<Integer String>> =
-    dictionaryLookupList(stringEq, env, map(TsIdentifierToDeclaration, prod_inputs));
+    lookupByList(stringEq, env, map(TsIdentifierToDeclaration, prod_inputs));
   return
     if prec_assoc_info.isJust then
       prec_assoc_info_as_TS_string(prec_assoc_info.fromJust) ++
@@ -444,6 +481,135 @@ String ::= prod_inputs::[String]
     head(prod_inputs);
 }
 
+{-- EMPTY STRING NONTERMINALS GRAMMAR MODIFICATION FUNCTIONS --}
+
+{--
+  Transforms the grammar so that no nonterminals can produce the empty string
+  @param oldGrammar the original grammar
+  @return the new grammar with no nonterminals that can produce the empty string
+--}
+function transformEmptyStringRules
+[Pair<String [[String]]>] ::= oldGrammar::[Pair<String [[String]]>]
+{
+  local attribute emptyStringEnv :: [Pair<String Boolean>] =
+    map(buildEmptyStringEnvEntry, oldGrammar);
+  local attribute emptyStringRule::Maybe<String> =
+    getFirstEmptyStringRule(emptyStringEnv);
+  return
+    -- if any nonterminal can produce the empty string
+    if emptyStringRule.isJust then
+        transformEmptyStringRules(
+          emptyStringGrammarModification(emptyStringRule.fromJust, oldGrammar))
+    else oldGrammar;
+}
+
+{--
+  Modifies the oldGrammar so that the emptyRule can no longer produce the empty
+  string but the grammar is equivalent
+  @param emptyRule The rule that can produce the empty string.
+    This should be given as treesitter declaration (e.g. List_c not $.List_c)
+  @return the modified grammar. 
+--}
+function emptyStringGrammarModification
+[Pair<String [[String]]>] ::= emptyRule::String oldGrammar::[Pair<String [[String]]>]
+{
+  return
+    if (null(oldGrammar)) then []
+    -- remove the empty productions from the rule
+    else if stringEq(fst(head(oldGrammar)), emptyRule) then
+      pair(emptyRule, refactorEmptyRule(TsDeclarationToIdentifier(emptyRule), removeEmptyLists(snd(head(oldGrammar)))))
+      :: emptyStringGrammarModification(emptyRule, tail(oldGrammar))
+    else
+      pair(fst(head(oldGrammar)), refactorEmptyRule(TsDeclarationToIdentifier(emptyRule), snd(head(oldGrammar))))
+      :: emptyStringGrammarModification(emptyRule, tail(oldGrammar));
+}
+
+{--
+  @param emptyRule The rule that can produce the empty string.
+    This should be given as a treesitter identifier (e.g. $.List_c not List_c)
+  @param oldProductions The old productions of the nonterminal
+
+  @return the new productions for the nonterminal adding productions so that
+    all productions that have elements that can produce the empty string are
+    removed in a newly added production.
+    e.g. if old productions were [ [$.notEmpty, $.canBeEmpty] ] the function
+      would return [ [$.notEmpty], [$.notEmpty, $.canBeEmpty] ]
+--}
+function refactorEmptyRule
+[[String]] ::= emptyRule::String oldProductions::[[String]]
+{
+  return
+    if null(oldProductions) then []
+    else if containsBy(stringEq, emptyRule, head(oldProductions)) then
+      append(
+        accountForEmptyRuleWithinProd(emptyRule, head(oldProductions)),
+        refactorEmptyRule(emptyRule, tail(oldProductions)))
+    else head(oldProductions) :: refactorEmptyRule(emptyRule, tail(oldProductions));
+}
+
+{--
+  Creates a list of new productions from an original production where every
+  instance of the emptyRule in the production is both included and excluded
+  @param emptyRule The rule that can produce the empty string.
+    This should be given as a treesitter identifier (e.g. $.List_c not List_c)
+  @param prod The production - the list of inputs to the production
+  @return The list of new productions that are equivalent to the original production
+  assuming the rule that used to be able to produce the empty string no longer can.
+--}
+function accountForEmptyRuleWithinProd
+[[String]] ::= emptyRule::String prod::[String]
+{
+  return
+    if null(prod) then [ [] ]
+    else if (stringEq(emptyRule, head(prod))) then
+      -- split by including the empty rule and excluding the empty rule
+      append(
+        consToAll(head(prod), accountForEmptyRuleWithinProd(emptyRule, tail(prod))),
+        accountForEmptyRuleWithinProd(emptyRule, tail(prod)))
+    else
+      consToAll(head(prod), accountForEmptyRuleWithinProd(emptyRule, tail(prod)));
+}
+{--
+  - Builds an entry in the empty string environment.
+  @param non_terminal The nonterminal for which the entry in the environment corresponds to
+  @return A pair specifying the nonterminal name and a Boolean indicating if
+    it can produce the empty string.
+--}
+function buildEmptyStringEnvEntry
+Pair<String Boolean> ::= non_terminal::Pair<String [[String]]>
+{
+  -- emptyListWithinListOfLists is equivalent to determining if a nonterminal
+  -- can produce the empty string
+  return pair(fst(non_terminal), emptyListWithinListOfLists(snd(non_terminal)));
+}
+
+{--
+  Checks if any nonterminal can produce the empty string
+  @param emptyStringEnv The empty string environment
+  @return true if any nonterminal
+--}
+function hasEmptyStringRules
+Boolean ::= emptyStringEnv::[Pair<String Boolean>]
+{
+  return any(map(snd, emptyStringEnv));
+}
+
+{--
+  Gets the first nonterminal in the empty string environment list that can produce
+  the empty string if one exists.
+  @param emptyStringEnv The empty string environment
+  @return The first nonterminal in the empty string environment that produces
+    the empty string if it exists.
+--}
+function getFirstEmptyStringRule
+Maybe<String> ::= emptyStringEnv::[Pair<String Boolean>]
+{
+  return
+    if null(emptyStringEnv) then nothing()
+    else if snd(head(emptyStringEnv)) then just(fst(head(emptyStringEnv)))
+    else getFirstEmptyStringRule(tail(emptyStringEnv));
+}
+
 {-- GETTER FUNCTIONS --}
 -- creates a pair of the precedence and associativity from the terminal modifiers
 function getPrecAssocInfo
@@ -473,10 +639,7 @@ function isTerminal
 Boolean ::= declaration::Decorated SyntaxDcl
 {
   return case declaration of
-  | syntaxLexerClass(_, _) -> false
   | syntaxTerminal(_, _, _) -> true
-  | syntaxNonterminal(_, _) -> false
-  | syntaxProduction(_, _) -> false
   | _ -> false
   end;
 }
@@ -485,10 +648,7 @@ function isIgnoreTerminal
 Boolean ::= declaration::Decorated SyntaxDcl
 {
   return case declaration of
-  | syntaxLexerClass(_, _) -> false
   | syntaxTerminal(_, _, modifiers) -> modifiers.ignored
-  | syntaxNonterminal(_, _) -> false
-  | syntaxProduction(_, _) -> false
   | _ -> false
   end;
 }
@@ -497,10 +657,7 @@ function isNonTerminal
 Boolean ::= declaration::Decorated SyntaxDcl
 {
   return case declaration of
-  | syntaxLexerClass(_, _) -> false
-  | syntaxTerminal(_, _, _) -> false
   | syntaxNonterminal(_, _) -> true
-  | syntaxProduction(_, _) -> false
   | _ -> false
   end;
 }
@@ -509,9 +666,6 @@ function isProduction
 Boolean ::= declaration::Decorated SyntaxDcl
 {
   return case declaration of
-  | syntaxLexerClass(_, _) -> false
-  | syntaxTerminal(_, _, _) -> false
-  | syntaxNonterminal(_, _) -> false
   | syntaxProduction(_, _) -> true
   | _ -> false
   end;
@@ -522,9 +676,15 @@ Boolean ::= declaration::Decorated SyntaxDcl
 {
   return case declaration of
   | syntaxLexerClass(_, _) -> true
-  | syntaxTerminal(_, _, _) -> false
-  | syntaxNonterminal(_, _) -> false
-  | syntaxProduction(_, _) -> false
+  | _ -> false
+  end;
+}
+
+function isSyntaxDisambiguationGroup
+Boolean ::= declaration::Decorated SyntaxDcl
+{
+  return case declaration of
+  | syntaxDisambiguationGroup(_, _, _) -> true
   | _ -> false
   end;
 }
@@ -596,6 +756,46 @@ String ::= str::String
   return substring(33, length(str), str);
 }
 
+{-- GENERAL PURPOSE LIST OF LIST MANIPULATION FUNCTIONS --}
+
+{--
+  Determines if a list of lists contains the empty list as one of its elements.
+  This is equivalent to determining if a nonterminal can produce the empty string.
+  @param listOfLists the list of lists (in this case the nonterminal productions)
+  @return true if a list of lists contains the empty list as one of its elements. False otherwise.
+--}
+-- can the nonterminal produce the empty string.
+-- this is equivalent to the nonterminal having a production with no input elements.
+function emptyListWithinListOfLists
+Boolean ::= listOfLists::[[a]]
+{
+  return any(map(null, listOfLists));
+}
+
+{--
+  Removes empty list elements from lists of lists
+  @param listOfLists the list of lists
+  @return the original list of lists with empty list elements removed
+--}
+function removeEmptyLists
+[[a]] ::= listOfLists::[[a]]
+{
+  return snd(partition(null, listOfLists));
+}
+
+
+{--
+  ConsToAll cons elem to each list within the list of lists
+  @param elem The element to cons onto each list in the list of lists
+  @param listOfLists The list of lists
+  @return The list of lists with elem cons to each list within the list of lists
+--}
+function consToAll
+[[a]] ::= elem::a listOfLists::[[a]]
+{
+  return map(cons(elem, _), listOfLists);
+}
+
 {-- GENERAL PURPOSE STRING MANIPULATION FUNCTIONS --}
 
 {--
@@ -633,29 +833,11 @@ String ::= str::String
   first match found by the earliest key in the key list. If no key matches
   then nothing is returned.
 --}
-function dictionaryLookupList
+function lookupByList
 Maybe<b> ::= eq::(Boolean ::= a a) dict::[Pair<a b>] keyList::[a]
 {
   return
   if null(keyList) then nothing()
   else
-    orElse(dictionaryLookup(eq, dict, head(keyList)), dictionaryLookupList(eq, dict, tail(keyList)));
-}
-
-{-- GENERAL PURPOSE LIST FUNCTIONS --}
-
-{--
-  Returns the value associated with the given key if the key exists in the
-  dictionary.
---}
-function dictionaryLookup
-Maybe<b> ::= eq::(Boolean ::= a a) dict::[Pair<a b>] key::a
-{
-  return
-    if null(dict) then
-      nothing()
-    else if eq(head(dict).fst, key) then
-      just(head(dict).snd)
-    else
-      dictionaryLookup(eq, tail(dict), key);
+    orElse(lookupBy(eq, head(keyList), dict), lookupByList(eq, dict, tail(keyList)));
 }
