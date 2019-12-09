3c59dbac3958fe08ef115ff2b4081d0ac56c7323
Lucas Kramer <krame505@umn.edu>
2019-12-09 17:25:48 -0600

    Fix bug in last commit

--- grammars/core/TerminalId.sv
+++ grammars/core/TerminalId.sv
@@ -5,15 +5,17 @@ Boolean ::= t1::TerminalId  t2::TerminalId
 {
   return t1 == t2;
 }
+
+function terminalIdLte
+Boolean ::= t1::TerminalId  t2::TerminalId
+{
+  return t1 <= t2;
+}
  
 function terminalSetEq
 Boolean ::= ts1::[TerminalId] ts2::[TerminalId]
 {
   return
     length(ts1) == length(ts2) &&
-    all(
-      zipWith(
-        terminalIdEq,
-        map(sortBy(terminalIdEq, _), ts1),
-        map(sortBy(terminalIdEq, _), ts2)));
+    all(zipWith(terminalIdEq, sortBy(terminalIdLte, ts1), sortBy(terminalIdLte, ts2)));
 }
