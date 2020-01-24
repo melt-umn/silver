ca0273dbdd5af4df13f22e218932880c6fe49380
Lucas Kramer <krame505@umn.edu>
2020-01-24 17:57:04 -0600

    Use the right test suite

--- test/silver_features/rewrite/Tests.sv
+++ test/silver_features/rewrite/Tests.sv
@@ -2,7 +2,7 @@ grammar silver_features:rewrite;
 
 imports silver:testing;
 imports lib:extcore;
-imports stdlib;
+imports silver_features;
 
 imports silver:rewrite as s;
 imports silver:reflect;
@@ -24,17 +24,17 @@ global s1::s:Strategy =
   | [1, 2, 3] -> [4, 5, 6]
   end;
 
-equalityTest(showRes(rewriteWith(s1, [1, 2, 3])), "[4, 5, 6]", String, core_tests);
-equalityTest(showRes(rewriteWith(s1, [3, 2, 1])), "fail", String, core_tests);
+equalityTest(showRes(rewriteWith(s1, [1, 2, 3])), "[4, 5, 6]", String, silver_tests);
+equalityTest(showRes(rewriteWith(s1, [3, 2, 1])), "fail", String, silver_tests);
 
 global s2::s:Strategy =
   rule on [a] of
   | a :: [_, b, c] -> b :: [c, a]
   end;
 
-equalityTest(showRes(rewriteWith(s2, [1, 2, 3, 4])), "[3, 4, 1]", String, core_tests);
-equalityTest(showRes(rewriteWith(s2, [true, false, true, false])), "[true, false, true]", String, core_tests);
-equalityTest(showRes(rewriteWith(s2, ["a", "b", "c", "d"])), "[\"c\", \"d\", \"a\"]", String, core_tests);
+equalityTest(showRes(rewriteWith(s2, [1, 2, 3, 4])), "[3, 4, 1]", String, silver_tests);
+equalityTest(showRes(rewriteWith(s2, [true, false, true, false])), "[true, false, true]", String, silver_tests);
+equalityTest(showRes(rewriteWith(s2, ["a", "b", "c", "d"])), "[\"c\", \"d\", \"a\"]", String, silver_tests);
 
 global s3::s:Strategy =
   rule on Pair<Integer Integer> of
@@ -42,8 +42,8 @@ global s3::s:Strategy =
   | pair(a, b) when a == b -> pair(a, 17)
   end;
 
-equalityTest(showRes(rewriteWith(s3, pair(1, 2))), "core:pair(2, 1)", String, core_tests);
-equalityTest(showRes(rewriteWith(s3, pair(42, 42))), "core:pair(42, 17)", String, core_tests);
+equalityTest(showRes(rewriteWith(s3, pair(1, 2))), "core:pair(2, 1)", String, silver_tests);
+equalityTest(showRes(rewriteWith(s3, pair(42, 42))), "core:pair(42, 17)", String, silver_tests);
 
 global s4::s:Strategy =
   rule on Pair<Integer Integer> of
@@ -51,8 +51,8 @@ global s4::s:Strategy =
   | pair(a, b) when a != b -> pair(b, a)
   end;
 
-equalityTest(showRes(rewriteWith(s4, pair(1, 2))), "core:pair(2, 1)", String, core_tests);
-equalityTest(showRes(rewriteWith(s4, pair(42, 42))), "core:pair(42, 17)", String, core_tests);
+equalityTest(showRes(rewriteWith(s4, pair(1, 2))), "core:pair(2, 1)", String, silver_tests);
+equalityTest(showRes(rewriteWith(s4, pair(42, 42))), "core:pair(42, 17)", String, silver_tests);
 
 global s5::s:Strategy =
   rule on Pair<Integer String> of
@@ -61,8 +61,8 @@ global s5::s:Strategy =
   | a -> a
   end;
 
-equalityTest(showRes(rewriteWith(s5, pair(123, "4"))), "core:pair(4, \"123\")", String, core_tests);
-equalityTest(showRes(rewriteWith(s5, pair(467, "foo"))), "core:pair(467, \"foo\")", String, core_tests);
+equalityTest(showRes(rewriteWith(s5, pair(123, "4"))), "core:pair(4, \"123\")", String, silver_tests);
+equalityTest(showRes(rewriteWith(s5, pair(467, "foo"))), "core:pair(467, \"foo\")", String, silver_tests);
 
 global s6::s:Strategy =
   rule on Pair<Integer String> of
@@ -73,15 +73,16 @@ global s6::s:Strategy =
     end
   end;
 
-equalityTest(showRes(rewriteWith(s6, pair(123, "4"))), "core:pair(4, \"123\")", String, core_tests);
-equalityTest(showRes(rewriteWith(s6, pair(467, "foo"))), "core:pair(467, \"foo\")", String, core_tests);
+equalityTest(showRes(rewriteWith(s6, pair(123, "4"))), "core:pair(4, \"123\")", String, silver_tests);
+equalityTest(showRes(rewriteWith(s6, pair(467, "foo"))), "core:pair(467, \"foo\")", String, silver_tests);
 
 global s7::s:Strategy =
   rule on Pair<Integer Integer> of
   | p -> pair(p.snd, p.fst)
   end;
 
-equalityTest(showRes(rewriteWith(s7, pair(123, 456))), "core:pair(456, 123)", String, core_tests);
+equalityTest(showRes(rewriteWith(s7, pair(123, 456))), "core:pair(456, 123)", String, silver_tests);
+
 
 synthesized attribute isEqual::Boolean;
 inherited attribute isEqualTo::Foo;
@@ -99,6 +100,6 @@ global s8::s:Strategy =
   | f1 :: f2 :: rest when decorate f1 with {isEqualTo = f2;}.isEqual -> f1 :: rest 
   end;
 
-equalityTest(showRes(rewriteWith(s8, [foo(1), foo(2), foo(3)])), "fail", String, core_tests);
-equalityTest(showRes(rewriteWith(s8, [foo(2), foo(2), foo(3)])), "[stdlib:rewrite:foo(2), stdlib:rewrite:foo(3)]", String, core_tests);
+equalityTest(showRes(rewriteWith(s8, [foo(1), foo(2), foo(3)])), "fail", String, silver_tests);
+equalityTest(showRes(rewriteWith(s8, [foo(2), foo(2), foo(3)])), "[silver_features:rewrite:foo(2), silver_features:rewrite:foo(3)]", String, silver_tests);
 
--- test/silver_features/rewrite/expreval/Tests.sv
+++ test/silver_features/rewrite/expreval/Tests.sv
@@ -2,20 +2,20 @@ grammar silver_features:rewrite:expreval;
 
 import silver:testing;
 import lib:extcore;
-import stdlib;
+import silver_features;
 
 global result1::Maybe<Expr> = rewriteWith(eval, parseExpr("1 + (2 * 3)"));
-equalityTest(result1.isJust, true, Boolean, core_tests);
-equalityTest(showExpr(fromMaybe(const(12345), result1)), "7", String, core_tests);
+equalityTest(result1.isJust, true, Boolean, silver_tests);
+equalityTest(showExpr(fromMaybe(const(12345), result1)), "7", String, silver_tests);
 
 global result2::Maybe<Expr> = rewriteWith(eval, parseExpr("7 + 4 - ((1 + 1) / 0)"));
-equalityTest(result2.isJust, true, Boolean, core_tests);
-equalityTest(showExpr(fromMaybe(const(12345), result2)), "-2 / 0", String, core_tests);
+equalityTest(result2.isJust, true, Boolean, silver_tests);
+equalityTest(showExpr(fromMaybe(const(12345), result2)), "-2 / 0", String, silver_tests);
 
 global result3::Maybe<Expr> = rewriteWith(eval, parseExpr("(2 + 2) / 6"));
-equalityTest(result3.isJust, true, Boolean, core_tests);
-equalityTest(showExpr(fromMaybe(const(12345), result3)), "2 / 3", String, core_tests);
+equalityTest(result3.isJust, true, Boolean, silver_tests);
+equalityTest(showExpr(fromMaybe(const(12345), result3)), "2 / 3", String, silver_tests);
 
 global result4::Maybe<Expr> = rewriteWith(eval, parseExpr("1 + 1 / (1 + 1 / (1 + 1 / (1 + 1 / (1 + 1 / (1 + 1 / (1 + 1))))))"));
-equalityTest(result4.isJust, true, Boolean, core_tests);
-equalityTest(showExpr(fromMaybe(const(12345), result4)), "34 / 21", String, core_tests);
+equalityTest(result4.isJust, true, Boolean, silver_tests);
+equalityTest(showExpr(fromMaybe(const(12345), result4)), "34 / 21", String, silver_tests);
