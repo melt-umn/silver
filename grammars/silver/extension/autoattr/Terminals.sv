b67b458a0ff698cb453a1caddedeabae1ea1b8ec
Lucas Kramer <krame505@umn.edu>
2020-04-02 14:33:11 -0500

    Implement except clause on global propagate

--- grammars/silver/extension/autoattr/Terminals.sv
+++ grammars/silver/extension/autoattr/Terminals.sv
@@ -1,6 +1,7 @@
 grammar silver:extension:autoattr;
 
 terminal Propagate_kwd 'propagate' lexer classes {KEYWORD,RESERVED};
+terminal Except_kwd    'except'    lexer classes {KEYWORD};
 
 terminal Functor_kwd   'functor' lexer classes {KEYWORD,RESERVED};
 terminal Monoid_kwd    'monoid'  lexer classes {KEYWORD,RESERVED};
