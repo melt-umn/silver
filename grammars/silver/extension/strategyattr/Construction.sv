2195e753b368795070e537d938a6fa1ad1282f8b
Lucas Kramer <krame505@umn.edu>
2020-04-13 19:32:47 -0500

    Improved version of quoteStrategyExpr, allow base genName for generated term to be specified at runtime

--- grammars/silver/extension/strategyattr/Construction.sv
+++ grammars/silver/extension/strategyattr/Construction.sv
@@ -2,16 +2,32 @@ grammar silver:extension:strategyattr;
 
 import silver:reflect;
 import silver:metatranslation;
+import silver:rewrite as s;
 
 terminal SilverStrategyExpr_t 'Silver_StrategyExpr' lexer classes {KEYWORD, RESERVED};
 terminal AntiquoteStrategyExpr_t '$StrategyExpr' lexer classes {Antiquote, Strategy};
 
-concrete production quoteStrategyExpr
+concrete production quoteStrategyExprOld
 top::Expr ::= 'Silver_StrategyExpr' genName::Name '{' cst::StrategyExpr_c '}'
 {
   top.unparse = s"Silver_StrategyExpr {${cst.unparse}}";
   cst.givenGenName = genName.name ++ "_" ++ toString(genInt());
   forwards to translate(top.location, reflect(cst.ast));
+}
+
+concrete production quoteStrategyExpr
+top::Expr ::= 'Silver_StrategyExpr' '(' genName::Expr ')' '{' cst::StrategyExpr_c '}'
+{
+  top.unparse = s"Silver_StrategyExpr {${cst.unparse}}";
+  cst.givenGenName = "";
+  forwards to
+    rewriteWith(
+      s:allTopDown(
+        rule on AnnoExpr of
+        | annoExpr(n, _, presentAppExpr(e), location=l) when n.name == "genName" ->
+          annoExpr(n, '=', presentAppExpr(plusPlus(genName, '++', e, location=l), location=l), location=l)
+        end),
+        translate(top.location, reflect(cst.ast))).fromJust;
 }
 
 concrete production antiquoteStrategyExpr_c
