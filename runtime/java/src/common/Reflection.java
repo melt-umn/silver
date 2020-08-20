b41f3ea3f0c9f6f93121c8ca5805780e8d2eed69
Louis Goessling <louis@goessling.com>
2020-08-20 16:43:42 -0500

    Fix reflection regression with decorated nodes

--- runtime/java/src/common/Reflection.java
+++ runtime/java/src/common/Reflection.java
@@ -69,8 +69,6 @@ public final class Reflection {
 	 * @return The reflected AST.
 	 */
 	public static NAST reflect(final ConsCell rules, Object o) {
-		if (o instanceof DecoratedNode) o = ((DecoratedNode)o).undecorate();
-
 		core.NOriginInfo origin = new core.PoriginOriginInfo(null, OriginsUtil.SET_FROM_REFLECTION_OIT, o, rules, true);
 		if(o instanceof Node) {
 			Node n = (Node)o;
@@ -101,7 +99,6 @@ public final class Reflection {
 		} else if(o instanceof Boolean) {
 			return new PbooleanAST(origin, (Boolean)o);
 		} else {
-			//if (System.getProperty("silver.origins.reflwarn")!=null) System.err.println("Reflection Warn: constructing PanyAST("+o.getClass().getName()+" "+o.toString()+")");
 			return new PanyAST(origin, o);
 		}
 	}
