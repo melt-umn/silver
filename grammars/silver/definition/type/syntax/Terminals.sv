eed856243cb8f2c9da4e3aae700dd96502bb4ae8
Lucas Kramer <krame505@umn.edu>
2020-12-26 16:05:43 -0600

    Fix ambiguity in TypeExpr with precedence

--- grammars/silver/definition/type/syntax/Terminals.sv
+++ grammars/silver/definition/type/syntax/Terminals.sv
@@ -1,7 +1,9 @@
 grammar silver:definition:type:syntax;
 
+-- '<' has precedence 9, assoc = left
+
 terminal Boolean_tkwd    'Boolean'    lexer classes {TYPE,RESERVED};
-terminal Decorated_tkwd  'Decorated'  lexer classes {TYPE,RESERVED};
+terminal Decorated_tkwd  'Decorated'  lexer classes {TYPE,RESERVED}, precedence=1;
 terminal Float_tkwd      'Float'      lexer classes {TYPE,RESERVED};
 terminal Integer_tkwd    'Integer'    lexer classes {TYPE,RESERVED};
 terminal String_tkwd     'String'     lexer classes {TYPE,RESERVED};
