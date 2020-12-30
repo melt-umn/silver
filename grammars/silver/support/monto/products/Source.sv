1f1f68dc94740add0f540e03db998b121a27b352
Lucas Kramer <krame505@umn.edu>
2020-12-30 17:43:06 -0600

    Removing obsolete monto code, fixes #403

--- grammars/silver/support/monto/products/Source.sv
+++
@@ -1,9 +0,0 @@
-grammar silver:support:monto:products;
-
-import silver:json;
-
-abstract production sourceProduct
-top::ProductValue ::= source::String
-{
-  forwards to productValue("source", jsonString(source));
-}
