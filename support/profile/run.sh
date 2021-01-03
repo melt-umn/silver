dee9808533aa3b341589db6db45eabb3a342b179
Lucas Kramer <krame505@umn.edu>
2021-01-03 14:18:23 -0600

    Renaming grammars in scripts

--- support/profile/run.sh
+++ support/profile/run.sh
@@ -8,14 +8,14 @@ fi
 mkdir -p build
 cd build
 
-java -Xss8M -Xmx3000M -Xrunhprof:heap=sites,cpu=samples -jar ../jars/silver.composed.Default.jar --clean silver:composed:Default
+java -Xss8M -Xmx3000M -Xrunhprof:heap=sites,cpu=samples -jar ../jars/silver.composed.Default.jar --clean silver:compiler:composed:Default
 
 # Heap profile:
-#java -Xss8M -Xmx3000M -Xrunhprof:heap=sites,depth=2 -jar ../jars/silver.composed.Default.jar --clean silver:composed:Default
+#java -Xss8M -Xmx3000M -Xrunhprof:heap=sites,depth=2 -jar ../jars/silver.composed.Default.jar --clean silver:compiler:composed:Default
 
 # Deep CPU stack profile:
-#java -Xss8M -Xmx3000M -Xrunhprof:cpu=samples,depth=12 -jar ../jars/silver.composed.Default.jar --clean silver:composed:Default
+#java -Xss8M -Xmx3000M -Xrunhprof:cpu=samples,depth=12 -jar ../jars/silver.composed.Default.jar --clean silver:compiler:composed:Default
 
 # Kinda useless CPU profile
-#java -Xss8M -Xmx3000M -Xrunhprof:cpu=samples,depth=1,interval=1 -jar ../jars/silver.composed.Default.jar --clean silver:composed:Default
+#java -Xss8M -Xmx3000M -Xrunhprof:cpu=samples,depth=1,interval=1 -jar ../jars/silver.composed.Default.jar --clean silver:compiler:composed:Default
 
