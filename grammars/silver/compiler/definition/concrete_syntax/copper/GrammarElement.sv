f92cfc142b2da64822260fa78d178dc88ae8e9ca
Nathan Ringo <nathan@remexre.xyz>
2021-01-06 12:18:27 -0800

    Possibly found a bug.
        (DN.319): While evaling syn 'silver:langutil:errors' in 'silver:compiler:definition:core:attributionDcl' (3d356e85, Regex.sv:9:0)
        (CA.62): Error evaluating base of collection attribute silver:langutil:errors via forward of silver:compiler:definition:core:attributionDcl
        (DN.319): While evaling syn 'silver:langutil:errors' in 'silver:compiler:definition:core:defaultAttributionDcl' (4778db1f, Regex.sv:9:0)
        (DN.319): While evaling syn 'silver:compiler:definition:env:dcl' in 'silver:compiler:definition:core:customLookup' (1b19e5bf)
        (silver.core.Perror in Perror.java:98): Error while evaluating function silver:core:error
        (U.91): INTERNAL ERROR: Accessing dcl of type RegexSeq at Regex.sv:9:65
    
    EDIT: When rebasing this, got
    
        Tried to get monadToString for a non-monadic type (silver:compiler:definition:concrete_syntax:copper:CharSet)

---
+++ grammars/silver/compiler/definition/concrete_syntax/copper/GrammarElement.sv
@@ -0,0 +1,16 @@
+grammar silver:compiler:definition:concrete_syntax:copper;
+
+-- edu.umn.cs.melt.copper.compiletime.spec.grammarbeans.GrammarElement
+type GrammarElement foreign;
+
+-- edu.umn.cs.melt.copper.compiletime.spec.grammarbeans.DisambiguationFunction
+
+-- edu.umn.cs.melt.copper.compiletime.spec.grammarbeans.GrammarSymbol
+
+-- edu.umn.cs.melt.copper.compiletime.spec.grammarbeans.OperatorClass
+
+-- edu.umn.cs.melt.copper.compiletime.spec.grammarbeans.ParserAttribute
+
+-- edu.umn.cs.melt.copper.compiletime.spec.grammarbeans.Production
+
+-- edu.umn.cs.melt.copper.compiletime.spec.grammarbeans.TerminalClass
