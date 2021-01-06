c92f07622bc8a1ef8ee8628a732b78613e9e90a4
Nathan Ringo <nathan@remexre.xyz>
2021-01-06 12:18:27 -0800

    Possibly found a bug.
        (DN.319): While evaling syn 'silver:langutil:errors' in 'silver:compiler:definition:core:attributionDcl' (3d356e85, Regex.sv:9:0)
        (CA.62): Error evaluating base of collection attribute silver:langutil:errors via forward of silver:compiler:definition:core:attributionDcl
        (DN.319): While evaling syn 'silver:langutil:errors' in 'silver:compiler:definition:core:defaultAttributionDcl' (4778db1f, Regex.sv:9:0)
        (DN.319): While evaling syn 'silver:compiler:definition:env:dcl' in 'silver:compiler:definition:core:customLookup' (1b19e5bf)
        (silver.core.Perror in Perror.java:98): Error while evaluating function silver:core:error
        (U.91): INTERNAL ERROR: Accessing dcl of type RegexSeq at Regex.sv:9:65

---
+++ grammars/silver/compiler/definition/concrete_syntax/copper/Regex.sv
@@ -0,0 +1,52 @@
+grammar silver:compiler:definition:concrete_syntax:copper;
+
+-- edu.umn.cs.melt.copper.compiletime.spec.grammarbeans.Regex
+type Regex foreign;
+
+-- edu.umn.cs.melt.copper.compiletime.spec.grammarbeans.EmptyStringRegex
+function emptyStringRegex
+Regex ::=
+{
+  return error("copper FFI function");
+} foreign {
+  "java" : return "new edu.umn.cs.melt.copper.compiletime.spec.grammarbeans.EmptyStringRegex()";
+}
+
+-- edu.umn.cs.melt.copper.compiletime.spec.grammarbeans.ChoiceRegex
+function choiceRegex
+Regex ::= subexps::[Regex]
+{
+  return error("copper FFI function");
+} foreign {
+  "java" : return "new edu.umn.cs.melt.copper.compiletime.spec.grammarbeans.ChoiceRegex().addSubexps(new java.util.ArrayList<edu.umn.cs.melt.copper.compiletime.spec.grammarbeans.Regex>(new common.javainterop.ConsCellCollection(%subexps%)))";
+}
+
+-- edu.umn.cs.melt.copper.compiletime.spec.grammarbeans.ConcatenationRegex
+function concatenationRegex
+Regex ::= subexps::[Regex]
+{
+  return error("copper FFI function");
+} foreign {
+  "java" : return "new edu.umn.cs.melt.copper.compiletime.spec.grammarbeans.ConcatenationRegex().addSubexps(new java.util.ArrayList<edu.umn.cs.melt.copper.compiletime.spec.grammarbeans.Regex>(new common.javainterop.ConsCellCollection(%subexps%)))";
+}
+
+-- edu.umn.cs.melt.copper.compiletime.spec.grammarbeans.KleeneStarRegex
+function kleeneStarRegex
+Regex ::= r::Regex
+{
+  return error("copper FFI function");
+} foreign {
+  "java" : return "new edu.umn.cs.melt.copper.compiletime.spec.grammarbeans.KleeneStarRegex((edu.umn.cs.melt.copper.compiletime.spec.grammarbeans.Regex) %r%)";
+}
+
+-- edu.umn.cs.melt.copper.compiletime.spec.grammarbeans.CharacterSetRegex
+function characterSetRegex
+Regex ::=
+{
+  return error("copper FFI function");
+} foreign {
+  -- "java" : return "common.rawlib.RawTreeSet.isEmpty((java.util.TreeSet<Object>)%s%)";
+  "java" : return "new edu.umn.cs.melt.copper.compiletime.spec.grammarbeans.CharacterSetRegex()"; -- TODO
+}
+
+nonterminal CharSet;
