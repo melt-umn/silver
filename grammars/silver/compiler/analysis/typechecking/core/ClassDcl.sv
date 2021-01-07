238d017492b1af6cc1e99077ced9ceefb275ce6e
Lucas Kramer <krame505@umn.edu>
2021-01-07 12:58:53 -0600

    Add check for ambiguous type variables in instance resolution

--- grammars/silver/compiler/analysis/typechecking/core/ClassDcl.sv
+++ grammars/silver/compiler/analysis/typechecking/core/ClassDcl.sv
@@ -13,6 +13,6 @@ top::ClassBodyItem ::= id::Name '::' ty::TypeExpr '=' e::Expr ';'
   
   e.downSubst = emptySubst();
   errCheck1.downSubst = e.upSubst;
-  e.finalSubst = e.upSubst;
+  e.finalSubst = errCheck1.upSubst;
   errCheck1.finalSubst = e.finalSubst;
 }
