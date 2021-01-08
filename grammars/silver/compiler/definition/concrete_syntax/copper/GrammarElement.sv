8c9708d69af760db5480aa0a915487efb5eb7b95
Nathan Ringo <nathan@remexre.xyz>
2021-01-07 16:48:17 -0800

    Start of some GrammarElements.

--- grammars/silver/compiler/definition/concrete_syntax/copper/GrammarElement.sv
+++ grammars/silver/compiler/definition/concrete_syntax/copper/GrammarElement.sv
@@ -5,7 +5,14 @@ type GrammarElement foreign;
 
 -- edu.umn.cs.melt.copper.compiletime.spec.grammarbeans.DisambiguationFunction
 
--- edu.umn.cs.melt.copper.compiletime.spec.grammarbeans.GrammarSymbol
+-- edu.umn.cs.melt.copper.compiletime.spec.grammarbeans.Nonterminal
+function nonterminal_
+GrammarElement ::= id::String  pp::String  type_::String
+{
+  return error("copper FFI function");
+} foreign {
+  "java" : return "common.CopperUtil.makeNonTerminal(%id%.toString(), %pp%.toString(), %type_%.toString())";
+}
 
 -- edu.umn.cs.melt.copper.compiletime.spec.grammarbeans.OperatorClass
 
@@ -13,4 +20,23 @@ type GrammarElement foreign;
 
 -- edu.umn.cs.melt.copper.compiletime.spec.grammarbeans.Production
 
+-- edu.umn.cs.melt.copper.compiletime.spec.grammarbeans.Terminal
+function terminal_
+GrammarElement ::= id::String  pp::String  regex::Regex  hasPrecedence::Boolean
+    precedence_::Integer  hasAssociativity::Boolean associativity::String
+    type_::String  code::String  classes_::[String] hasPrefix::Boolean
+    prefix_::String submits_::[String]  dominates_::[String]
+{
+  return error("copper FFI function");
+} {- foreign {
+  "java" : return "common.CopperUtil.makeTerminalClass(%id%.toString())";
+} -}
+
 -- edu.umn.cs.melt.copper.compiletime.spec.grammarbeans.TerminalClass
+function terminalClass
+GrammarElement ::= id::String
+{
+  return error("copper FFI function");
+} foreign {
+  "java" : return "common.CopperUtil.makeTerminalClass(%id%.toString())";
+}
