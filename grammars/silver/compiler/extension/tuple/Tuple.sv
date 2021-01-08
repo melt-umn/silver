5cc30c98ce90d30299295e99440d7c5710dcda53
Alexandra Hanson <alexandra.rose.hanson@gmail.com>
2021-01-08 11:41:25 -0600

    Added pattern matching on tuples

--- grammars/silver/compiler/extension/tuple/Tuple.sv
+++ grammars/silver/compiler/extension/tuple/Tuple.sv
@@ -4,43 +4,29 @@ imports silver:compiler:definition:core;
 imports silver:compiler:definition:env;
 imports silver:compiler:definition:type;
 
+imports silver:compiler:extension:patternmatching;
+
 terminal Comma_t ',' ;
 
--- Question: What attribute occurences should I include w/ TupleList?
-nonterminal TupleList with location, config, unparse, env, frame, errors, translation;
+nonterminal TupleList with location, unparse, translation;
 synthesized attribute translation :: Expr;
 
--- We consider only tuples containing two or more elements
+-- Note: We consider only tuples containing two or more elements
 
-concrete production tupleExpr_c
+concrete production tuple_c
 top::Expr ::= '(' tl::TupleList ')'
 {
     top.unparse = "(" ++ tl.unparse ++ ")";
-    forwards to tupleExpr_a(tl, location = top.location);
-
-}
-
-abstract production tupleExpr_a
-top::Expr ::= tl::TupleList
-{
-    top.unparse = "(" ++ tl.unparse ++ ")";
     forwards to tl.translation;
+
 }
 
 -- TupleList cases:
+
 --   There are two elements in the tuple
 concrete production tupleList_2Elements
 top::TupleList ::= fst::Expr ',' snd::Expr
 {
-    top.unparse = fst.unparse ++ ", " ++ snd.unparse;
-    forwards to tupleList_2a(fst, snd, location = top.location);
-
-}
-
-abstract production tupleList_2a
-top::TupleList ::= fst::Expr snd::Expr
-{
-
     top.unparse = fst.unparse ++ ", " ++ snd.unparse;
     top.translation = Silver_Expr { silver:core:pair($Expr{fst}, $Expr{snd}) };
 
@@ -52,13 +38,36 @@ top::TupleList ::= fst::Expr ',' snd::TupleList
 {
 
     top.unparse = fst.unparse ++ ", " ++ snd.unparse;
-    forwards to tupleList_na(fst, snd, location = top.location);
+    top.translation = Silver_Expr { silver:core:pair($Expr{fst}, $Expr{snd.translation}) };
 
 }
 
-abstract production tupleList_na
-top::TupleList ::= fst::Expr snd::TupleList
+-- Pattern matching on tuples
+
+nonterminal TuplePatternList with location, unparse, patternList;
+
+-- Turns TuplePatternList into [Pattern]
+synthesized attribute patternList :: [Decorated Pattern];
+
+concrete production tuplePattern
+top::Pattern ::= '(' ts::TuplePatternList ')'
+{
+    top.unparse = s"(${ts.unparse})";
+    forwards to ts.asTuplePattern;
+}
+
+synthesized attribute asTuplePattern::Pattern occurs on TuplePatternList;
+
+concrete production patternTuple_two
+top::TuplePatternList ::= fst::Pattern ',' snd::Pattern
 {
     top.unparse = fst.unparse ++ ", " ++ snd.unparse;
-    top.translation = Silver_Expr { silver:core:pair($Expr{fst}, $Expr{snd.translation}) };
+    top.asTuplePattern = Silver_Pattern { silver:core:pair($Pattern{fst}, $Pattern{snd}) };
+}
+
+concrete production patternTuple_more
+top::TuplePatternList ::= fst::Pattern ',' snd::TuplePatternList
+{
+    top.unparse = fst.unparse ++ ", " ++ snd.unparse;
+    top.asTuplePattern = Silver_Pattern { silver:core:pair($Pattern{fst}, $Pattern{snd.asTuplePattern}) };
 }
