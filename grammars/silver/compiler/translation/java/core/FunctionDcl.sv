d4bddef2854011898a2d5f68ffd97d6587e13e40
Lucas Kramer <krame505@umn.edu>
2021-01-09 15:41:08 -0600

    Actually fix the bug

--- grammars/silver/compiler/translation/java/core/FunctionDcl.sv
+++ grammars/silver/compiler/translation/java/core/FunctionDcl.sv
@@ -16,8 +16,6 @@ top::AGDcl ::= 'function' id::Name ns::FunctionSignature body::ProductionBody
   local argsAccess :: String =
     implode(", ", map((.contextRefElem), namedSig.contexts) ++ map((.childRefElem), namedSig.inputElements));
 
-  local commaIfArgs :: String = if length(namedSig.contexts) + length(namedSig.inputElements)!=0 then "," else "";
-
   local funBody :: String =
 s"""			final common.DecoratedNode context = new P${id.name}(${argsAccess}).decorate(originCtx);
 			//${head(body.uniqueSignificantExpression).unparse}
@@ -48,7 +46,7 @@ String ::= whatGrammar::String whatName::String whatSig::NamedSignature whatResu
   local localVar :: String = 
     s"count_local__ON__${makeIdName(whatGrammar)}_${whatName}";
 
-  local commaIfArgs :: String = if length(whatSig.inputElements) != 0 then "," else "";
+  local commaIfArgs :: String = if length(namedSig.contexts) + length(whatSig.inputElements) != 0 then "," else "";
 
   return s"""
 package ${makeName(whatGrammar)};
