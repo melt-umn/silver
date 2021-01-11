b9c69e2ad973f16b75fc1dc560e026d652828e6c
Lucas Kramer <krame505@umn.edu>
2021-01-11 15:32:51 -0600

    Update rewriting tests to use type classes for equality rather than string comparison

--- test/silver_features/EqualityAttr.sv
+++ test/silver_features/EqualityAttr.sv
@@ -1,37 +1,37 @@
 grammar silver_features;
 
-equality attribute isEqualTo, isEqual;
+equality attribute isEqualToTest, isEqualTest;
 
-nonterminal EqExpr with isEqualTo, isEqual;
+nonterminal EqExpr with isEqualToTest, isEqualTest;
 
 abstract production addEqExpr
 top::EqExpr ::= e1::EqExpr e2::EqExpr
 {
-  propagate isEqualTo, isEqual;
+  propagate isEqualToTest, isEqualTest;
 }
 
 abstract production intEqExpr
 top::EqExpr ::= i::Integer
 {
-  propagate isEqualTo, isEqual;
+  propagate isEqualToTest, isEqualTest;
 }
 
 abstract production appEqExpr
 top::EqExpr ::= n::String e::EqExpr
 {
-  propagate isEqualTo, isEqual;
+  propagate isEqualToTest, isEqualTest;
 }
 
 global ee1::EqExpr = addEqExpr(intEqExpr(42), appEqExpr("abc", intEqExpr(5)));
 global ee2::EqExpr = addEqExpr(intEqExpr(42), appEqExpr("c", intEqExpr(5)));
 global ee3::EqExpr = addEqExpr(appEqExpr("c", intEqExpr(5)), intEqExpr(42));
 
-equalityTest(decorate ee1 with {isEqualTo = ee1;}.isEqual, true, Boolean, silver_tests);
-equalityTest(decorate ee1 with {isEqualTo = ee2;}.isEqual, false, Boolean, silver_tests);
-equalityTest(decorate ee1 with {isEqualTo = ee3;}.isEqual, false, Boolean, silver_tests);
-equalityTest(decorate ee2 with {isEqualTo = ee1;}.isEqual, false, Boolean, silver_tests);
-equalityTest(decorate ee2 with {isEqualTo = ee2;}.isEqual, true, Boolean, silver_tests);
-equalityTest(decorate ee2 with {isEqualTo = ee3;}.isEqual, false, Boolean, silver_tests);
-equalityTest(decorate ee3 with {isEqualTo = ee1;}.isEqual, false, Boolean, silver_tests);
-equalityTest(decorate ee3 with {isEqualTo = ee2;}.isEqual, false, Boolean, silver_tests);
-equalityTest(decorate ee3 with {isEqualTo = ee3;}.isEqual, true, Boolean, silver_tests);
+equalityTest(decorate ee1 with {isEqualToTest = ee1;}.isEqualTest, true, Boolean, silver_tests);
+equalityTest(decorate ee1 with {isEqualToTest = ee2;}.isEqualTest, false, Boolean, silver_tests);
+equalityTest(decorate ee1 with {isEqualToTest = ee3;}.isEqualTest, false, Boolean, silver_tests);
+equalityTest(decorate ee2 with {isEqualToTest = ee1;}.isEqualTest, false, Boolean, silver_tests);
+equalityTest(decorate ee2 with {isEqualToTest = ee2;}.isEqualTest, true, Boolean, silver_tests);
+equalityTest(decorate ee2 with {isEqualToTest = ee3;}.isEqualTest, false, Boolean, silver_tests);
+equalityTest(decorate ee3 with {isEqualToTest = ee1;}.isEqualTest, false, Boolean, silver_tests);
+equalityTest(decorate ee3 with {isEqualToTest = ee2;}.isEqualTest, false, Boolean, silver_tests);
+equalityTest(decorate ee3 with {isEqualToTest = ee3;}.isEqualTest, true, Boolean, silver_tests);
