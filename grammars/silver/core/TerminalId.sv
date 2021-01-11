90f88a2926a2ccf13bc0a51e842ae9d13f3dd345
Lucas Kramer <krame505@umn.edu>
2021-01-11 10:38:42 -0600

    Remove more deprecated eq functions

--- grammars/silver/core/TerminalId.sv
+++ grammars/silver/core/TerminalId.sv
@@ -1,23 +1,10 @@
 grammar silver:core;
 
-function terminalIdEq
-Boolean ::= t1::TerminalId  t2::TerminalId
-{
-  return t1 == t2;
-}
-
-function terminalIdLte
-Boolean ::= t1::TerminalId  t2::TerminalId
-{
-  return t1 <= t2;
-}
-
+-- TODO: Should these be generic list functions?
 function terminalSetEq
 Boolean ::= ts1::[TerminalId] ts2::[TerminalId]
 {
-  return
-    length(ts1) == length(ts2) &&
-    all(zipWith(terminalIdEq, sortBy(terminalIdLte, ts1), sortBy(terminalIdLte, ts2)));
+  return length(ts1) == length(ts2) && all(zipWith(eq, sort(ts1), sort(ts2)));
 }
 
 function terminalSubset
