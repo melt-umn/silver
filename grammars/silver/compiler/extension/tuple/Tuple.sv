e8bf76f25ea54149ceeb4fc2bdd57ea473686501
Alexandra Hanson <alexandra.rose.hanson@gmail.com>
2021-01-12 13:38:35 -0600

    Added TypeExprs to tuples to improve syntax for type declarations

--- grammars/silver/compiler/extension/tuple/Tuple.sv
+++ grammars/silver/compiler/extension/tuple/Tuple.sv
@@ -2,7 +2,7 @@ grammar silver:compiler:extension:tuple;
 
 imports silver:compiler:definition:core;
 imports silver:compiler:definition:env;
-imports silver:compiler:definition:type;
+imports silver:compiler:definition:type:syntax;
 
 imports silver:compiler:extension:patternmatching;
 
@@ -18,23 +18,24 @@ synthesized attribute translation :: Expr;
 concrete production tupleTypeExpr
 top::TypeExpr ::= '(' tes::ListOfTypeExprs ')'
 {
-    top.unparse = '(' ++ tes.unparse ++ ')';
-    forwards to tes.translation;
+    top.unparse = "(" ++ tes.unparse ++ ")";
+    forwards to tes.te_translation;
 }
 
 concrete production tupleTypeExpr2
 top::ListOfTypeExprs ::= te1::TypeExpr ',' te2::TypeExpr
 {
-    top.unparse = te1.unparse ++ ',' ++ te2.unparse;
-    --TO DO: top.te_translation = $TypeExpr { silver:core:pair($Expr{te1}, $Expr{te2}) };
+    top.unparse = te1.unparse ++ "," ++ te2.unparse;
+    top.te_translation = Silver_TypeExpr {silver:core:Pair<$TypeExpr{te1} $TypeExpr{te2}>};
 
 }
 
 concrete production tupleTypeExprn
 top::ListOfTypeExprs ::= te::TypeExpr ',' tes::ListOfTypeExprs
 {
-    top.unparse = te1.unparse ++ ',' ++ te2.unparse;
-    --TO DO: top.te_translation = $TypeExpr { silver:core:pair($Expr{te1}, $Expr{te2.translation}) };
+    top.unparse = te.unparse ++ "," ++ tes.unparse;
+    --local temp::TypeExpr = tes.te_translation;
+    top.te_translation = Silver_TypeExpr {silver:core:Pair<$TypeExpr{te} $TypeExpr{tes.te_translation}>};
 }
 
 
