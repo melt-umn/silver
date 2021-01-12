7ef9e0a471a2971ead516d056fb468cda28ee4c3
Alexandra Hanson <alexandra.rose.hanson@gmail.com>
2021-01-12 17:10:14 -0600

    Moved tuple TypeExprs to a seperate file

--- grammars/silver/compiler/extension/tuple/Tuple.sv
+++ grammars/silver/compiler/extension/tuple/Tuple.sv
@@ -10,32 +10,8 @@ imports silver:compiler:extension:patternmatching;
 
 terminal Comma_t ',' ;
 
-nonterminal ListOfTypeExprs with location, unparse, te_translation;
-synthesized attribute te_translation :: TypeExpr;
 nonterminal TupleList with location, unparse, translation;
 synthesized attribute translation :: Expr;
-
-concrete production tupleTypeExpr
-top::TypeExpr ::= '(' tes::ListOfTypeExprs ')'
-{
-  top.unparse = "(" ++ tes.unparse ++ ")";
-  forwards to tes.te_translation;
-}
-
-concrete production tupleTypeExpr2
-top::ListOfTypeExprs ::= te1::TypeExpr ',' te2::TypeExpr
-{
-  top.unparse = te1.unparse ++ "," ++ te2.unparse;
-  top.te_translation = Silver_TypeExpr {silver:core:Pair<$TypeExpr{te1} $TypeExpr{te2}>};
-}
-
-concrete production tupleTypeExprn
-top::ListOfTypeExprs ::= te::TypeExpr ',' tes::ListOfTypeExprs
-{
-  top.unparse = te.unparse ++ "," ++ tes.unparse;
-  top.te_translation = Silver_TypeExpr {silver:core:Pair<$TypeExpr{te} $TypeExpr{tes.te_translation}>};
-}
-
 
 concrete production tuple_c
 top::Expr ::= '(' tl::TupleList ')'
---
+++ grammars/silver/compiler/extension/tuple/Type.sv
@@ -0,0 +1,25 @@
+grammar silver:compiler:extension:tuple;
+
+nonterminal ListOfTypeExprs with location, unparse, te_translation;
+synthesized attribute te_translation :: TypeExpr;
+
+concrete production tupleTypeExpr
+top::TypeExpr ::= '(' tes::ListOfTypeExprs ')'
+{
+  top.unparse = "(" ++ tes.unparse ++ ")";
+  forwards to tes.te_translation;
+}
+
+concrete production tupleTypeExpr2
+top::ListOfTypeExprs ::= te1::TypeExpr ',' te2::TypeExpr
+{
+  top.unparse = te1.unparse ++ "," ++ te2.unparse;
+  top.te_translation = Silver_TypeExpr {silver:core:Pair<$TypeExpr{te1} $TypeExpr{te2}>};
+}
+
+concrete production tupleTypeExprn
+top::ListOfTypeExprs ::= te::TypeExpr ',' tes::ListOfTypeExprs
+{
+  top.unparse = te.unparse ++ "," ++ tes.unparse;
+  top.te_translation = Silver_TypeExpr {silver:core:Pair<$TypeExpr{te} $TypeExpr{tes.te_translation}>};
+}
