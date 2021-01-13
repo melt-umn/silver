ba9f4ae1bfe4a77f12fc9bd6bc1a25bbd10cb175
Ian Kariniemi <i.kariniemi@gmail.com>
2021-01-13 11:20:40 -0600

    Adds more tests.

--- test/silver_features/ConvenienceAspects.sv
+++ test/silver_features/ConvenienceAspects.sv
@@ -30,44 +30,34 @@ aspect foopp on FooExpr of
 | _ -> "default"
 end;
 
--- aspect unparse on Thing of
--- | foo([]) -> "emptyFoo"
--- | foo(h :: t) -> h.unparse ++ " and then " ++ t.unparse
--- | bar(x) -> x
--- end;
--- aspect production addfoo
--- top::FooExpr ::= _ _
--- { top.foopp = "test test";}
+synthesized attribute ppList :: [String];
+nonterminal BarExpr with ppList, value;
 
--- aspect default production
--- top::FooExpr ::=
--- { top.foopp = "unparse test";}
+abstract production barInit1
+top::BarExpr ::= ppList::[String] value::Integer
+{
+  top.ppList = ppList;
+  top.value = value;
+}
 
--- silver:compiler:definition:core:aspectProductionDcl('aspect', 'production',
--- silver:compiler:definition:core:qNameId(silver:compiler:definition:core:nameIdLower('addfoo')),
--- silver:compiler:definition:core:aspectProductionSignature(silver:compiler:definition:core:aspectProductionLHSTyped(silver:compiler:definition:core:nameIdLower('top'),
--- '::',
--- silver:compiler:definition:type:syntax:nominalTypeExpr(silver:compiler:definition:core:qNameTypeId('FooExpr'))),
--- '::=',
--- silver:compiler:definition:core:aspectRHSElemCons(silver:compiler:definition:core:aspectRHSElemNone('_'),
--- silver:compiler:definition:core:aspectRHSElemCons(silver:compiler:definition:core:aspectRHSElemNone('_'),
--- silver:compiler:definition:core:aspectRHSElemNil())))
--- silver:compiler:definition:core:productionBody('{',
--- silver:compiler:definition:core:productionStmtsSnoc(silver:compiler:definition:core:productionStmtsNil(),
--- silver:compiler:definition:core:attributeDef(silver:compiler:definition:core:concreteDefLHS(silver:compiler:definition:core:qNameId(silver:compiler:definition:core:nameIdLower('top'))),
--- '.',
--- silver:compiler:definition:core:qNameAttrOccur(silver:compiler:definition:core:qNameId(silver:compiler:definition:core:nameIdLower('foopp'))),
--- '=', silver:compiler:definition:core:stringConst('"test test"'), ';')), '}'))
+abstract production barInit2
+top::BarExpr ::= ppList::[String] value::Integer
+{
+  top.ppList = ppList;
+  top.value = value + 1;
+}
 
+abstract production barInit3
+top::BarExpr ::= ppList::[String] value::Integer
+{
+  top.ppList = ppList ++ ["extra"];
+  top.value = value;
+}
 
+attribute foopp occurs on BarExpr;
 
--- prodAppPattern:silver:compiler:extension:patternmatching:prodAppPattern_named(
---     silver:compiler:definition:core:qNameId(silver:compiler:definition:core:nameIdLower('addfoo')),
---     '(',
---     silver:compiler:extension:patternmatching:patternList_snoc(
---         silver:compiler:extension:patternmatching:patternList_one(silver:compiler:extension:patternmatching:wildcPattern('_')),
---         ',',
---         silver:compiler:extension:patternmatching:wildcPattern('_')),
---     ',',
---     silver:compiler:extension:patternmatching:namedPatternList_nil(),
---     ')')
+aspect foopp on BarExpr of
+| barInit1([],_) -> "emptyFoopp"
+| barInit2(h :: t,value) -> h ++ " and then " ++ toStringFromInteger(value)
+| barInit3(_,val) -> toStringFromInteger(val)
+end;
