cda16531947419831fe97a83cb9225319a9f199e
Ian Kariniemi <i.kariniemi@gmail.com>
2021-01-14 22:24:52 -0600

    Mostly working setup.

--- grammars/silver/compiler/extension/convenienceaspects/ConcreteSyntax.sv
+++ grammars/silver/compiler/extension/convenienceaspects/ConcreteSyntax.sv
@@ -1,4 +1,69 @@
 grammar silver:compiler:extension:convenienceaspects;
+import silver:core;
+
+
+function extractAgDclFromRuleList
+AGDcl ::= rules::[AbstractMatchRule] aspectTy::TypeExpr aspectAttr::QName location::Location
+{
+
+  local makeProdParamTypes::([Type] ::= QName) = \prod::QName ->
+    case prod.lookupValue.typeScheme.typerep of
+    | functionType(_, paramTypes, _) -> paramTypes
+    | _ -> error("invalid production type")
+    end;
+  -- local makeProdParamsList::([AspectRHSElem] ::= [Type]) = \prodParamTypes::[Type] ->
+  --   map(\ty::Type -> aspectRHSElemFull(name("__generated_" ++ toString(genIntReally(ty)), location), ty, location=location),
+  --   prodParamTypes);
+  local makeProdParamsList::([AspectRHSElem] ::= [Name] [Type]) =
+    \prodParamNames::[Name] prodParamTypes::[Type] ->
+      zipWith(aspectRHSElemFull(_, _, location=location), prodParamNames, prodParamTypes);
+  local makeProdParams::(AspectRHS ::= [AspectRHSElem] ) = \prodParamsList::[AspectRHSElem] ->
+    foldr(aspectRHSElemCons(_, _, location=location),
+          aspectRHSElemNil(location=location),
+          prodParamsList);
+  local makeQNamesFromNames::([QName] ::= [Name]) = map(qNameId(_, location=location),_);
+  -- local makeProdParamNames::([QName] ::= [AspectRHSElem]) = \prodParamsList::[AspectRHSElem] ->
+  --   map((\asp::AspectRHSElem -> case asp of
+  --                               | aspectRHSElemFull(n,_) -> qNameId(n, location=location)
+  --                               | _ -> error("Somehow an element other than an AspectRHSElem is in this list, which shouldn't happen")
+  --                               end),
+  --           prodParamsList);
+  local makeParamCaseSubExpr::([Expr] ::= [QName]) = \prodParamNames::[QName] ->
+    map(baseExpr(_,location=location),prodParamNames);
+  local makeParamsCaseExpr::(Expr ::= [Expr] [AbstractMatchRule]) =
+    \paramsCaseSubExpr::[Expr] mRules::[AbstractMatchRule] ->
+      caseExpr(paramsCaseSubExpr, mRules,
+        mkStrFunctionInvocation(location, "silver:core:error",
+            [stringConst(terminal(String_t,
+            "\"Error: pattern match failed at " ++ location.unparse ++ "\\n\""), location=location)]),
+            freshType(), location=location);
+  local makeAspect::(AGDcl ::= Expr QName AspectRHS) =
+    \paramsCaseExpr::Expr prod::QName prodParams::AspectRHS ->
+      Silver_AGDcl {
+        aspect production $QName{prod}
+        top::$TypeExpr{aspectTy} ::= $AspectRHS{prodParams}
+        { top.$QName{aspectAttr} = $Expr{paramsCaseExpr}; }
+      };
+  return case head(rules) of
+    | matchRule(prodAppPattern(name,_,_,_)::_, cond,e) ->
+    -- I wish let bindings were stable
+    makeAspect(
+      makeParamsCaseExpr(
+        makeParamCaseSubExpr(
+          makeQNamesFromNames(head(rules).aspectProdParamsList)), rules),
+      name,
+      makeProdParams(makeProdParamsList(head(rules).aspectProdParamsList, makeProdParamTypes(name))))
+    -- makeAspect(
+    --   makeParamsCaseExpr(
+    --     makeParamCaseSubExpr(
+    --       makeProdParamNames(
+    --         makeProdParamsList(
+    --           makeProdParamTypes(name)))),
+    --     rules),
+    --     name
+    --     )
+    end;
+}
 
 concrete production convenienceAspects_c
 top::AGDcl ::= 'aspect' attr::QName 'on' ty::TypeExpr 'of' Opt_Vbar_t ml::MRuleList 'end' ';'
@@ -6,14 +71,47 @@ top::AGDcl ::= 'aspect' attr::QName 'on' ty::TypeExpr 'of' Opt_Vbar_t ml::MRuleL
   top.defs := [];
   ml.aspectAttr = attr;
   ml.aspectTy = ty;
-  forwards to makeAppendAGDclOfAGDcls(ml.aspectDcls);
+
+  -- local extractAgDclFromRuleList::(AGDcl ::= [AbstractMatchRule]) =
+  --   \rules::[AbstractMatchRule] ->
+  --     case rules of
+  --     | [matchRule(prodAppPattern(name,_,_,_),)] :: rest ->
+  --       foldr( w )
+
+
+  local groupedMRules::[[AbstractMatchRule]] = groupMRules(ml.matchRuleList);
+  local fwrd::AGDcl = makeAppendAGDclOfAGDcls(ml.aspectDcls);
+
+  forwards to unsafeTrace(fwrd, print(hackUnparse(groupedMRules) ++ "\n\n",unsafeIO()));
 }
+
+
+
+aspect production matchRule
+top::AbstractMatchRule ::= pl::[Decorated Pattern] cond::Maybe<Pair<Expr Maybe<Pattern>>> e::Expr
+{
+  top.aspectProdParamsList = case pl of
+    | prodAppPattern_named(prod, _, ps,_,_,_) :: _ ->
+      map(\pat::Decorated Pattern ->
+        name("__generated_" ++ toString(genIntReally(pat)), top.location),
+        ps.patternList)
+    | _ -> []
+    end;
+
+}
+
+
+--TODO: Do this, but generate warning on <-,
+-- Error("This is subject to change.")
+-- aspect foo on bar with := of ... end
+-- Defaults to =
+-- aspect foo on bar of ... end
 
 autocopy attribute aspectAttr::QName occurs on MRuleList, MatchRule, PatternList, Pattern;
 autocopy attribute aspectTy::TypeExpr occurs on MRuleList, MatchRule, PatternList, Pattern;
 
 synthesized attribute aspectDcls::AGDcls occurs on MRuleList;
-flowtype aspectDcls {aspectTy, aspectAttr} on MRuleList;
+synthesized attribute aspectProdParamsList::[Name] occurs on AbstractMatchRule;
 
 aspect production mRuleList_one
 top::MRuleList ::= m::MatchRule
@@ -28,8 +126,8 @@ top::MRuleList ::= h::MatchRule '|' t::MRuleList
   top.aspectDcls = consAGDcls(h.aspectDcl, t.aspectDcls, location=top.location);
 }
 
+
 synthesized attribute aspectDcl::AGDcl occurs on MatchRule;
-flowtype aspectDcl {aspectTy, aspectAttr} on MatchRule;
 
 aspect production matchRule_c
 top::MatchRule ::= pt::PatternList _ e::Expr
@@ -40,11 +138,11 @@ top::MatchRule ::= pt::PatternList _ e::Expr
 }
 
 synthesized attribute makeAspect::(AGDcl ::= Expr) occurs on Pattern;
-flowtype makeAspect {aspectTy, aspectAttr} on Pattern;
 
 function genIntReally -- zzz
 Integer ::= a
 { return genInt(); }
+
 
 aspect production prodAppPattern_named
 top::Pattern ::= prod::QName '(' ps::PatternList ',' nps::NamedPatternList ')'
--- test/silver_features/ConvenienceAspects.sv
+++ test/silver_features/ConvenienceAspects.sv
@@ -58,6 +58,44 @@ attribute foopp occurs on BarExpr;
 
 aspect foopp on BarExpr of
 | barInit1([],_) -> "emptyFoopp"
+| barInit1(h::t,_) -> h ++ "Foopp"
 | barInit2(h :: t,value) -> h ++ " and then " ++ toStringFromInteger(value)
 | barInit3(_,val) -> toStringFromInteger(val)
 end;
+
+
+
+-- Which is tranformed to the following.
+-- (This is the unparse output of the generated tree)
+-- ----->
+
+aspect production addfoo
+top ::= __generated_284::silver_features:FooExpr __generated_285::silver_features:FooExpr
+	top.foopp = case __generated_284, __generated_285,  of l, _ -> "foo " ++ l.prettierprint end;
+aspect production subtractFoo
+top ::= __generated_286::silver_features:FooExpr __generated_287::silver_features:FooExpr
+
+	top.foopp = case __generated_286, __generated_287,  of l, r -> "foo " ++ l.prettierprint ++ "-" ++ r.prettierprint end;
+aspect default production
+top::FooExpr ::=
+
+	top.foopp = "default";
+
+aspect production barInit1
+top ::= __generated_288::[String] __generated_289::Integer
+
+	top.foopp = case __generated_288, __generated_289,  of [], _ -> "emptyFoopp" end;
+aspect production barInit1
+top ::= __generated_290::[String] __generated_291::Integer
+
+	top.foopp = case __generated_290, __generated_291,  of h::t, _ -> h ++ "Foopp" end;
+aspect production barInit2
+top ::= __generated_292::[String] __generated_293::Integer
+
+	top.foopp = case __generated_292, __generated_293,  of h::t, value -> h ++ " and then " ++ toStringFromInteger(value,) end;
+aspect production barInit3
+top ::= __generated_294::[String] __generated_295::Integer
+
+	top.foopp = case __generated_294, __generated_295,  of _, val -> toStringFromInteger(val,) end;
+
+
