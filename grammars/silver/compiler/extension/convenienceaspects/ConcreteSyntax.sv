dd5cda2752483abc2810bac49b9c0dc02cf3c913
Ian Kariniemi <i.kariniemi@gmail.com>
2021-01-17 00:16:49 -0600

    Adds support for :=,<- in aspect conv. syntax.

--- grammars/silver/compiler/extension/convenienceaspects/ConcreteSyntax.sv
+++ grammars/silver/compiler/extension/convenienceaspects/ConcreteSyntax.sv
@@ -1,9 +1,11 @@
 grammar silver:compiler:extension:convenienceaspects;
 import silver:core;
+import silver:compiler:modification:collection;
+import silver:compiler:extension:constructparser;
 
 
 function extractAgDclFromRuleList
-Pair<AGDcl [Message]> ::= rules::[Decorated AbstractMatchRule] aspectTy::TypeExpr aspectAttr::QName location::Location
+Pair<AGDcl [Message]> ::= rules::[Decorated AbstractMatchRule] aspectTy::TypeExpr aspectAttr::QNameAttrOccur genDef::(ProductionStmt ::= DefLHS QNameAttrOccur Expr Location) location::Location
 {
 
   local makeProdParamTypes::([Type] ::= Decorated QName) = \prod::Decorated QName ->
@@ -46,42 +48,46 @@ Pair<AGDcl [Message]> ::= rules::[Decorated AbstractMatchRule] aspectTy::TypeExp
       caseExpr(paramsCaseSubExpr, transformPatternMatchRule(mRules),
         mkStrFunctionInvocation(location, "silver:core:error",
             [stringConst(terminal(String_t,
-            "\"Error: pattern match failed at " ++ location.unparse ++ "\\n\""), location=location)]),
-            freshType(), location=location);
+            "\"Error: pattern match failed at " ++ head(mRules).headPattern.location.unparse ++ "\\n\""), location=head(mRules).headPattern.location)]),
+            freshType(), location=head(mRules).headPattern.location);
   local makeAspect::(AGDcl ::= Expr QName AspectRHS) =
     \paramsCaseExpr::Expr prod::QName prodParams::AspectRHS ->
       Silver_AGDcl {
         aspect production $QName{prod}
         top::$TypeExpr{aspectTy} ::= $AspectRHS{prodParams}
-        { top.$QName{aspectAttr} = $Expr{paramsCaseExpr}; }
+        { $ProductionStmt{genDef(defTop,aspectAttr,paramsCaseExpr, paramsCaseExpr.location)}}
       };
+  local defTop::DefLHS = concreteDefLHS(qNameId(name("top",location), location=location), location=location);
   return case rules of
     | matchRule(prodAppPattern(name,_,_,_)::_, cond,e) :: _ ->
     -- I wish let bindings were stable...
-    pair(makeAspect(
-      makeParamsCaseExpr(
-        makeParamCaseSubExpr(makeQNamesFromNames(head(rules).aspectProdParamsList)),
-        rules),
-      name,
-      makeProdParams(
-        makeProdParamsList(
-          head(rules).aspectProdParamsList,
-          makeProdParamTypes(name)))),
-        [])
+    pair(
+      makeAspect(
+        makeParamsCaseExpr(
+            makeParamCaseSubExpr(makeQNamesFromNames(head(rules).aspectProdParamsList)),
+            rules),
+        name,
+        makeProdParams(
+            makeProdParamsList(
+            head(rules).aspectProdParamsList,
+            makeProdParamTypes(name)))),
+      [])
     | [matchRule(wildcPattern(_)::_,_,e)] ->
-      pair(Silver_AGDcl {
-        aspect default production
-        top::$TypeExpr{aspectTy} ::=
-        { top.$QName{aspectAttr} = $Expr{e}; }
+      pair(
+        Silver_AGDcl {
+          aspect default production
+          top::$TypeExpr{aspectTy} ::=
+          { $ProductionStmt{genDef(defTop,aspectAttr,e,head(rules).location)}}
       },
       [])
     | matchRule(wildcPattern(_)::_,_,e) :: _ ->
-      pair(Silver_AGDcl {
-        aspect default production
-        top::$TypeExpr{aspectTy} ::=
-        { top.$QName{aspectAttr} = $Expr{e}; }
-      },
-      [wrn(location, "wildcard patterns after this one are dead code.")])
+      pair(
+        Silver_AGDcl {
+          aspect default production
+          top::$TypeExpr{aspectTy} ::=
+          { $ProductionStmt{genDef(defTop,aspectAttr,e,head(rules).location)}}
+        },
+        [wrn(location, "wildcard patterns after this one are dead code.")])
     | _ ->
       pair(
         error("Patterns in aspect convenience syntax should be productions or wildcards only"),
@@ -90,15 +96,15 @@ Pair<AGDcl [Message]> ::= rules::[Decorated AbstractMatchRule] aspectTy::TypeExp
     end;
 }
 
+synthesized attribute aspectProdParamsList::[Name] occurs on AbstractMatchRule;
+synthesized attribute genAspectAttrDef::(ProductionStmt ::= DefLHS QNameAttrOccur Expr Location) occurs on AGDcl;
 
-
-concrete production convenienceAspects_c
-top::AGDcl ::= 'aspect' attr::QName 'on' ty::TypeExpr 'of' Opt_Vbar_t ml::MRuleList 'end' ';'
+abstract production convenienceAspects
+top::AGDcl ::= attr::QNameAttrOccur ty::TypeExpr ml::MRuleList
 {
   top.defs := [];
-  ml.aspectAttr = attr;
-  ml.aspectTy = ty;
 
+  -- TODO: Lookup whether attribute is a collection attribute, and error out accordingly
   -- local extractAgDclFromRuleList::(AGDcl ::= [AbstractMatchRule]) =
   --   \rules::[AbstractMatchRule] ->
   --     case rules of
@@ -107,9 +113,9 @@ top::AGDcl ::= 'aspect' attr::QName 'on' ty::TypeExpr 'of' Opt_Vbar_t ml::MRuleL
 
 
   local groupedMRules::[[Decorated AbstractMatchRule]] =
-    map(\rules::[AbstractMatchRule] -> map(\rule_::AbstractMatchRule -> decorate rule_ with { env = top.env; }, rules),
+    map(\rules::[AbstractMatchRule] -> map(\rule_::AbstractMatchRule -> decorate rule_ with { env = top.env; grammarName = top.grammarName; }, rules),
         groupMRules(ml.matchRuleList));
-  local groupExtractResults::[Pair<AGDcl [Message]>] = map(extractAgDclFromRuleList(_,ty,attr,top.location), groupedMRules);
+  local groupExtractResults::[Pair<AGDcl [Message]>] = map(extractAgDclFromRuleList(_,ty,attr,top.genAspectAttrDef,top.location), groupedMRules);
   local combinedAspectProds::[AGDcl] = map(fst(_),groupExtractResults);
   local combinedAspectDcls::AGDcls = foldr(
    consAGDcls(_,_,location=top.location),
@@ -118,9 +124,38 @@ top::AGDcl ::= 'aspect' attr::QName 'on' ty::TypeExpr 'of' Opt_Vbar_t ml::MRuleL
   local fwrd::AGDcl = makeAppendAGDclOfAGDcls(combinedAspectDcls);
 
   -- forwards to fwrd ;
-  forwards to unsafeTracePrint(fwrd, fwrd.unparse);
+  forwards to unsafeTraceDump(fwrd);
+
 }
 
+concrete productions top::AGDcl
+| 'aspect' attr::QNameAttrOccur 'on' ty::TypeExpr 'using' '=' 'of' Opt_Vbar_t ml::MRuleList 'end' ';'
+{
+  top.genAspectAttrDef = attributeDef(_,'.',_,'=',_,';',location=_);
+  forwards to convenienceAspects(attr,ty,ml,location=top.location);
+
+}
+| 'aspect' attr::QNameAttrOccur 'on' ty::TypeExpr 'using' ':=' 'of' Opt_Vbar_t ml::MRuleList 'end' ';'
+{
+  top.genAspectAttrDef = attrContainsBase(_,'.',_,':=',_,';',location=_);
+  forwards to convenienceAspects(attr,ty,ml,location=top.location);
+}
+| 'aspect' attr::QNameAttrOccur 'on' ty::TypeExpr 'using' '<-' 'of' Opt_Vbar_t ml::MRuleList 'end' ';'
+{
+  top.genAspectAttrDef = attrContainsAppend(_,'.',_,'<-',_,';', location=_);
+  forwards to convenienceAspects(attr,ty,ml,location=top.location);
+}
+| 'aspect' attr::QNameAttrOccur 'on' ty::TypeExpr 'of' Opt_Vbar_t ml::MRuleList 'end' ';'
+{
+  top.genAspectAttrDef = attributeDef(_,'.',_,'=',_,';',location=_);
+  forwards to convenienceAspects(attr,ty,ml,location=top.location);
+}
+
+aspect default production
+top::AGDcl ::=
+{
+  top.genAspectAttrDef = error("Attribute should not be used in this context.");
+}
 
 
 aspect production matchRule
@@ -136,107 +171,106 @@ top::AbstractMatchRule ::= pl::[Decorated Pattern] cond::Maybe<Pair<Expr Maybe<P
 
 }
 
-
---TODO: Do this, but generate warning on <-,
--- Error("This is subject to change.")
--- aspect foo on bar with := of ... end
--- Defaults to =
--- aspect foo on bar of ... end
-
-autocopy attribute aspectAttr::QName occurs on MRuleList, MatchRule, PatternList, Pattern;
-autocopy attribute aspectTy::TypeExpr occurs on MRuleList, MatchRule, PatternList, Pattern;
-
-synthesized attribute aspectDcls::AGDcls occurs on MRuleList;
-synthesized attribute aspectProdParamsList::[Name] occurs on AbstractMatchRule;
-
-aspect production mRuleList_one
-top::MRuleList ::= m::MatchRule
+aspect default production
+top::AbstractMatchRule ::=
 {
-  top.aspectDcls = consAGDcls(m.aspectDcl, nilAGDcls(location=top.location),
-                              location=top.location);
-}
-
-aspect production mRuleList_cons
-top::MRuleList ::= h::MatchRule '|' t::MRuleList
-{
-  top.aspectDcls = consAGDcls(h.aspectDcl, t.aspectDcls, location=top.location);
+  top.aspectProdParamsList = [];
 }
 
 
-synthesized attribute aspectDcl::AGDcl occurs on MatchRule;
+-- autocopy attribute aspectAttr::QNameAttrOccur occurs on MRuleList, MatchRule, PatternList, Pattern;
+-- autocopy attribute aspectTy::TypeExpr occurs on MRuleList, MatchRule, PatternList, Pattern;
 
-aspect production matchRule_c
-top::MatchRule ::= pt::PatternList _ e::Expr
-{
-  top.aspectDcl = case pt.patternList of
-  | [pat] -> pat.makeAspect(e)
-  end;
-}
+-- synthesized attribute aspectDcls::AGDcls occurs on MRuleList;
 
-synthesized attribute makeAspect::(AGDcl ::= Expr) occurs on Pattern;
+-- aspect production mRuleList_one
+-- top::MRuleList ::= m::MatchRule
+-- {
+--   top.aspectDcls = consAGDcls(m.aspectDcl, nilAGDcls(location=top.location),
+--                               location=top.location);
+-- }
+
+-- aspect production mRuleList_cons
+-- top::MRuleList ::= h::MatchRule '|' t::MRuleList
+-- {
+--   top.aspectDcls = consAGDcls(h.aspectDcl, t.aspectDcls, location=top.location);
+-- }
+
+
+-- synthesized attribute aspectDcl::AGDcl occurs on MatchRule;
+
+-- aspect production matchRule_c
+-- top::MatchRule ::= pt::PatternList _ e::Expr
+-- {
+--   top.aspectDcl = case pt.patternList of
+--   | [pat] -> pat.makeAspect(e)
+--   end;
+-- }
+
+-- synthesized attribute makeAspect::(AGDcl ::= Expr) occurs on Pattern;
 
 function genIntReally -- zzz
 Integer ::= a
 { return genInt(); }
 
 
-aspect production prodAppPattern_named
-top::Pattern ::= prod::QName '(' ps::PatternList ',' nps::NamedPatternList ')'
-{
-  local prodParamTypes::[Type] =
-    case prod.lookupValue.typeScheme.typerep of
-    | functionType(_, paramTypes, _) -> paramTypes
-    | _ -> error("invalid production type")
-    end;
-  local prodParamsList::[AspectRHSElem] =
-    map(\ty::Type -> aspectRHSElemFull(name("__generated_" ++ toString(genIntReally(ty)), top.location), ty, location=top.location),
-    prodParamTypes);
-  local prodParamNames::[QName] =
-    map((\asp::AspectRHSElem -> case asp of
-                                | aspectRHSElemFull(n,_) -> qNameId(n, location=top.location)
-                                | _ -> qNameId(name("ThisNameShouldntComeUp",top.location), location=top.location)
-                                end),
-            prodParamsList);
-  local paramCaseSubExpr::Exprs = foldr(
-    \param1::QName accum::Exprs ->
-      exprsCons(
-        baseExpr(param1,location=top.location),
-        ',',
-        accum,
-        location=top.location),
-    exprsEmpty(location=top.location),
-    prodParamNames);
-  local prodParams::AspectRHS = foldr(
-    aspectRHSElemCons(_, _, location=top.location),
-    aspectRHSElemNil(location=top.location),
-    prodParamsList);
+-- aspect production prodAppPattern_named
+-- top::Pattern ::= prod::QName '(' ps::PatternList ',' nps::NamedPatternList ')'
+-- {
+--   local prodParamTypes::[Type] =
+--     case prod.lookupValue.typeScheme.typerep of
+--     | functionType(_, paramTypes, _) -> paramTypes
+--     | _ -> error("invalid production type")
+--     end;
+--   local prodParamsList::[AspectRHSElem] =
+--     map(\ty::Type -> aspectRHSElemFull(name("__generated_" ++ toString(genIntReally(ty)), top.location), ty, location=top.location),
+--     prodParamTypes);
+--   local prodParamNames::[QName] =
+--     map((\asp::AspectRHSElem -> case asp of
+--                                 | aspectRHSElemFull(n,_) -> qNameId(n, location=top.location)
+--                                 | _ -> qNameId(name("ThisNameShouldntComeUp",top.location), location=top.location)
+--                                 end),
+--             prodParamsList);
+--   local paramCaseSubExpr::Exprs = foldr(
+--     \param1::QName accum::Exprs ->
+--       exprsCons(
+--         baseExpr(param1,location=top.location),
+--         ',',
+--         accum,
+--         location=top.location),
+--     exprsEmpty(location=top.location),
+--     prodParamNames);
+--   local prodParams::AspectRHS = foldr(
+--     aspectRHSElemCons(_, _, location=top.location),
+--     aspectRHSElemNil(location=top.location),
+--     prodParamsList);
 
-  top.makeAspect = \rhsExpr::Expr ->
-      Silver_AGDcl {
-        aspect production $QName{prod}
-        top::$TypeExpr{top.aspectTy} ::= $AspectRHS{prodParams}
-        { top.$QName{top.aspectAttr} = $Expr{caseExpr_c('case',
-                                                        paramCaseSubExpr,
-                                                        'of',
-                                                        terminal(Opt_Vbar_t, "|"),
-                                                        mRuleList_one(
-                                                            matchRule_c(ps,
-                                                                        '->',
-                                                                        rhsExpr,
-                                                                        location=top.location),
-                                                            location=top.location),
-                                                        'end',
-                                                        location=top.location)}; }
-      };
-}
+--   top.makeAspect = \rhsExpr::Expr ->
+--       Silver_AGDcl {
+--         aspect production $QName{prod}
+--         top::$TypeExpr{top.aspectTy} ::= $AspectRHS{prodParams}
+--         { top.$QNameAttrOccur{top.aspectAttr} = $Expr{caseExpr_c('case',
+--                                                         paramCaseSubExpr,
+--                                                         'of',
+--                                                         terminal(Opt_Vbar_t, "|"),
+--                                                         mRuleList_one(
+--                                                             matchRule_c(ps,
+--                                                                         '->',
+--                                                                         rhsExpr,
+--                                                                         location=top.location),
+--                                                             location=top.location),
+--                                                         'end',
+--                                                         location=top.location)}; }
+--       };
+-- }
 
-aspect production wildcPattern
-top::Pattern ::= '_'
-{
-  top.makeAspect = \rhsExpr::Expr ->
-    Silver_AGDcl {
-      aspect default production
-      top::$TypeExpr{top.aspectTy} ::=
-      { top.$QName{top.aspectAttr} = $Expr{rhsExpr}; }
-    };
-}
+-- aspect production wildcPattern
+-- top::Pattern ::= '_'
+-- {
+--   top.makeAspect = \rhsExpr::Expr ->
+--     Silver_AGDcl {
+--       aspect default production
+--       top::$TypeExpr{top.aspectTy} ::=
+--       { top.$QNameAttrOccur{top.aspectAttr} = $Expr{rhsExpr}; }
+--     };
+-- }
