6f11cbbcb59c5c5306a1977ea9189fc58cb772de
Nathan Ringo <nathan@remexre.xyz>
2021-01-21 15:55:02 -0600

    Adds typeclasses from algebra.

---
+++ grammars/silver/core/HeytingAlgebra.sv
@@ -0,0 +1,54 @@
+grammar silver:core;
+
+@{- This is the typeclass that provides the `true` and `false` constants, as
+  - well as the `&&`, `||`, and `!` operators.
+  -
+  - Laws:
+  -
+  - * Associativity:
+  -   * `conj(x, conj(y, z)) = conj(conj(x, y), z)`
+  -   * `disj(x, disj(y, z)) = disj(disj(x, y), z)`
+  - * Commutativity:
+  -   * `conj(x, y) = conj(y, x)`
+  -   * `disj(x, y) = disj(y, x)`
+  - * Absorption:
+  -   * `conj(x, disj(x, y)) = x`
+  -   * `disj(x, conj(x, y)) = x`
+  - * Idempotence:
+  -   * `conj(x, x) = x`
+  -   * `disj(x, x) = x`
+  - * Identity:
+  -   * `conj(x, true) = x`
+  -   * `disj(x, false) = x`
+  - * Implication:
+  -   * `implies(x, x) = true`
+  -   * `conj(x, implies(x, y)) = conj(x, y)`
+  -   * `conj(y, implies(x, y)) = y`
+  -   * `implies(x, conj(y, z)) = conj(implies(x, y), implies(x, z))`
+  - * Complement: `not(x) = implies(x, false)`
+  -}
+class HeytingAlgebra a {
+  -- TODO: Rename to false
+  @{- The false constant. -}
+  ff :: a;
+  -- TODO: Rename to true
+  @{- The true constant. -}
+  tt :: a;
+  @{- Implication.
+    -
+    - Note that it is not the case that `implies(x, y) = disj(not(p), q)` for
+    - all Heyting algebras. If you require this property, you want a
+    - `BooleanAlgebra`.
+    -}
+  implies :: (a ::= a a);
+  @{- Conjunction. This function corresponds to the `&&` operator. -}
+  conj :: (a ::= a a);
+  @{- Disjunction. This function corresponds to the `||` operator. -}
+  disj :: (a ::= a a);
+  @{- Complement. This function corresponds to the `!` operator.
+    -
+    - Note that it is not the case that `not(not(x)) = x` for all Heyting
+    - algebras. If you require this property, you want a `BooleanAlgebra`.
+    -}
+  not :: (a ::= a);
+}
---
+++ grammars/silver/core/Semiring.sv
@@ -0,0 +1,45 @@
+grammar silver:core;
+
+@{- Provides operator overloads for the `+` and `*` operators.
+  -
+  - Laws:
+  -
+  - * Commutative monoid under addition:
+  -   * Associativity: `add(add(x, y), z) = add(x, add(y, z))`
+  -   * Left Identity: `add(zero, x) = x`
+  -   * Right Identity: `add(x, zero) = x`
+  -   * Commutativity: `add(x, y) = add(y, x)`
+  - * Monoid under multiplication:
+  -   * Associativity: `mul(mul(x, y), z) = mul(x, mul(y, z))`
+  -   * Left Identity: `mul(one, x) = x`
+  -   * Right Identity: `mul(x, one) = x`
+  - * Multiplication distributes over addition:
+  -   * Left Distributivity: `mul(x, add(y, z)) = add(mul(x, y), mul(x, z))`
+  -   * Right Distributivity: `mul(add(x, y), z) = add(mul(x, z), mul(y, z))`
+  - * Left Annihilation: `mul(zero, x) = zero`
+  - * Right Annihilation: `mul(x, zero) = zero`
+  -}
+class Semiring a {
+  @{- The function corresponding to the `+` operator. -}
+  add :: (a ::= a a);
+  @{- The value corresponding to the `0` constant. -}
+  zero :: a;
+  @{- The function corresponding to the `*` operator. -}
+  mul :: (a ::= a a);
+  @{- The value corresponding to the `1` constant. -}
+  one :: a;
+}
+
+@{- Converts a non-negative integer into an arbitrary semiring. -}
+function fromNonnegativeInteger
+Semiring a => a ::= n::Integer
+{
+  local fromN2::a = fromNonnegativeInteger(n / 2);
+  return
+    if n == 0 then
+      zero
+    else if n % 2 == 0 then
+      add(fromN2, fromN2)
+    else
+      add(add(fromN2, one), fromN2);
+}
