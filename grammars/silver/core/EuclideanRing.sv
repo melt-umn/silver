cd8cd74cc4af86e2510c873aab8621400dc733ad
Nathan Ringo <nathan@remexre.xyz>
2021-01-21 15:55:02 -0600

    Adds typeclasses from algebra.

---
+++ grammars/silver/core/EuclideanRing.sv
@@ -0,0 +1,41 @@
+grammar silver:core;
+
+@{- Provides the `/` and `%` operators.
+  -
+  - Laws:
+  -
+  - * Integral Domain (TODO: are these equivalent?):
+  -   * `one != zero`
+  -   * `(a != zero && b != zero) -> mul(a, b) != zero`
+  - * Non-negativity of `degree`: `degree(x) >= 0`
+  - * Quotient: `(b != zero) -> (add(mul(div(a, b), b), mod(a, b)) = a)`
+  - * `degree`-`mod` interaction: `(b != zero) -> (mod(a, b) = 0 || degree(mod(a, b)) < degree(b))`
+  - * `degree`-`mul` interaction: `(a != zero && b != zero) -> (degree(a) <= degree(mul(a, b)))`
+  -}
+class CommutativeRing a => EuclideanRing a {
+  degree :: (Integer ::= a);
+  div :: (a ::= a a);
+  mod :: (a ::= a a);
+}
+
+@{- Computes the greatest common divisor of two numbers. -}
+function gcd
+Eq a, EuclideanRing a => a ::= a::a  b::a
+{
+  return
+    if b == zero then
+      a
+    else
+      gcd(b, mod(a, b));
+}
+
+@{- Computes the least common multiple of two numbers. -}
+function lcm
+Eq a, EuclideanRing a => a ::= a::a  b::a
+{
+  return
+    if a == zero || b == zero then
+      zero
+    else
+      div(mul(a, b), gcd(a, b));
+}
---
+++ grammars/silver/core/Ring.sv
@@ -0,0 +1,29 @@
+grammar silver:core;
+
+@{- This is the typeclass that provides the unary and binary `-` operators.
+  -
+  - Laws:
+  -
+  - * Left Inverse: `sub(x, x) = zero`
+  - * Right Inverse: `add(sub(zero, x), x) = zero`
+  -}
+class Semiring a => Ring a {
+  @{- The function corresponding to the binary `-` operator. -}
+  sub :: (a ::= a a);
+}
+
+@{- The function corresponding to the unary `-` operator. -}
+function negate
+Ring a => a ::= x::a
+{ return sub(zero, x); }
+
+@{- Converts an integer into an arbitrary ring. -}
+function fromInteger
+Ring a => a ::= n::Integer
+{
+  return
+    if n < 0 then
+      negate(fromNonnegativeInteger(-n))
+    else
+      fromNonnegativeInteger(n);
+}
