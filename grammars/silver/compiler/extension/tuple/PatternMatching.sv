4aeb8687defc7bc2d6300e76f81ff5bc3d10a1c9
Alexandra Hanson <alexandra.rose.hanson@gmail.com>
2021-02-01 18:42:34 -0600

    Moved tuple pattern matching syntax into its own file

---
+++ grammars/silver/compiler/extension/tuple/PatternMatching.sv
@@ -0,0 +1,32 @@
+-- Pattern matching on tuples
+nonterminal TuplePatternList with location, unparse;
+
+concrete production emptyTuplePattern
+top::Pattern ::= '(' ')'
+{
+  top.unparse = "()";
+  forwards to Silver_Pattern { silver:core:unit() };
+}
+
+concrete production tuplePattern
+top::Pattern ::= '(' ts::TuplePatternList ')'
+{
+  top.unparse = s"(${ts.unparse})";
+  forwards to ts.asTuplePattern;
+}
+
+synthesized attribute asTuplePattern::Pattern occurs on TuplePatternList;
+
+concrete production patternTuple_two
+top::TuplePatternList ::= fst::Pattern ',' snd::Pattern
+{
+  top.unparse = fst.unparse ++ ", " ++ snd.unparse;
+  top.asTuplePattern = Silver_Pattern { silver:core:pair($Pattern{fst}, $Pattern{snd}) };
+}
+
+concrete production patternTuple_more
+top::TuplePatternList ::= fst::Pattern ',' snd::TuplePatternList
+{
+  top.unparse = fst.unparse ++ ", " ++ snd.unparse;
+  top.asTuplePattern = Silver_Pattern { silver:core:pair($Pattern{fst}, $Pattern{snd.asTuplePattern}) };
+}
