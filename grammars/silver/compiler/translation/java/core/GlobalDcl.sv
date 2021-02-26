8d9c405dd646353c8b08c04ed60797aaa34b54e9
Lucas Kramer <krame505@umn.edu>
2021-02-26 13:49:17 -0600

    Fix syntax error

--- grammars/silver/compiler/translation/java/core/GlobalDcl.sv
+++ grammars/silver/compiler/translation/java/core/GlobalDcl.sv
@@ -4,14 +4,11 @@ aspect production globalValueDclConcrete
 top::AGDcl ::= 'global' id::Name '::' cl::ConstraintList '=>' t::TypeExpr '=' e::Expr ';'
 {
   top.initValues :=
-    
-  if null(cl.context) then 
-    s"\tpublic static final common.Thunk<${t.typerep.transType}> global_${id.name} = ${wrapThunkText(e.translation, t.typerep.transType)};\n"
-  else
-    s"""
+    if null(cl.contexts) then
+      s"\tpublic static final common.Thunk<${t.typerep.transType}> global_${id.name} = ${wrapThunkText(e.translation, t.typerep.transType)};\n"
+    else s"""
   public static final common.Thunk<${t.typerep.transType}> global_${id.name}(${implode(", ", map((.contextSigElem), cl.contexts))}){
     return ${wrapThunkText(e.translation, t.typerep.transType)};
   }
-"""
-
+""";
 }
