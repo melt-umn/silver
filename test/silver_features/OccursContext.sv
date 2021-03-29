60e8c821c487b96f765b070283a8d6f73eefcfaf
Lucas Kramer <lkramer@google.com>
2021-03-28 20:19:54 -0700

    Initial occurs constraint test case

---
+++ test/silver_features/OccursContext.sv
@@ -0,0 +1,19 @@
+grammar silver_features;
+
+function eqA
+attribute isEqualTo occurs on a, attribute isEqual {isEqualTo} occurs on a =>
+Boolean ::= x::a y::a
+{
+  x.isEqualTo = y;
+  return y.isEqual;
+}
+
+equalityTest(eqA(ee1, ee1), true, Boolean, silver_tests);
+equalityTest(eqA(ee1, ee2), false, Boolean, silver_tests);
+equalityTest(eqA(ee1, ee3), false, Boolean, silver_tests);
+equalityTest(eqA(ee2, ee1), false, Boolean, silver_tests);
+equalityTest(eqA(ee2, ee2), true, Boolean, silver_tests);
+equalityTest(eqA(ee2, ee3), false, Boolean, silver_tests);
+equalityTest(eqA(ee3, ee1), false, Boolean, silver_tests);
+equalityTest(eqA(ee3, ee2), false, Boolean, silver_tests);
+equalityTest(eqA(ee3, ee3), true, Boolean, silver_tests);
