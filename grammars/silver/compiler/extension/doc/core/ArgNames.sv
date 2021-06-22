55ab83c30d9c75c58f1df2777a6d16dcc73613be
Louis Goessling <louis@goessling.com>
2021-06-22 12:13:11 -0500

    Fix related issue with aspect functions/productions

--- grammars/silver/compiler/extension/doc/core/ArgNames.sv
+++ grammars/silver/compiler/extension/doc/core/ArgNames.sv
@@ -1,7 +1,8 @@
 grammar silver:compiler:extension:doc:core;
 
 synthesized attribute argNames::[String] 
-occurs on FunctionSignature, ProductionSignature, ProductionRHS, ProductionRHSElem;
+occurs on FunctionSignature, ProductionSignature, AspectProductionSignature,
+          AspectFunctionSignature, AspectRHS, AspectRHSElem, ProductionRHS, ProductionRHSElem;
 
 aspect production functionSignature
 top::FunctionSignature ::= cl::ConstraintList '=>' lhs::FunctionLHS '::=' rhs::ProductionRHS 
@@ -37,4 +38,44 @@ aspect production productionRHSElemType
 top::ProductionRHSElem ::= t::TypeExpr
 {
   top.argNames = []; -- Don't consider unnamed parameters against count
+}
+
+
+
+
+aspect production aspectProductionSignature
+top::AspectProductionSignature ::= lhs::AspectProductionLHS '::=' rhs::AspectRHS 
+{
+  top.argNames = rhs.argNames;
+}
+
+aspect production aspectFunctionSignature
+top::AspectFunctionSignature ::= lhs::AspectFunctionLHS '::=' rhs::AspectRHS 
+{
+  top.argNames = rhs.argNames;
+}
+
+
+aspect production aspectRHSElemNil
+top::AspectRHS ::= 
+{
+  top.argNames = [];
+}
+
+aspect production aspectRHSElemCons
+top::AspectRHS ::= h::AspectRHSElem t::AspectRHS
+{
+  top.argNames = h.argNames ++ t.argNames;
+}
+
+aspect production aspectRHSElemNone
+top::AspectRHSElem ::= '_'
+{
+  top.argNames = []; -- Don't consider _ parameters against count
+}
+
+aspect production aspectRHSElemFull
+top::AspectRHSElem ::= id::Name t::Type
+{
+  top.argNames = [id.name];
 }
