316afdec0790c65356c16eedb7e3f78191da6406
Louis Goessling <louis@goessling.com>
2021-06-30 16:26:24 -0500

    Docs

--- runtime/java/src/common/RTTIManager.java
+++ runtime/java/src/common/RTTIManager.java
@@ -2,6 +2,22 @@ package common;
 
 import java.util.*;
 
+/**
+ * Collects maps between fully-qualified silver names (some:package:NT) and singletons for terminals,
+ * nonterminals, and productions. The singletons represent the terminal/nonterminal/production itself,
+ * not instances thereof. So functions on the singletons grant access to static fields/methods on the
+ * terminal/nonterminal/production. These are meant to be usable for all the same functions we
+ * previously did via java reflection. So a Terminalton<T> is akin to a Class<T extends Terminal>, a
+ * Nonterminalton<T> to a Class<T extends Node>, and Prodleton<T> to a Class<T extends N extends Node>
+ * where .getNonterminalton() -> Nonterminalton<N>. These are registered into these lookup tables at
+ * init time.
+ * 
+ * This is used for the following:
+ *  - silver reflection
+ *  - decorators (read: implementing autocopy)
+ * 
+ * @author louisg
+ */
 public final class RTTIManager {
 	private static final Map<String, Prodleton<?>> productionsByName = new HashMap<String, Prodleton<?>>(4096, 0.5f); // guesstimates from silver
 	private static final Map<String, Nonterminalton<?>> nonterminalsByName = new HashMap<String, Nonterminalton<?>>(512, 0.5f);
@@ -31,6 +47,10 @@ public final class RTTIManager {
 		terminalsByName.put(tton.getName(), tton);
 	}
 
+	// Represents a production (not an instance of the production, but the production itself.)
+	// So implements reify and access to static fields (childtypes, childinheritedattributes) on
+	//  the nonterminal.
+	// T = the production type (PSomething)
 	public static abstract class Prodleton<T> {
 		public abstract T reify(
 			final silver.core.NAST origAST,
@@ -47,11 +67,15 @@ public final class RTTIManager {
 		public abstract Lazy[][] getChildInheritedAttributes();
 	}
 
+	// Represents a terminal (not an instance of the terminal, but the terminal itself.)
+	// T = the terminal type (TSomething)
 	public static abstract class Terminalton<T> {
 		public abstract T construct(final StringCatter lexeme, final silver.core.NLocation location);
 		public abstract String getName();
 	}
 
+	// Represents a nonterminal (not a production or instance, but the nonterminal type itself.)
+	// T = the nonterminal type (NSomething)
 	public static abstract class Nonterminalton<T> {
 		public abstract String[] getOccursInh();
 		public abstract String getName();
