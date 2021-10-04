b8ef9d58c829e71416d822b2eef232f965deb652
Lucas Kramer <krame505@umn.edu>
2021-10-04 13:07:25 -0500

    Add tests, fix bug with instance Ord [a]

--- test/silver_features/EqualityAttr.sv
+++ test/silver_features/EqualityAttr.sv
@@ -28,26 +28,55 @@ Eq a => top::EqExpr ::= x::a
 }
 -}
 
+abstract production seqEqExpr
+top::EqExpr ::= es::[EqExpr]
+{
+  propagate compareTo, isEqual, compareKey, compare;
+}
+
 global ee1::EqExpr = addEqExpr(intEqExpr(42), appEqExpr("abc", intEqExpr(5)));
 global ee2::EqExpr = addEqExpr(intEqExpr(42), appEqExpr("c", intEqExpr(5)));
 global ee3::EqExpr = addEqExpr(appEqExpr("c", intEqExpr(5)), intEqExpr(42));
+global ee4::EqExpr = seqEqExpr([ee1, ee2, ee3]);
+global ee5::EqExpr = seqEqExpr([ee3, ee2, ee1]);
 
 equalityTest(ee1 == ee1, true, Boolean, silver_tests);
 equalityTest(ee1 == ee2, false, Boolean, silver_tests);
 equalityTest(ee1 == ee3, false, Boolean, silver_tests);
+equalityTest(ee1 == ee4, false, Boolean, silver_tests);
 equalityTest(ee2 == ee1, false, Boolean, silver_tests);
 equalityTest(ee2 == ee2, true, Boolean, silver_tests);
 equalityTest(ee2 == ee3, false, Boolean, silver_tests);
+equalityTest(ee2 == ee4, false, Boolean, silver_tests);
 equalityTest(ee3 == ee1, false, Boolean, silver_tests);
 equalityTest(ee3 == ee2, false, Boolean, silver_tests);
 equalityTest(ee3 == ee3, true, Boolean, silver_tests);
+equalityTest(ee3 == ee4, false, Boolean, silver_tests);
+equalityTest(ee4 == ee1, false, Boolean, silver_tests);
+equalityTest(ee4 == ee2, false, Boolean, silver_tests);
+equalityTest(ee4 == ee3, false, Boolean, silver_tests);
+equalityTest(ee4 == ee4, true, Boolean, silver_tests);
+equalityTest(ee4 == ee5, false, Boolean, silver_tests);
+equalityTest([ee1, ee2, ee3] == [ee1, ee2, ee3], true, Boolean, silver_tests);
+equalityTest([ee1, ee2, ee3] == [ee3, ee2, ee1], false, Boolean, silver_tests);
 
 equalityTest(ee1 < ee1, false, Boolean, silver_tests);
 equalityTest(ee1 < ee2, true, Boolean, silver_tests);
 equalityTest(ee1 < ee3, false, Boolean, silver_tests);
+equalityTest(ee1 < ee4, true, Boolean, silver_tests);
 equalityTest(ee2 < ee1, false, Boolean, silver_tests);
 equalityTest(ee2 < ee2, false, Boolean, silver_tests);
 equalityTest(ee2 < ee3, false, Boolean, silver_tests);
+equalityTest(ee2 < ee4, true, Boolean, silver_tests);
 equalityTest(ee3 < ee1, true, Boolean, silver_tests);
 equalityTest(ee3 < ee2, true, Boolean, silver_tests);
 equalityTest(ee3 < ee3, false, Boolean, silver_tests);
+equalityTest(ee3 < ee4, true, Boolean, silver_tests);
+equalityTest(ee4 < ee1, false, Boolean, silver_tests);
+equalityTest(ee4 < ee2, false, Boolean, silver_tests);
+equalityTest(ee4 < ee3, false, Boolean, silver_tests);
+equalityTest(ee4 < ee4, false, Boolean, silver_tests);
+equalityTest(ee4 < ee5, false, Boolean, silver_tests);
+equalityTest(ee5 < ee4, true, Boolean, silver_tests);
+equalityTest([ee1, ee2, ee3] < [ee1, ee2, ee3], false, Boolean, silver_tests);
+equalityTest([ee3, ee2, ee1] < [ee1, ee2, ee3], true, Boolean, silver_tests);
