66e4e9e9743a455eb98054213975c3cfbbad66e2
Alexandra Hanson <alexandra.rose.hanson@gmail.com>
2021-10-18 13:57:49 -0500

    Initial attempt making lambda type signatures optional

--- grammars/silver/compiler/modification/lambda_fn/Lambda.sv
+++ grammars/silver/compiler/modification/lambda_fn/Lambda.sv
@@ -8,7 +8,55 @@ import silver:util:treeset as ts;
 terminal Lambda_kwd '\' lexer classes {KEYWORD,RESERVED};
 terminal Arrow_t '->' precedence = 0, lexer classes {SPECOP};
 
--- Using ProductionRHS here, it is basicly just a list of names with type expressions
+nonterminal LambdaParams with config, grammarName, env, location, unparse, errors, defs, inputElements,
+                              lambdaDefs, lambdaBoundVars, lexicalTyVarKinds, lexicalTypeVariables;
+nonterminal LambdaParam with config, grammarName, env, location, unparse, errors, defs, inputElements, deterministicCount,
+                              lambdaDefs, lambdaBoundVars, lexicalTyVarKinds, lexicalTypeVariables;
+
+concrete production lambdaParamsOne
+top::LambdaParams ::= p::LambdaParam {
+
+  top.unparse = p.unparse;
+  top.inputElements = p.inputElements;
+
+  p.deterministicCount = 0;
+
+}
+
+concrete production lambdaParamsCons
+top::LambdaParams ::= h::LambdaParam t::LambdaParams
+{
+  top.unparse = h.unparse ++ " " ++ t.unparse;
+
+  top.inputElements = h.inputElements ++ t.inputElements;
+  h.deterministicCount = length(t.inputElements);
+}
+
+concrete production lambdaParam
+top::LambdaParam ::= id::Name
+{
+  top.unparse = id.unparse;
+
+  local t::Type = freshType();
+
+  top.inputElements = [namedSignatureElement(id.name, t)];
+  top.defs := [childDef(top.grammarName, id.location, id.name, t)];
+
+  top.lexicalTyVarKinds <- []; -- TO DO
+  top.lexicalTypeVariables <- [];
+
+  production fName :: String = toString(genInt()) ++ ":" ++ id.name;
+  top.lambdaDefs := [lambdaParamDef(top.grammarName, id.location, fName, t)];
+  top.lambdaBoundVars := [id.name];
+
+  top.errors <-
+    if length(getValueDclInScope(id.name, top.env)) > 1 
+    then [err(id.location, "Value '" ++ id.name ++ "' is already bound.")]
+    else [];
+
+}
+
+-- Using ProductionRHS here, it is basically just a list of names with type expressions
 -- It is also used for the parameter definitions in functions, so using it here for consistancy
 concrete production lambda_c
 top::Expr ::= '\' params::ProductionRHS '->' e::Expr
@@ -18,8 +66,43 @@ top::Expr ::= '\' params::ProductionRHS '->' e::Expr
   forwards to lambdap(params, e, location=top.location);
 }
 
+concrete production lambda_c_wot
+top::Expr ::= '\' params::LambdaParams '->' e::Expr
+{
+
+  top.unparse = "\\ " ++ params.unparse ++ "->" ++ e.unparse;
+
+  forwards to lambdap_wot(params, e, location=top.location);
+
+}
+
 abstract production lambdap
 top::Expr ::= params::ProductionRHS e::Expr
+{
+  top.unparse = "\\ " ++ params.unparse ++ " -> " ++ e.unparse;
+  top.freeVars := ts:removeAll(params.lambdaBoundVars, e.freeVars);
+  
+  propagate errors;
+  
+  top.typerep = appTypes(functionType(length(params.inputElements), []), map((.typerep), params.inputElements) ++ [e.typerep]);
+
+  production attribute sigDefs::[Def] with ++;
+  sigDefs := params.lambdaDefs;
+  sigDefs <-
+    addNewLexicalTyVars_ActuallyVariables(
+      top.grammarName, top.location, params.lexicalTyVarKinds,
+      filter(\ tv::String -> null(getTypeDcl(tv, top.env)), nub(params.lexicalTypeVariables)));
+
+  propagate downSubst, upSubst;
+  propagate flowDeps, flowDefs;
+  
+  params.env = newScopeEnv(sigDefs, top.env);
+  e.env = params.env;
+  e.frame = inLambdaContext(top.frame, sourceGrammar=top.frame.sourceGrammar); --TODO: Is this sourceGrammar correct?
+}
+
+abstract production lambdap_wot
+top::Expr ::= params::LambdaParams e::Expr
 {
   top.unparse = "\\ " ++ params.unparse ++ " -> " ++ e.unparse;
   top.freeVars := ts:removeAll(params.lambdaBoundVars, e.freeVars);
