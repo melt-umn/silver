53facb36853516cc663040223a769ec83c8d22e6
Lucas Kramer <krame505@umn.edu>
2021-12-28 19:38:08 -0600

    Implement a mechanism for generating random terminal values

---
+++ grammars/silver/compiler/extension/treegen/TerminalGen.sv
@@ -0,0 +1,39 @@
+grammar silver:compiler:extension:treegen;
+
+import silver:compiler:metatranslation;
+import silver:reflect;
+import silver:regex;
+
+terminal GenArbTerminal_t 'genArbTerminal' lexer classes {KEYWORD, RESERVED};
+
+concrete production genArbTerminalNoLocExpr
+top::Expr ::= 'genArbTerminal' '(' te::TypeExpr ',' starProb::Expr ',' '_' ')'
+{
+  top.unparse = s"genArbTerminal(${te.unparse}, ${starProb.unparse}, _)";
+  
+  local regex::Regex =
+    case getTypeDcl(te.typerep.typeName, top.env) of
+    | termDcl(_, r, _) :: _ -> r
+    | _ -> empty()
+    end;
+  
+  forwards to
+    if null(getAttrDcl("silver:regex:genArbMatch", top.env))
+    then errorExpr([err(top.location, "Generation of arbitrary terminal values requires import of silver:regex")], location=top.location)
+    else Silver_Expr {
+      let genLexeme::RandomGen<String> =
+        decorate $Expr{translate(top.location, reflect(new(regex)))} with { starProb = $Expr{starProb}; }.genArbMatch
+      in \ loc::silver:core:Location -> silver:core:map(\ lexeme::String -> terminal($TypeExpr{te}, lexeme, loc))
+      end
+    };
+}
+
+concrete production genArbTerminalExpr
+top::Expr ::= 'genArbTerminal' '(' te::TypeExpr ',' starProb::Expr ',' loc::Expr ')'
+{
+  top.unparse = s"genArbTerminal(${te.unparse}, ${starProb.unparse}, ${loc.unparse})";
+  forwards to
+    mkFunctionInvocation(top.location,
+      genArbTerminalNoLocExpr('genArbTerminal', '(', te, ',', starProb, ',', '_', ')', location=top.location),
+      [loc]);
+}
---
+++ grammars/silver/regex/Arbitrary.sv
@@ -0,0 +1,106 @@
+grammar silver:regex;
+
+implicit synthesized attribute genArbMatch::RandomGen<String>;
+
+restricted inherited attribute starProb::Float;
+
+restricted inherited attribute altCountIn::Integer;
+restricted synthesized attribute altCount::Integer;
+
+type GenInhs = {starProb, altCountIn};
+restricted synthesized attribute altOptions::[Decorated Regex with GenInhs];
+
+attribute genArbMatch, starProb, altCountIn, altCount, altOptions occurs on Regex;
+-- propagate starProb on Regex;  -- TODO: Make propagate work with restricted attributes
+
+aspect default production
+top::Regex ::=
+{
+  top.altCount = top.altCountIn + 1;
+  top.altOptions = [top];
+}
+
+aspect production empty
+top::Regex ::=
+{
+  top.genArbMatch = error("Can't generate a match for empty regex");
+  top.altCount = top.altCountIn;
+  top.altOptions = [];
+}
+
+aspect production epsilon
+top::Regex ::=
+{
+  top.genArbMatch = "";
+}
+
+aspect production char
+top::Regex ::= c::Integer
+{
+  top.genArbMatch = charsToString([c]);
+}
+
+aspect production wildChar
+top::Regex ::=
+{
+  -- Generate ASCII chars, excluding \n
+  top.genArbMatch =
+    let i::Integer = randomRange(1, 126)
+    in charsToString([if i < newlineChar then i else i + 1])
+    end;
+  top.altCount = top.altCountIn + 126;
+}
+
+aspect production charRange
+top::Regex ::= l::Integer u::Integer
+{
+  top.genArbMatch = charsToString([randomRange(l, u)]);
+  top.altCount = top.altCountIn + u - l + 1;
+}
+
+aspect production negChars
+top::Regex ::= r::Regex
+{
+  production validAsciiChars::[Integer] =
+    filter(\ c::Integer -> decorate r with {wrt = c;}.deriv.nullable, range(1, 128));
+  top.genArbMatch = charsToString([head(drop(randomRange(0, length(validAsciiChars) - 1), validAsciiChars))]);
+  top.altCount = top.altCountIn + length(validAsciiChars);
+  r.altCountIn = 0;
+}
+
+aspect production seq
+top::Regex ::= r1::Regex r2::Regex
+{
+  top.genArbMatch = r1.genArbMatch ++ r2.genArbMatch;
+
+  r1.starProb = top.starProb;
+  r2.starProb = top.starProb;
+  r1.altCountIn = 0;
+  r2.altCountIn = 0;
+}
+
+aspect production alt
+top::Regex ::= r1::Regex r2::Regex
+{
+  top.genArbMatch =
+    let i::Integer = randomRange(0, top.altCount - 1)
+    in head(dropWhile(\ r::Decorated Regex with GenInhs -> r.altCount < i, top.altOptions)).genArbMatch
+    end;
+  thread altCountIn, altCount on top, r1, r2, top;
+  top.altOptions = r1.altOptions ++ r2.altOptions;
+
+  r1.starProb = top.starProb;
+  r2.starProb = top.starProb;
+}
+
+aspect production star
+top::Regex ::= r::Regex
+{
+  top.genArbMatch =
+    if random < top.starProb
+    then r.genArbMatch ++ top.genArbMatch
+    else "";
+
+  r.starProb = top.starProb;
+  r.altCountIn = 0;
+}
