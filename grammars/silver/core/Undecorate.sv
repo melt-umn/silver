9dad92f8382c5a300a33c5e1f35c0d2609885217
Lucas Kramer <krame505@umn.edu>
2022-01-14 16:04:07 -0600

    Add a function to undecorate a partial reference

--- grammars/silver/core/Undecorate.sv
+++ grammars/silver/core/Undecorate.sv
@@ -7,6 +7,13 @@ foreign {
   "java": return "common.OriginsUtil.duplicatePoly(%x%.undecorate(), originCtx)";
 }
 
+function newPartial
+a ::= x::PartiallyDecorated a with i
+{ return error("foreign function"); }
+foreign {
+  "java": return "common.OriginsUtil.duplicatePoly(%x%.undecorate(), originCtx)";
+}
+
 function castRef
 i1 subset i2 => Decorated a with i1 ::= x::Decorated a with i2
 { return error("foreign function"); }
