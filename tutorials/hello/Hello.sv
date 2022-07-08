c3b2a839f8e41744bd3762d65a588e439f61d0d6
unironically <lbvac230@gmail.com>
2022-07-07 21:55:12 -0500

    Refactored tutorials/hello to use monadic main function.

--- tutorials/hello/Hello.sv
+++ tutorials/hello/Hello.sv
@@ -1,27 +1,5 @@
 grammar hello;
 
-{-
-function main 
-IOVal<Integer> ::= largs::[String] ioin::IOToken
-{
-  return ioval(printT(" World!\n",
-                 printT("Hello", ioin)),
-               0);
-}
--}
-
-{-
-function main 
-IOVal<Integer> ::= largs::[String] ioin::IOToken
-{
-  return evalIO( do {
-    print("Hello, world!\n");
-    return 0;
-  }, ioin);
-}
--}
-
-
 function main 
 IO<Integer> ::= largs::[String]
 {
