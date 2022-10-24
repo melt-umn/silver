10789f3a09a548ea644c2ec0014ec956926ed343
Ian Kariniemi <ian@kariniemi.dev>
2022-10-24 16:29:38 -0500

    Initial setup for datalog extension.

---
+++ grammars/silver/compiler/extension/datalog/AbstractSyntax.sv
@@ -0,0 +1,1 @@
+grammar silver:compiler:extension:datalog;
---
+++ grammars/silver/compiler/extension/datalog/Project.sv
@@ -0,0 +1,11 @@
+grammar silver:compiler:extension:datalog;
+
+imports silver:core;
+imports silver:compiler:analysis:typechecking:core;
+imports silver:compiler:definition:core;
+imports silver:compiler:definition:env;
+imports silver:compiler:definition:type;
+imports silver:compiler:definition:type:syntax;
+imports silver:compiler:extension:patternmatching;
+imports silver:compiler:extension:silverconstruction;
+imports silver:compiler:modification:defaultattr;
