e6da2149eb59d19cae5c014f87898617ffc24237
Lucas Kramer <krame505@umn.edu>
2023-04-03 15:07:55 -0500

    Fix #746, properly check that implicit forward copy eqs for host syn attributes don't exceed flow types

---
+++ test/flow/FwrdInh.sv
@@ -0,0 +1,13 @@
+grammar flow;
+
+nonterminal FIExpr with env1, env2, errors1;
+flowtype FIExpr = errors1 {env1};
+
+warnCode "In production flow:fwrdEqSiteExceedsFT, the implicit copy equation for flow:errors1 (due to forwarding) would exceed the attribute's flow type with dependencies on flow:env2" {
+production fwrdEqSiteExceedsFT
+top::FIExpr ::= e::FIExpr
+{
+  forward.env1 = top.env2;
+  forwards to e;
+}
+}
