1f8a390b781b0fa0951296346311221c1d5c4974
Lucas Kramer <krame505@umn.edu>
2023-07-27 20:41:38 -0500

    Fix test

--- test/origintracking/tracing_origins/test.sv
+++ test/origintracking/tracing_origins/test.sv
@@ -13,8 +13,8 @@ Nat ::=
 equalityTest(
 	case getOriginInfo(constructFour()) of
 	| just(originAndRedexOriginInfo(_, _,
-		[traceNote("nat.sv:18:16")], _, 
-		[traceNote("test.sv:14:27"), traceNote("test.sv:10:15")], _)) -> "OK"
+		[traceNote("test.sv:14:27"), traceNote("test.sv:10:15")], _,
+		originNotes=[traceNote("nat.sv:18:16")])) -> "OK"
 	| _ -> "NO"
 	end,
 	"OK", String, oitests);
