7d14075911edc89741b0266a40acbbf2d445d262
Lucas Kramer <krame505@umn.edu>
2023-08-08 19:53:46 -0500

    FIx origin note order in test

--- test/origintracking/tracing_origins/test.sv
+++ test/origintracking/tracing_origins/test.sv
@@ -13,8 +13,8 @@ Nat ::=
 equalityTest(
 	case getOriginInfo(constructFour()) of
 	| just(originAndRedexOriginInfo(_, _,
-		[traceNote("tracing_origins:test.sv:14:27"), traceNote("tracing_origins:test.sv:10:15")], _,
+		[traceNote("tracing_origins:test.sv:10:15"), traceNote("tracing_origins:test.sv:14:27")], _,
 		originNotes=[traceNote("common:nat.sv:18:16")])) -> "OK"
-	| _ -> "NO"
+	| ou -> hackUnparse(ou)
 	end,
 	"OK", String, oitests);
