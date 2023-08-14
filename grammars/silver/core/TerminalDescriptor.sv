79d10d169ae5fdbed357924b16ad9e451b71be7f
Lucas Kramer <krame505@umn.edu>
2023-08-14 17:37:48 -0500

    Use new data syntax

--- grammars/silver/core/TerminalDescriptor.sv
+++ grammars/silver/core/TerminalDescriptor.sv
@@ -1,26 +1,10 @@
 grammar silver:core;
 
-synthesized attribute lexeme :: String;
-synthesized attribute lexerClasses :: [String];
-synthesized attribute terminalLocation :: Location;
-synthesized attribute terminalName :: String;
+annotation lexeme :: String;
+annotation lexerClasses :: [String];
+annotation terminalLocation :: Location;
+annotation terminalName :: String;
 
-data nonterminal TerminalDescriptor with lexeme, lexerClasses, terminalLocation, terminalName;
-
--- TODO: Derive this
-instance Eq TerminalDescriptor {
-  eq = \ x::TerminalDescriptor y::TerminalDescriptor ->
-    x.lexeme == y.lexeme &&
-    x.lexerClasses == y.lexerClasses &&
-    x.terminalName == y.terminalName &&
-    x.terminalLocation == y.terminalLocation;
-}
-
-abstract production terminalDescriptor
-top::TerminalDescriptor ::= lexeme::String lexerClasses::[String] terminalName::String terminalLocation::Location
-{
-  top.lexeme = lexeme;
-  top.lexerClasses = lexerClasses;
-  top.terminalLocation = terminalLocation;
-  top.terminalName = terminalName;
-}
+data TerminalDescriptor = terminalDescriptor
+  with lexeme, lexerClasses, terminalLocation, terminalName;
+derive Eq on TerminalDescriptor;
