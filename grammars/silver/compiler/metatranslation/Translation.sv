9254830054c4f00f7a80a12ecb0da1b6bf88bbf0
Lucas Kramer <krame505@umn.edu>
2023-10-27 16:41:23 +0200

    Add comments

--- grammars/silver/compiler/metatranslation/Translation.sv
+++ grammars/silver/compiler/metatranslation/Translation.sv
@@ -184,8 +184,12 @@ top::AST ::= terminalName::String lexeme::String location::Location
 
   top.translation = Silver_Expr {
     terminal(
+      -- The type expression from the name of the terminal
       $TypeExpr{nominalTypeExpr(makeQNameType(terminalName, top.givenLocation))},
+      -- The quoted lexeme
       $Expr{stringConst(terminal(String_t, s"\"${escapeString(lexeme)}\"", top.givenLocation))},
+      -- Try to get the location dynamically from the parsed origin location if tracking is available,
+      -- or else fall back to the compile-time AST location.
       silver:core:fromMaybe($Expr{locationAST.translation}, silver:core:getParsedOriginLocation(silver:core:ambientOrigin())))
   };
   
