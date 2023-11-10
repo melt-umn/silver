368f5ed404383d9e6adb954a803a2c760e326975
Lucas Kramer <krame505@umn.edu>
2023-11-09 19:30:30 -0600

    Add debugger stub function

---
+++ runtime/java/src/common/Debug.java
@@ -0,0 +1,11 @@
+package common;
+
+public class Debug {
+    
+    public static DecoratedNode runDebug(DecoratedNode tree) {
+        // Do interactive debug stuff
+
+        // Return the original tree when the user exits
+        return tree;
+    }
+}
