015ae8d14de6d0ddec36e8b4ac05b04485b65e43
Lucas Kramer <krame505@umn.edu>
2023-12-21 19:44:56 -0600

    Remove unused import

--- grammars/silver/compiler/modification/concisefunctions/Project.sv
+++ grammars/silver/compiler/modification/concisefunctions/Project.sv
@@ -1,8 +1,7 @@
 grammar silver:compiler:modification:concisefunctions;
 
 imports silver:compiler:definition:core;
-imports silver:compiler:modification:lambda_fn;
-imports silver:compiler:definition:type:syntax hiding Arrow_t; -- We use lambda_fn:Arrow_t
+imports silver:compiler:definition:type:syntax;
 imports silver:compiler:definition:type;
 imports silver:compiler:definition:env;
 imports silver:compiler:definition:flow:env;
