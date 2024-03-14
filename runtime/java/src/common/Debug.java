48bd0bc369960ebebb0722a3a9d5dbbc7fbede95
Matthew Feraru <ferar003@umn.edu>
2024-03-13 21:25:09 -0500

    args ready, but file/html contextualization still not ready

--- runtime/java/src/common/Debug.java
+++ runtime/java/src/common/Debug.java
@@ -49,9 +49,9 @@ public class Debug {
         // creating a context stack when we run the debugger
         CMDContextVisualization cStack = new CMDContextVisualization("********************************");
         // if we want a file visualization:
-        // FileContextVisualization cStack = new FileContextVisualization();
+        // FileContextVisualization cStack = new FileContextVisualization("context.txt", "********************************");
         // if we want an HTML visualization:
-        // HTMLContextVisualization cStack = new HTMLContextVisualization();
+        // HTMLContextVisualization cStack = new HTMLContextVisualization("********************************");
         cStack.push(currentNode);
         if(toggleCStackDisplay){
             cStack.show();
