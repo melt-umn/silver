melt-umn/silver

commit 22987cb9c7b66f311a0ca37c35bdc71244d41ee0

Improve flow info tracking for curried dispatch application

grammars/silver/compiler/definition/core/Expr.sv
@@ -401,16 +401,16 @@ top::Expr ::= @e::Expr @es::AppExprs @anns::AnnoAppExprs
   top.unparse = e.unparse ++ "(" ++ es.unparse ++ "," ++ anns.unparse ++ ")";
 
   local t :: Type = performSubstitution(e.typerep, e.upSubst);
-  nondecorated local extraArgs :: AppExprs =
+  production extraArgs :: AppExprs =
     foldl(snocAppExprs(_, ',', _), emptyAppExprs(),
       map(presentAppExpr, drop(es.appExprSize - length(t.inputTypes), es.rawExprs)));
-  nondecorated local dispatchArgs :: AppExprs =
+  production dispatchArgs :: AppExprs =
     foldl(snocAppExprs(_, ',', _), emptyAppExprs(),
       map(presentAppExpr, take(es.appExprSize - length(t.inputTypes), es.rawExprs)));
 
   forwards to application(
-    application(@e, '(', extraArgs, ',', emptyAnnoAppExprs(), ')'),
-    '(', dispatchArgs, ',', ^anns, ')');
+    application(@e, '(', @extraArgs, ',', emptyAnnoAppExprs(), ')'),
+    '(', @dispatchArgs, ',', ^anns, ')');
 }
 
 abstract production dispatchApplication implements Application

grammars/silver/compiler/definition/flow/env/Expr.sv
@@ -248,15 +248,20 @@ top::Expr ::= @e::Expr @es::AppExprs @anns::AnnoAppExprs
 aspect production curriedDispatchApplication
 top::Expr ::= @e::Expr @es::AppExprs @anns::AnnoAppExprs
 {
-  es.appProd =
+  -- We override these attributes in what we forward to, as a special case to
+  -- be more precise about what production is being applied.
+  dispatchArgs.appProd =
     case e of
     | productionReference(q) -> just(q.lookupValue.dcl.namedSignature)
     | _ -> nothing()
     end;
-  es.appIndexOffset = 0;
-  e.decSiteVertexInfo = nothing();
-  es.decSiteVertexInfo = top.decSiteVertexInfo;
-  es.alwaysDecorated = top.alwaysDecorated;
+  dispatchArgs.appIndexOffset = 0;
+  dispatchArgs.decSiteVertexInfo = top.decSiteVertexInfo;
+  dispatchArgs.alwaysDecorated = top.alwaysDecorated;
+  extraArgs.appProd = dispatchArgs.appProd;
+  extraArgs.appIndexOffset = dispatchArgs.appExprSize;
+  extraArgs.decSiteVertexInfo = top.decSiteVertexInfo;
+  extraArgs.alwaysDecorated = top.alwaysDecorated;
 }
 
 aspect production dispatchApplication

commit d7c123c1469d6523df32b30604511562522d6572

Fix a flow error caused by 22987cb9c7b66f311a0ca37c35bdc71244d41ee0

grammars/silver/compiler/definition/core/Expr.sv
@@ -401,6 +401,8 @@ top::Expr ::= @e::Expr @es::AppExprs @anns::AnnoAppExprs
   top.unparse = e.unparse ++ "(" ++ es.unparse ++ "," ++ anns.unparse ++ ")";
 
   local t :: Type = performSubstitution(e.typerep, e.upSubst);
+
+  -- TODO: args are being (unavoidably?) re-decorated here.
   production extraArgs :: AppExprs =
     foldl(snocAppExprs(_, ',', _), emptyAppExprs(),
       map(presentAppExpr, drop(es.appExprSize - length(t.inputTypes), es.rawExprs)));

grammars/silver/compiler/definition/flow/env/Expr.sv
@@ -248,17 +248,23 @@ top::Expr ::= @e::Expr @es::AppExprs @anns::AnnoAppExprs
 aspect production curriedDispatchApplication
 top::Expr ::= @e::Expr @es::AppExprs @anns::AnnoAppExprs
 {
-  -- We override these attributes in what we forward to, as a special case to
-  -- be more precise about what production is being applied.
-  dispatchArgs.appProd =
+  es.appProd =
     case e of
     | productionReference(q) -> just(q.lookupValue.dcl.namedSignature)
     | _ -> nothing()
     end;
+  es.appIndexOffset = 0;
+  e.decSiteVertexInfo = nothing();
+  es.decSiteVertexInfo = top.decSiteVertexInfo;
+  es.alwaysDecorated = top.alwaysDecorated;
+
+  -- We override these attributes in what we forward to, as a special case to
+  -- be more precise about what production is being applied.
+  dispatchArgs.appProd = es.appProd;
   dispatchArgs.appIndexOffset = 0;
   dispatchArgs.decSiteVertexInfo = top.decSiteVertexInfo;
   dispatchArgs.alwaysDecorated = top.alwaysDecorated;
-  extraArgs.appProd = dispatchArgs.appProd;
+  extraArgs.appProd = es.appProd;
   extraArgs.appIndexOffset = dispatchArgs.appExprSize;
   extraArgs.decSiteVertexInfo = top.decSiteVertexInfo;
   extraArgs.alwaysDecorated = top.alwaysDecorated;