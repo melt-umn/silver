a9212ffdde230a51709e8c54ecbecfe45423fa77
Lucas Kramer <krame505@umn.edu>
2024-10-14 20:14:37 -0500

    Make findPossibleDecSites more efficient by caching all explored decoration sites, rather than just the current branch of the search tree

--- grammars/silver/compiler/definition/flow/env/DecSites.sv
+++ grammars/silver/compiler/definition/flow/env/DecSites.sv
@@ -48,8 +48,7 @@ DecSiteTree ::=
        else neverDec()) +
       case vt of
       -- Via flow type
-      -- TODO: We shouldn't actually ever ask for these?
-      | lhsVertexType_real() -> alwaysDec()
+      | lhsVertexType_real() -> error("findDecSites: lhsVertexType") -- Should never actually be a decoration site
       | transAttrVertexType(lhsVertexType_real(), attrName) -> alwaysDec()
       -- Via forwarding
       | forwardVertexType_real() -> forwardDec(prodName, nothing())
@@ -108,19 +107,18 @@ DecSiteTree ::=
  - might be supplied to some vertex type.
  - This is used in checking for potentially hidden transitive dependencies.
  - This mirrors the above, but we also consider sites where a tree is only conditionally shared.
+ - Since we only care if a vertex is *possibly* supplied with an attribute, we can memoize the
+ - vertices visited in the enitre search (using a State monad) rather than just the current branch.
  -
  - @param prodName The name of the production containing the vertex type.
  - @param vt The vertex type to find decoration sites for.
- - @param seen A list of (production name, vertex type) pairs that have already been visited.
- - @param seenDispatch A list of (dispatch name, dispatch rhs name) pairs that have already been visited.
  - @param flowEnv The flow environment.
  - @param realEnv The regular environment.
  - @return A decision tree to determine if an inherited attributes could possibly be supplied for vt.
  -}
 function findPossibleDecSites
-DecSiteTree ::=
+State<([(String, VertexType)], [(String, String)]) DecSiteTree> ::=
   prodName::String vt::VertexType
-  seen::[(String, VertexType)] seenDispatch::[(String, String)]
   flowEnv::FlowEnv realEnv::Env
 {
   local prodDcl :: [ValueDclInfo] = getValueDcl(prodName, realEnv);
@@ -137,74 +135,73 @@ DecSiteTree ::=
     | _ -> ""
     end;
 
-  local recurse::(DecSiteTree ::= String VertexType) =
-    findPossibleDecSites(_, _, (prodName, vt) :: seen, seenDispatch, flowEnv, realEnv);
+  local recurse::(State<([(String, VertexType)], [(String, String)]) DecSiteTree> ::= String VertexType) =
+    findPossibleDecSites(_, _, flowEnv, realEnv);
 
-  return
-    if contains((prodName, vt), seen)
-    then neverDec()
-    else
-      -- Direct inherited equation at a decoration site
-      (if vt.isInhDefVertex
-       then directDec(prodName, vt)
-       else neverDec()) +
-      case vt of
-      -- Via flow type
-      -- TODO: We shouldn't actually ever ask for these?
-      | lhsVertexType_real() -> alwaysDec()
-      | transAttrVertexType(lhsVertexType_real(), attrName) -> alwaysDec()
-      -- Via forwarding
-      | forwardVertexType_real() -> forwardDec(prodName, nothing())
-      | localVertexType("forward") -> forwardDec(prodName, nothing())
-      | localVertexType(fName) when
-          isForwardProdAttr(fName,
-            newScopeEnv(flatMap((.prodDefs), getProdAttrs(prodName, realEnv)), emptyEnv())) ->
-        forwardDec(prodName, just(fName))
-      -- Via projected remote equation
-      | subtermVertexType(_, prodOrSig, sigName) ->
-          if !null(getValueDcl(prodOrSig, realEnv))
-          -- Projected from a production
-          then recurse(prodOrSig, rhsVertexType(sigName))
-          -- Projected from a dispatch signature
-          else if contains((prodOrSig, sigName), seenDispatch)
-          -- This is a dispatch that we are already trying to resolve.
-          then neverDec()
-          -- Otherwise, look at all the (host) productions that implement this dispatch signature
-          else sum(map(
-            \ prod::(String, [String]) ->
-              case getTypeDcl(prodOrSig, realEnv) of
-              | sigDcl :: _
-                  when drop(positionOf(sigName, sigDcl.dispatchSignature.inputNames), prod.2)
-                  matches sn :: _ ->
-                findPossibleDecSites(
-                  prod.1, rhsVertexType(sn),
-                  (prodOrSig, rhsVertexType(sigName)) :: seen,
-                  (prod.1, sn) :: seenDispatch,
-                  flowEnv, realEnv)
-              | _ -> error(s"findDecSites: Couldn't resolve ${sigName} in ${prodOrSig}")
-              end,
-            getImplementingProds(prodOrSig, flowEnv)))
-      -- Via signature/dispatch sharing
-      | rhsVertexType(sigName) when lookupSignatureInputElem(sigName, ns).elementShared ->
-        sum(unzipWith(recurse,
-          -- places where this child was decorated in a production forwarding to this one,
-          -- or in a dispatch signature that this production implements
-          lookupAllSigShareSites(prodName, sigName, flowEnv, realEnv)))
-      | _ -> neverDec()
-      end +
-      -- Via direct sharing
-      sum(map(recurse(prodName, _), lookupRefPossibleDecSites(prodName, vt, flowEnv))) +
-      -- Via translation attribute sharing
-      sum(
-        flatMap(
-          \ attrName ->
-            case getAttrDcl(attrName, realEnv) of
-            | dcl :: _ when dcl.isTranslation ->
-              map(\ transDecSite -> transAttrDec(attrName, recurse(prodName, transDecSite)),
-                lookupRefPossibleDecSites(prodName, transAttrVertexType(vt, attrName), flowEnv))
-            | _ -> []
-            end,
-          getHostSynsFor(ntName, flowEnv)));
+  return do {
+    seen :: ([(String, VertexType)], [(String, String)]) <- getState();
+    if contains((prodName, vt), seen.1)
+    then pure(neverDec())
+    else do {
+      setState(((prodName, vt) :: seen.1, seen.2));
+      viaVertex :: DecSiteTree <-
+        case vt of
+        -- Via forwarding
+        | forwardVertexType_real() -> pure(forwardDec(prodName, nothing()))
+        | localVertexType("forward") -> pure(forwardDec(prodName, nothing()))
+        | localVertexType(fName) when
+            isForwardProdAttr(fName,
+              newScopeEnv(flatMap((.prodDefs), getProdAttrs(prodName, realEnv)), emptyEnv())) ->
+          pure(forwardDec(prodName, just(fName)))
+        -- Via projected remote equation
+        | subtermVertexType(_, prodOrSig, sigName) ->
+            if !null(getValueDcl(prodOrSig, realEnv))
+            -- Projected from a production
+            then recurse(prodOrSig, rhsVertexType(sigName))
+            -- Projected from a dispatch signature
+            else if contains((prodOrSig, sigName), seen.2)
+            -- This is a dispatch that we are already trying to resolve.
+            then pure(neverDec())
+            -- Otherwise, look at all the (host) productions that implement this dispatch signature
+            else map(sum, traverseA(
+              \ prod::(String, [String]) ->
+                case getTypeDcl(prodOrSig, realEnv) of
+                | sigDcl :: _
+                    when drop(positionOf(sigName, sigDcl.dispatchSignature.inputNames), prod.2)
+                    matches sn :: _ -> do {
+                      setState((seen.1, (prod.1, sn) :: seen.2));
+                      recurse(prod.1, rhsVertexType(sn));
+                    }
+                | _ -> error(s"findDecSites: Couldn't resolve ${sigName} in ${prodOrSig}")
+                end,
+              getImplementingProds(prodOrSig, flowEnv)))
+        -- Via signature/dispatch sharing
+        | rhsVertexType(sigName) when lookupSignatureInputElem(sigName, ns).elementShared ->
+          map(sum, sequence(unzipWith(recurse,
+            -- places where this child was decorated in a production forwarding to this one,
+            -- or in a dispatch signature that this production implements
+            lookupAllSigShareSites(prodName, sigName, flowEnv, realEnv))))
+        | _ -> pure(neverDec())
+        end;
+      viaDirectShare :: [DecSiteTree] <-
+        traverseA(recurse(prodName, _), lookupRefPossibleDecSites(prodName, vt, flowEnv));
+      viaTransAttrShare :: [[DecSiteTree]] <-
+        traverseA(\ attrName ->
+          case getAttrDcl(attrName, realEnv) of
+          | dcl :: _ when dcl.isTranslation ->
+            traverseA(\ transDecSite -> map(transAttrDec(attrName, _), recurse(prodName, transDecSite)),
+              lookupRefPossibleDecSites(prodName, transAttrVertexType(vt, attrName), flowEnv))
+          | _ -> pure([])
+          end,
+          getHostSynsFor(ntName, flowEnv));
+      return
+        -- Direct inherited equation at a decoration site
+        (if vt.isInhDefVertex
+        then directDec(prodName, vt)
+        else neverDec()) +
+        viaVertex + sum(viaDirectShare) + sum(concat(viaTransAttrShare));
+    };
+  };
 }
 
 -- Flatten a resolved decision tree, to determine the minimal places where an
@@ -342,4 +339,5 @@ function decSitesMissingInhEqs
  -}
 fun possibleDecSiteHasInhEq
 Boolean ::= prodName::String vt::VertexType attrName::String flowEnv::FlowEnv realEnv::Env =
-  resolveDecSiteInhEq(attrName, findPossibleDecSites(prodName, vt, [], [], flowEnv, realEnv), flowEnv) == alwaysDec();
+  resolveDecSiteInhEq(attrName, evalState(findPossibleDecSites(prodName, vt, flowEnv, realEnv), ([], [])), flowEnv)
+  == alwaysDec();
