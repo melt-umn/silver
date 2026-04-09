aac542f3a04390d562c9654f6f15a93b84abbe72
Luke <bessa028@umn.edu>
2026-04-09 10:55:23 -0500

    Moved LabelNames.sv declarations into GraphSpec.sv

--- grammars/silver/compiler/extension/scopegraphs/GraphSpec.sv
+++ grammars/silver/compiler/extension/scopegraphs/GraphSpec.sv
@@ -1,9 +1,13 @@
 grammar silver:compiler:extension:scopegraphs;
 
---
+--------------------------
+-- Scope graph labels spec
 
 production graphSpec
-top::AGDcl ::= sgId::Maybe<String> qns::LabelNames labsId::String
+top::AGDcl ::=
+  sgId::Maybe<String>
+  qns::LabelNames
+  labsId::String
 {
   -- alias of the set of inherited attributes corresponding to graph label set
   nondecorated local alias::AGDcl = Silver_AGDcl {
@@ -20,4 +24,26 @@ top::AGDcl ::= sgId::Maybe<String> qns::LabelNames labsId::String
 
   -- 'graph definition' consisting of an identifier and constituent labels
   top.scopeGraphDefs := [scopeGraphDef(defaultEnvItem(graphDcl(sgName, qns.names)))];
+}
+
+--------------
+-- Label names
+
+synthesized attribute names::[String] occurs on LabelNames;
+synthesized attribute fsInhs::FlowSpecInhs occurs on LabelNames;
+
+production labelNamesOne
+top::LabelNames ::= lab::String
+{
+  top.names = [lab];
+  top.fsInhs = oneFlowSpecInhs(flowSpecInh(qNameAttrOccur(qName(lab))));
+}
+
+production labelNamesCons
+top::LabelNames ::= lab::String ns::LabelNames
+{
+  top.names = lab::ns.names;
+  top.fsInhs = consFlowSpecInhs(
+    flowSpecInh(qNameAttrOccur(qName(lab))), ',', ns.fsInhs
+  );
 }
